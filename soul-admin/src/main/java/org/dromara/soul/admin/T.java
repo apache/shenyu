a4f255314c8eed6d4ec5a991fa851ddcdc331d69
Administrator <549477611@qq.com>
2018-07-11 17:20:57 +0800

    first commit

---
+++ soul-admin/src/main/java/org/dromara/soul/admin/T.java
@@ -0,0 +1,12 @@
+package org.dromara.soul.admin;
+
+/**
+ * <p>Description: .</p>
+ *
+ * @author xiaoyu(Myth)
+ * @version 1.0
+ * @date 2018/7/11 17:09
+ * @since JDK 1.8
+ */
+public class T {
+}
