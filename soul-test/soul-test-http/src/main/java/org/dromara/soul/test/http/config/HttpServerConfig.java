aea781ac01a8245849a6a712411eb6196f539339
Administrator <549477611@qq.com>
2018-11-20 15:21:11 +0800

    soul commit.

--- soul-test/soul-test-http/src/main/java/org/dromara/soul/test/http/config/HttpServerConfig.java
+++ soul-test/soul-test-http/src/main/java/org/dromara/soul/test/http/config/HttpServerConfig.java
@@ -20,20 +20,13 @@ package org.dromara.soul.test.http.config;
 
 import org.dromara.soul.test.http.router.SoulTestHttpRouter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
-import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.server.reactive.HttpHandler;
-import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
-import reactor.ipc.netty.http.server.HttpServer;
-import reactor.ipc.netty.resources.LoopResources;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -56,26 +49,6 @@ public class HttpServerConfig {
     @Bean
     public RouterFunction<ServerResponse> monoRouterFunction(SoulTestHttpRouter soulTestHttpRouter) {
         return soulTestHttpRouter.routes();
-    }
-
-    @Bean
-    public HttpServer httpServer(RouterFunction<?> routerFunction) {
-        HttpHandler httpHandler = RouterFunctions.toHttpHandler(routerFunction);
-        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(httpHandler);
-        HttpServer server = HttpServer.create(
-                environment.getProperty("server.address"),
-                Integer.valueOf(environment.getProperty("server.port")));
-        server.newHandler(adapter);
-        return server;
-    }
-
-    @Bean
-    public ReactiveWebServerFactory reactiveWebServerFactory() {
-        NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
-        factory.addServerCustomizers(builder -> {
-            builder.loopResources(LoopResources.create("webflux-http", 8, true));
-        });
-        return factory;
     }
 
 
