85070db69550a9b70346d6cf65f753996f11ba74
Administrator <549477611@qq.com>
2018-11-30 15:36:17 +0800

    license commit.

--- soul-web/src/main/java/org/dromara/soul/web/concurrent/SoulThreadFactory.java
+++ soul-web/src/main/java/org/dromara/soul/web/concurrent/SoulThreadFactory.java
@@ -1,19 +1,18 @@
 /*
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements.  See the NOTICE file distributed with
+ *   this work for additional information regarding copyright ownership.
+ *   The ASF licenses this file to You under the Apache License, Version 2.0
+ *   (the "License"); you may not use this file except in compliance with
+ *   the License.  You may obtain a copy of the License at
  *
- *  * Licensed to the Apache Software Foundation (ASF) under one or more
- *  * contributor license agreements.  See the NOTICE file distributed with
- *  * this work for additional information regarding copyright ownership.
- *  * The ASF licenses this file to You under the Apache License, Version 2.0
- *  * (the "License"); you may not use this file except in compliance with
- *  * the License.  You may obtain a copy of the License at
- *  *
- *  *     http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
  */
 
@@ -33,13 +32,13 @@ public final class SoulThreadFactory implements ThreadFactory {
 
     private static final ThreadGroup THREAD_GROUP = new ThreadGroup("soul");
 
-    private static volatile boolean daemon;
+    private boolean daemon;
 
     private final String namePrefix;
 
     private SoulThreadFactory(final String namePrefix, final boolean daemon) {
         this.namePrefix = namePrefix;
-        SoulThreadFactory.daemon = daemon;
+        this.daemon = daemon;
     }
 
     /**
