5dc02fec3ea6edf6dfff0177fe7a083c86b34a8b
zhengkun <zk@kuaihuoyun.com>
2019-02-21 16:28:12 +0800

    1。修改参数为汉字报错的bug 2。修改参数为数据bug

--- soul-test/soul-test-dubbo/soul-test-dubbo-api/src/main/java/org/dromara/soul/test/dubbo/api/entity/Address.java
+++ soul-test/soul-test-dubbo/soul-test-dubbo-api/src/main/java/org/dromara/soul/test/dubbo/api/entity/Address.java
@@ -3,8 +3,10 @@ package org.dromara.soul.test.dubbo.api.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class Address {
+public class Address implements Serializable {
 
     private String province;
 }
