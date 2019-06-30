9ca2bea5cf3eb7f127b3ba735eae1145c1391790
huangxfchn <huangxfchn@163.com>
2019-06-30 13:15:56 +0800

    Abstract ZkDTO

---
+++ soul-common/src/main/java/org/dromara/soul/common/enums/ConfigGroupEnum.java
@@ -0,0 +1,47 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements.  See the NOTICE file distributed with
+ *   this work for additional information regarding copyright ownership.
+ *   The ASF licenses this file to You under the Apache License, Version 2.0
+ *   (the "License"); you may not use this file except in compliance with
+ *   the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.dromara.soul.common.enums;
+
+/**
+ * config group
+ * @author huangxiaofeng
+ * @date 2019/6/29 23:03
+ */
+public enum ConfigGroupEnum {
+
+    /**
+     * {@link org.dromara.soul.common.dto.AppAuthData AppAuthData} Group
+     */
+    APP_AUTH,
+
+    /**
+     * {@link org.dromara.soul.common.dto.PluginData PluginData} Group
+     */
+    PLUGIN,
+
+    /**
+     * {@link org.dromara.soul.common.dto.RuleData RuleData} Group
+     */
+    RULE,
+
+    /**
+     * {@link org.dromara.soul.common.dto.SelectorData SelectorData} Group
+     */
+    SELECTOR
+
+}
