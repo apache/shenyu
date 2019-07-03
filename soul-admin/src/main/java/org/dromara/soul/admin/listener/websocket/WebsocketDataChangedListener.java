97876b6bd74f558fd3d41d82da6ac1ca56fae483
xiaoyu <549477611@qq.com>
2019-07-03 19:42:32 +0800

    websocket

--- soul-admin/src/main/java/org/dromara/soul/admin/listener/websocket/WebsocketDataChangedListener.java
+++ soul-admin/src/main/java/org/dromara/soul/admin/listener/websocket/WebsocketDataChangedListener.java
@@ -6,21 +6,58 @@ import org.dromara.soul.common.dto.AppAuthData;
 import org.dromara.soul.common.dto.PluginData;
 import org.dromara.soul.common.dto.RuleData;
 import org.dromara.soul.common.dto.SelectorData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * The type Websocket data changed listener.
  *
  * @author xiaoyu(Myth)
  */
+@ServerEndpoint("/websocket")
 public class WebsocketDataChangedListener implements DataChangedListener {
 
-    /**
-     * Instantiates a new Websocket data changed listener.
-     */
-    public WebsocketDataChangedListener() {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebsocketDataChangedListener.class);
 
+    private static final Set<Session> SESSION_MAP = new HashSet<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        SESSION_MAP.add(session);
+        send("Hello, connection opened!");
+    }
+
+    @OnMessage
+    public void onMessage(final String message, final Session session) {
+        for (Session entry : SESSION_MAP) {
+            LOGGER.info("session.id={}", session.getId());
+
+            break;
+        }
+        LOGGER.info("接收到消息：{}", message);
+
+    }
+
+    @OnClose
+    public void onClose(final Session session) {
+        SESSION_MAP.remove(session);
+    }
+
+    @OnError
+    public void onError(final Session session, final Throwable error) {
+        SESSION_MAP.remove(session);
+        LOGGER.error("websocket collection error:{}", error);
     }
 
     @Override
@@ -41,6 +78,16 @@ public class WebsocketDataChangedListener implements DataChangedListener {
     @Override
     public void onAppAuthChanged(final List<AppAuthData> changed, final DataEventType eventType) {
 
+    }
+
+    private void send(final String message) {
+        for (Session session : SESSION_MAP) {
+            try {
+                session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                LOGGER.error("websocket send exception:{}", e);
+            }
+        }
     }
 
 }
