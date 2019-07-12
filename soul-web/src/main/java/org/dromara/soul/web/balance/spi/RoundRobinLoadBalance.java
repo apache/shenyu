f3fa60108d170e363f66c27a8d61cefbeae1f8b1
wjl4107336 <wjlcoder@outlook.com>
2019-07-12 18:41:24 +0800

    fix round robin load balance

--- soul-web/src/main/java/org/dromara/soul/web/balance/spi/RoundRobinLoadBalance.java
+++ soul-web/src/main/java/org/dromara/soul/web/balance/spi/RoundRobinLoadBalance.java
@@ -23,21 +23,20 @@ import org.dromara.soul.common.dto.convert.DivideUpstream;
 import org.dromara.soul.common.enums.LoadBalanceEnum;
 import org.dromara.soul.web.balance.LoadBalance;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.stream.Collectors;
 
 /**
  * RoundRobin  LoadBalance Impl.
  *
- * @author xiaoyu(Myth)
+ * @author wanglaomo
  */
 public class RoundRobinLoadBalance implements LoadBalance {
 
     private final ConcurrentMap<String, AtomicPositiveInteger> sequences = new ConcurrentHashMap<>();
-
-    private final ConcurrentMap<String, AtomicPositiveInteger> weightSequences = new ConcurrentHashMap<>();
 
     @Override
     public DivideUpstream select(final List<DivideUpstream> upstreamList, final String ip) {
@@ -49,40 +48,45 @@ public class RoundRobinLoadBalance implements LoadBalance {
         int maxWeight = 0;
         // 最小权重
         int minWeight = Integer.MAX_VALUE;
+        // 权重总值
+        int weightSum = 0;
+        final LinkedHashMap<DivideUpstream, IntegerWrapper> weightMap = new LinkedHashMap<>();
         for (int i = 0; i < length; i++) {
             int weight = resultUpstreamList.get(i).getWeight();
             // 累计最大权重
             maxWeight = Math.max(maxWeight, weight);
             // 累计最小权重
             minWeight = Math.min(minWeight, weight);
-        }
-        // 权重不一样
-        if (maxWeight > 0 && minWeight < maxWeight) {
-            AtomicPositiveInteger weightSequence = weightSequences.get(key);
-            if (weightSequence == null) {
-                weightSequences.putIfAbsent(key, new AtomicPositiveInteger());
-                weightSequence = weightSequences.get(key);
+            if(weight > 0) {
+                weightMap.put(resultUpstreamList.get(i), new IntegerWrapper(weight));
+                weightSum += weight;
             }
-            int currentWeight = weightSequence.getAndIncrement() % maxWeight;
-            //筛选权重大于当前权重基数的Invoker
+        }
 
-            final List<DivideUpstream> weightInvokers =
-                    resultUpstreamList.stream()
-                            .filter(divideHandle -> divideHandle.getWeight() > currentWeight)
-                            .collect(Collectors.toList());
-            int weightLength = weightInvokers.size();
-            if (weightLength == 1) {
-                return weightInvokers.get(0);
-            } else if (weightLength > 1) {
-                resultUpstreamList = weightInvokers;
-                length = resultUpstreamList.size();
-            }
-        }
         AtomicPositiveInteger sequence = sequences.get(key);
         if (sequence == null) {
             sequences.putIfAbsent(key, new AtomicPositiveInteger());
             sequence = sequences.get(key);
         }
+        int currentSequence = sequence.getAndIncrement();
+        // 权重不一样
+        if (maxWeight > 0 && minWeight < maxWeight) {
+            int mod = currentSequence % weightSum;
+            for (int i = 0; i < maxWeight; i++) {
+                for(Map.Entry<DivideUpstream, IntegerWrapper> entry : weightMap.entrySet()) {
+                    final DivideUpstream divideUpstream = entry.getKey();
+                    IntegerWrapper weight = entry.getValue();
+                    if(mod == 0 && weight.getValue() > 0) {
+                        return divideUpstream;
+                    }
+                    if(weight.getValue() > 0) {
+                        weight.decrement();
+                        mod--;
+                    }
+                }
+            }
+        }
+
         // 取模轮循
         return resultUpstreamList.get(sequence.getAndIncrement() % length);
     }
@@ -95,5 +99,25 @@ public class RoundRobinLoadBalance implements LoadBalance {
     @Override
     public String algorithm() {
         return LoadBalanceEnum.ROUND_ROBIN.getName();
+    }
+
+    private static final class IntegerWrapper {
+        private int value;
+
+        public IntegerWrapper(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public void decrement() {
+            this.value--;
+        }
     }
 }
