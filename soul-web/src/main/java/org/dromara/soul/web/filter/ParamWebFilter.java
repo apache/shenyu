bd5bbc216a04fbfa9296c4385eaf01a25c03eb58
lanjw <1079199821@qq.com>
2019-07-19 19:20:53 +0800

    Monitor增加请求执行时间

--- soul-web/src/main/java/org/dromara/soul/web/filter/ParamWebFilter.java
+++ soul-web/src/main/java/org/dromara/soul/web/filter/ParamWebFilter.java
@@ -32,6 +32,7 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+
 import java.util.Objects;
 
 /**
@@ -68,7 +69,6 @@ public class ParamWebFilter extends AbstractWebFilter {
 
     private Boolean verify(final RequestDTO requestDTO, final ServerWebExchange exchange) {
         if (Objects.isNull(requestDTO)
-                || StringUtils.isBlank(requestDTO.getModule())
                 || StringUtils.isBlank(requestDTO.getMethod())) {
             return false;
         }
