f297987fcc9a634d0428ec21e906c44ddb8564b2
lanjw <1079199821@qq.com>
2019-07-22 18:54:25 +0800

    调整 HttpCommand fallback Resposne的状态码

--- soul-web/src/main/java/org/dromara/soul/web/plugin/hystrix/HttpCommand.java
+++ soul-web/src/main/java/org/dromara/soul/web/plugin/hystrix/HttpCommand.java
@@ -224,8 +224,8 @@ public class HttpCommand extends HystrixObservableCommand<Void> {
             }
         } else if (exception instanceof HystrixTimeoutException) {
             exchange.getResponse().setStatusCode(HttpStatus.GATEWAY_TIMEOUT);
-        }
-        exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        } else
+            exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
         final SoulResult error = SoulResult.error(Constants.HTTP_ERROR_RESULT);
         return exchange.getResponse().writeWith(Mono.just(exchange.getResponse()
                 .bufferFactory().wrap(Objects.requireNonNull(JsonUtils.toJson(error)).getBytes())));
