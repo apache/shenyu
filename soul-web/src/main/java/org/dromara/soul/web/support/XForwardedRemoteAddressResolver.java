f05cec4f2c6a0ea335cf71a54be9b864e02310fc
xiaoyu <xy199195>
2019-12-11 14:52:18 +0800

    2.1.1-RELEASE

--- soul-web/src/main/java/org/dromara/soul/web/support/XForwardedRemoteAddressResolver.java
+++ soul-web/src/main/java/org/dromara/soul/web/support/XForwardedRemoteAddressResolver.java
@@ -82,16 +82,6 @@ public class XForwardedRemoteAddressResolver implements RemoteAddressResolver {
      * should use a value of `1` for the `maxTrustedIndex`.
      * Given the X-Forwarded-For value of [0.0.0.1, 0.0.0.2, 0.0.0.3]:
      *
-     * <pre>
-     * maxTrustedIndex -> result
-     *
-     * [MIN_VALUE,0] -> IllegalArgumentException
-     * 1 -> 0.0.0.3
-     * 2 -> 0.0.0.2
-     * 3 -> 0.0.0.1
-     * [4, MAX_VALUE] -> 0.0.0.1
-     * </pre>
-     *
      * @param maxTrustedIndex correlates to the number of trusted proxies expected in
      *                        front of Spring Cloud Gateway (index starts at 1).
      * @return a {@link XForwardedRemoteAddressResolver} which extracts the last
