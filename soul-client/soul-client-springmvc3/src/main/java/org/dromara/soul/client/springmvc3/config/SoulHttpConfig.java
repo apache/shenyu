987a71a544423fef13f9b450e0cdfa9b0b89b106
yangpf <muyieyueyueniaofei@163.com>
2020-03-13 22:10:05 +0800

    添加SpringBoot 1.5.x系列的支持，Spring 5以下的版本 即将 WebServerInitializedEvent 替换为 EmbeddedServletContainerInitializedEvent 同时通过@Value获取端口，同时删除只有Spring5中才有的@NonNull注解

---
+++ soul-client/soul-client-springmvc3/src/main/java/org/dromara/soul/client/springmvc3/config/SoulHttpConfig.java
@@ -0,0 +1,22 @@
+package org.dromara.soul.client.springmvc3.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * The type Soul http config.
+ *
+ * @author xiaoyu
+ */
+@Data
+@ConfigurationProperties(prefix = "soul.http")
+public class SoulHttpConfig {
+
+    private String adminUrl;
+
+    private String zookeeperUrl;
+
+    private String contextPath;
+
+    private String appName;
+}
