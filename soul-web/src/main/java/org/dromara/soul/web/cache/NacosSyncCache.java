bff2f2ccc0b9eebff3f12eaea7554825ac185d05
xiaoyu <549477611@qq.com>
2020-04-13 10:44:59 +0800

    modify

--- soul-web/src/main/java/org/dromara/soul/web/cache/NacosSyncCache.java
+++ soul-web/src/main/java/org/dromara/soul/web/cache/NacosSyncCache.java
@@ -8,32 +8,31 @@ import com.google.gson.Gson;
 
 /**
  * NacosSyncCache
- * 
+ *
  * @author Chenxj
- * @date 2020年3月12日-下午1:49:55
  */
 public class NacosSyncCache extends NacosCacheHandler implements CommandLineRunner, DisposableBean {
-
-	public NacosSyncCache(final ConfigService configService) {
-		super(configService);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Gson gson=new Gson();
-		watcherData(authDataId,changeData->updateAuthMap(gson, changeData, e->publishConfig(gson, authDataId, AUTH_MAP)));
-		watcherData(pluginDataId, changeData->updatePluginMap(gson, changeData, e->publishConfig(gson, pluginDataId, PLUGIN_MAP)));
-		watcherData(selectorDataId, changeData->updateSelectorMap(gson, changeData, e->publishConfig(gson, selectorDataId, SELECTOR_MAP)));
-		watcherData(metaDataId, changeData->updateMetaDataMap(gson, changeData, e->publishConfig(gson, metaDataId, META_DATA)));
-		watcherData(ruleDataId, changeData->updateRuleMap(gson, changeData, e->publishConfig(gson, ruleDataId, RULE_MAP)));
-	}
-	
-	@Override
-	public void destroy() throws Exception {
-		listeners.forEach((dataId,lss)->{
-			lss.stream().forEach(listener->configService.removeListener(dataId, group, listener));
-			lss.clear();
-		});
-		listeners.clear();
-	}
+    
+    public NacosSyncCache(final ConfigService configService) {
+        super(configService);
+    }
+    
+    @Override
+    public void run(String... args) {
+        Gson gson = new Gson();
+        watcherData(authDataId, changeData -> updateAuthMap(gson, changeData, e -> publishConfig(gson, authDataId, AUTH_MAP)));
+        watcherData(pluginDataId, changeData -> updatePluginMap(gson, changeData, e -> publishConfig(gson, pluginDataId, PLUGIN_MAP)));
+        watcherData(selectorDataId, changeData -> updateSelectorMap(gson, changeData, e -> publishConfig(gson, selectorDataId, SELECTOR_MAP)));
+        watcherData(metaDataId, changeData -> updateMetaDataMap(gson, changeData, e -> publishConfig(gson, metaDataId, META_DATA)));
+        watcherData(ruleDataId, changeData -> updateRuleMap(gson, changeData, e -> publishConfig(gson, ruleDataId, RULE_MAP)));
+    }
+    
+    @Override
+    public void destroy() {
+        listeners.forEach((dataId, lss) -> {
+            lss.forEach(listener -> configService.removeListener(dataId, group, listener));
+            lss.clear();
+        });
+        listeners.clear();
+    }
 }
