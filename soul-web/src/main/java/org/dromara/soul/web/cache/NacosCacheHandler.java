778fd09a96e91ebd2dc0b3f5595478c614a4bfc0
xiaoyu <549477611@qq.com>
2020-04-13 10:44:59 +0800

    modify

--- soul-web/src/main/java/org/dromara/soul/web/cache/NacosCacheHandler.java
+++ soul-web/src/main/java/org/dromara/soul/web/cache/NacosCacheHandler.java
@@ -24,157 +24,177 @@ import com.google.gson.JsonObject;
 
 /**
  * NacosCacheHandler
- * 
- * @author Chenxj
- * @date 2020年3月12日-下午2:07:12
+ *
+ * @author Chenxjx
+ * @author xiaoyu
  */
 public class NacosCacheHandler extends CommonCacheHandler {
-	protected static final String group="DEFAULT_GROUP";
-	protected static final String pluginDataId="soul.plugin.json";
-	protected static final String selectorDataId="soul.selector.json";
-	protected static final String ruleDataId="soul.rule.json";
-	protected static final String authDataId="soul.auth.json";
-	protected static final String metaDataId="soul.meta.json";
-	protected final ConfigService configService;
-	protected final Map<String, List<Listener>>listeners=Maps.newConcurrentMap();
 	
-	public NacosCacheHandler(final ConfigService configService) {
-		this.configService=configService;
-	}
-	protected void updateAuthMap(Gson gson,String configInfo,ErrorAction ea) {
-		try {
-			JsonObject jo=gson.fromJson(configInfo, JsonObject.class);
-			Set<String>set=new HashSet<>(AUTH_MAP.keySet());
-			for(Entry<String, JsonElement>e:jo.entrySet()) {
-				set.remove(e.getKey());
-				AUTH_MAP.put(e.getKey(), gson.fromJson(e.getValue(), AppAuthData.class));
-			}
-			AUTH_MAP.keySet().removeAll(set);
-		}catch (Exception e) {
-			if(ea!=null) {
-				ea.act(e);
-			}
-		}
-	}
-	protected void updatePluginMap(Gson gson,String configInfo,ErrorAction ea) {
-		try {
-			JsonObject jo=gson.fromJson(configInfo, JsonObject.class);
-			Set<String>set=new HashSet<>(PLUGIN_MAP.keySet());
-			for(Entry<String, JsonElement>e:jo.entrySet()) {
-				set.remove(e.getKey());
-				PluginData pluginData=gson.fromJson(e.getValue(), PluginData.class);
-				configPlugin(pluginData);
-				PLUGIN_MAP.put(e.getKey(), pluginData);
-			}
-			PLUGIN_MAP.keySet().removeAll(set);
-		}catch (Exception e) {
-			if(ea!=null) {
-				ea.act(e);
-			}
-		}
-	}
-	protected void updateSelectorMap(Gson gson,String configInfo,ErrorAction ea) {
-		try {
-			JsonObject jo=gson.fromJson(configInfo, JsonObject.class);
-			Set<String>set=new HashSet<>(SELECTOR_MAP.keySet());
-			for(Entry<String, JsonElement>e:jo.entrySet()) {
-				set.remove(e.getKey());
-				List<SelectorData>ls=new ArrayList<>();
-				e.getValue().getAsJsonArray().forEach(je->ls.add(gson.fromJson(je, SelectorData.class)));
-				SELECTOR_MAP.put(e.getKey(), ls);
-			}
-			SELECTOR_MAP.keySet().removeAll(set);
-		}catch (Exception e) {
-			if(ea!=null) {
-				ea.act(e);
-			}
-		}
-	}
-	protected void updateMetaDataMap(Gson gson,String configInfo,ErrorAction ea) {
-		try {
-			JsonObject jo=gson.fromJson(configInfo, JsonObject.class);
-			Set<String>set=new HashSet<>(META_DATA.keySet());
-			for(Entry<String, JsonElement>e:jo.entrySet()) {
-				set.remove(e.getKey());
-				MetaData metaData=gson.fromJson(e.getValue(), MetaData.class);
-				initDubboRef(metaData);
-				META_DATA.put(e.getKey(), metaData);
-			}
-			if(!set.isEmpty()) {
-				META_DATA.keySet().removeAll(set);
-				set.stream().forEach(key->ApplicationConfigCache.getInstance()
-						.invalidate(META_DATA.get(key).getServiceName()));
-			}
-		}catch (Exception e) {
-			if(ea!=null) {
-				ea.act(e);
-			}
-		}
-	}
-	protected void updateRuleMap(Gson gson,String configInfo,ErrorAction ea) {
-		try {
-			JsonObject jo=gson.fromJson(configInfo, JsonObject.class);
-			Set<String>set=new HashSet<>(RULE_MAP.keySet());
-			for(Entry<String, JsonElement>e:jo.entrySet()) {
-				set.remove(e.getKey());
-				List<RuleData>ls=new ArrayList<>();
-				e.getValue().getAsJsonArray().forEach(je->ls.add(gson.fromJson(je, RuleData.class)));
-				RULE_MAP.put(e.getKey(), ls);
-			}
-			RULE_MAP.keySet().removeAll(set);
-		}catch (Exception e) {
-			if(ea!=null) {
-				ea.act(e);
-			}
-		}
-	}
-	
-	protected String getConfig(String dataId) {
-		try {
-			return configService.getConfig(dataId, group, 6000);
-		}catch (Exception e) {
-			return "{}";
-		}
-	}
-	protected String getConfigAndSignListener(String dataId,Listener listener) {
-		try {
-			return configService.getConfigAndSignListener(dataId, group, 6000, listener);
-		}catch (Exception e) {
-			return "{}";
-		}
-	}
-	protected void publishConfig(Gson gson,String dataId,Object data) {
-		try {
-			configService.publishConfig(dataId, group, gson.toJson(data));
-		}catch (Exception e) {
-		}
-	}
-	protected String buildMetaKey(MetaData meta) {
-		return meta.getAppName()+'-'+meta.getServiceName()+meta.getMethodName();
-	}
-	protected void watcherData(String dataId,OnChange oc) {
-		try {
-			Listener listener=new Listener() {
-				@Override
-				public void receiveConfigInfo(String configInfo) {
-					oc.change(configInfo);
-				}
-				@Override
-				public Executor getExecutor() {
-					return null;
-				}
-			};
-			oc.change(getConfigAndSignListener(dataId, listener));
-			listeners
-			.getOrDefault(dataId, new ArrayList<>())
-			.add(listener);
-		}catch (Exception e) {
-		}
-	}
-	protected static interface ErrorAction{
-		public void act(Object o);
-	}
-	protected static interface OnChange{
-		public void change(String changeData);
-	}
+    protected static final String group = "DEFAULT_GROUP";
+    
+    protected static final String pluginDataId = "soul.plugin.json";
+    
+    protected static final String selectorDataId = "soul.selector.json";
+    
+    protected static final String ruleDataId = "soul.rule.json";
+    
+    protected static final String authDataId = "soul.auth.json";
+    
+    protected static final String metaDataId = "soul.meta.json";
+    
+    protected final ConfigService configService;
+    
+    protected final Map<String, List<Listener>> listeners = Maps.newConcurrentMap();
+    
+    public NacosCacheHandler(final ConfigService configService) {
+        this.configService = configService;
+    }
+    
+    protected void updateAuthMap(Gson gson, String configInfo, ErrorAction ea) {
+        try {
+            JsonObject jo = gson.fromJson(configInfo, JsonObject.class);
+            Set<String> set = new HashSet<>(AUTH_MAP.keySet());
+            for (Entry<String, JsonElement> e : jo.entrySet()) {
+                set.remove(e.getKey());
+                AUTH_MAP.put(e.getKey(), gson.fromJson(e.getValue(), AppAuthData.class));
+            }
+            AUTH_MAP.keySet().removeAll(set);
+        } catch (Exception e) {
+            if (ea != null) {
+                ea.act(e);
+            }
+        }
+    }
+    
+    protected void updatePluginMap(Gson gson, String configInfo, ErrorAction ea) {
+        try {
+            JsonObject jo = gson.fromJson(configInfo, JsonObject.class);
+            Set<String> set = new HashSet<>(PLUGIN_MAP.keySet());
+            for (Entry<String, JsonElement> e : jo.entrySet()) {
+                set.remove(e.getKey());
+                PluginData pluginData = gson.fromJson(e.getValue(), PluginData.class);
+                configPlugin(pluginData);
+                PLUGIN_MAP.put(e.getKey(), pluginData);
+            }
+            PLUGIN_MAP.keySet().removeAll(set);
+        } catch (Exception e) {
+            if (ea != null) {
+                ea.act(e);
+            }
+        }
+    }
+    
+    protected void updateSelectorMap(Gson gson, String configInfo, ErrorAction ea) {
+        try {
+            JsonObject jo = gson.fromJson(configInfo, JsonObject.class);
+            Set<String> set = new HashSet<>(SELECTOR_MAP.keySet());
+            for (Entry<String, JsonElement> e : jo.entrySet()) {
+                set.remove(e.getKey());
+                List<SelectorData> selectorDataList = new ArrayList<>();
+                e.getValue().getAsJsonArray().forEach(je -> selectorDataList.add(gson.fromJson(je, SelectorData.class)));
+                SELECTOR_MAP.put(e.getKey(), selectorDataList);
+                selectorDataList.forEach(UpstreamCacheManager::submit);
+            }
+            SELECTOR_MAP.keySet().removeAll(set);
+        } catch (Exception e) {
+            if (ea != null) {
+                ea.act(e);
+            }
+        }
+    }
+    
+    protected void updateMetaDataMap(Gson gson, String configInfo, ErrorAction ea) {
+        try {
+            JsonObject jo = gson.fromJson(configInfo, JsonObject.class);
+            Set<String> set = new HashSet<>(META_DATA.keySet());
+            for (Entry<String, JsonElement> e : jo.entrySet()) {
+                set.remove(e.getKey());
+                MetaData metaData = gson.fromJson(e.getValue(), MetaData.class);
+                initDubboRef(metaData);
+                META_DATA.put(e.getKey(), metaData);
+            }
+            if (!set.isEmpty()) {
+                META_DATA.keySet().removeAll(set);
+                set.forEach(key -> ApplicationConfigCache.getInstance().invalidate(META_DATA.get(key).getServiceName()));
+            }
+        } catch (Exception e) {
+            if (ea != null) {
+                ea.act(e);
+            }
+        }
+    }
+    
+    protected void updateRuleMap(Gson gson, String configInfo, ErrorAction ea) {
+        try {
+            JsonObject jo = gson.fromJson(configInfo, JsonObject.class);
+            Set<String> set = new HashSet<>(RULE_MAP.keySet());
+            for (Entry<String, JsonElement> e : jo.entrySet()) {
+                set.remove(e.getKey());
+                List<RuleData> ls = new ArrayList<>();
+                e.getValue().getAsJsonArray().forEach(je -> ls.add(gson.fromJson(je, RuleData.class)));
+                RULE_MAP.put(e.getKey(), ls);
+            }
+            RULE_MAP.keySet().removeAll(set);
+        } catch (Exception e) {
+            if (ea != null) {
+                ea.act(e);
+            }
+        }
+    }
+    
+    protected String getConfig(String dataId) {
+        try {
+            return configService.getConfig(dataId, group, 6000);
+        } catch (Exception e) {
+            return "{}";
+        }
+    }
+    
+    protected String getConfigAndSignListener(String dataId, Listener listener) {
+        try {
+            return configService.getConfigAndSignListener(dataId, group, 6000, listener);
+        } catch (Exception e) {
+            return "{}";
+        }
+    }
+    
+    protected void publishConfig(Gson gson, String dataId, Object data) {
+        try {
+            configService.publishConfig(dataId, group, gson.toJson(data));
+        } catch (Exception e) {
+        }
+    }
+    
+    protected String buildMetaKey(MetaData meta) {
+        return meta.getAppName() + '-' + meta.getServiceName() + meta.getMethodName();
+    }
+    
+    protected void watcherData(String dataId, OnChange oc) {
+        try {
+            Listener listener = new Listener() {
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    oc.change(configInfo);
+                }
+                
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+            };
+            oc.change(getConfigAndSignListener(dataId, listener));
+            listeners
+                    .getOrDefault(dataId, new ArrayList<>())
+                    .add(listener);
+        } catch (Exception e) {
+        }
+    }
+    
+    protected interface ErrorAction {
+        void act(Object o);
+    }
+    
+    protected interface OnChange {
+        void change(String changeData);
+    }
 }
--- soul-web/src/main/java/org/dromara/soul/web/plugin/dubbo/DefaultGenericParamResolveServiceImpl.java
+++ soul-web/src/main/java/org/dromara/soul/web/plugin/dubbo/DefaultGenericParamResolveServiceImpl.java
@@ -36,55 +36,55 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author xiaoyu
  */
 public class DefaultGenericParamResolveServiceImpl implements GenericParamResolveService {
-	private Logger log = LoggerFactory.getLogger(getClass());
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public Pair<String[], Object[]> buildParameter(final String body, final String parameterTypes) {
-		ObjectMapper om = new ObjectMapper();
-		String[] ptypes = splitParameterTypes(parameterTypes);
-		Object[] params = new Object[ptypes.length];
-		if (ptypes.length > 0) {
-			try {
-				Object o = om.readValue(body, Object.class);
-				if (o instanceof List) {
-					((List<Object>) o)
-					.stream()
-					.limit(ptypes.length)
-					.collect(Collectors.toList())
-					.toArray(params);
-				} else {
-					params[0] = o;
-				}
-			} catch (Exception e) {
-				log.warn("body[{}] parsing failed cause by[{}]", body, e.getMessage());
-			}
-		}
-		return new ImmutablePair<>(ptypes, params);
-	}
-	
-	private static String[] splitParameterTypes(String parameterTypes) {
-		if(parameterTypes.length()==0) {
-			return new String[0];
-		}
-		List<String>ls=new ArrayList<>();
-		int pL=parameterTypes.length();
-		int i=0,j=-1;
-		for(;i<pL;i++) {
-			if(parameterTypes.charAt(i)==','&&i>j) {
-				if(i!=j+1) {
-					ls.add(parameterTypes.substring(j+1, i));
-				}
-				j=i;
-			}
-		}
-		if(i>j) {
-			if(i!=j+1) {
-				ls.add(parameterTypes.substring(j+1, i));
-			}
-		}
-		String[]ss=new String[ls.size()];
-		ls.toArray(ss);
-		return ss;
-	}
+    private Logger log = LoggerFactory.getLogger(getClass());
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public Pair<String[], Object[]> buildParameter(final String body, final String parameterTypes) {
+        ObjectMapper om = new ObjectMapper();
+        String[] ptypes = splitParameterTypes(parameterTypes);
+        Object[] params = new Object[ptypes.length];
+        if (ptypes.length > 0) {
+            try {
+                Object o = om.readValue(body, Object.class);
+                if (o instanceof List) {
+                    ((List<Object>) o)
+                            .stream()
+                            .limit(ptypes.length)
+                            .collect(Collectors.toList())
+                            .toArray(params);
+                } else {
+                    params[0] = o;
+                }
+            } catch (Exception e) {
+                log.warn("body[{}] parsing failed cause by[{}]", body, e.getMessage());
+            }
+        }
+        return new ImmutablePair<>(ptypes, params);
+    }
+    
+    private static String[] splitParameterTypes(String parameterTypes) {
+        if (parameterTypes.length() == 0) {
+            return new String[0];
+        }
+        List<String> ls = new ArrayList<>();
+        int pL = parameterTypes.length();
+        int i = 0, j = -1;
+        for (; i < pL; i++) {
+            if (parameterTypes.charAt(i) == ',' && i > j) {
+                if (i != j + 1) {
+                    ls.add(parameterTypes.substring(j + 1, i));
+                }
+                j = i;
+            }
+        }
+        if (i > j) {
+            if (i != j + 1) {
+                ls.add(parameterTypes.substring(j + 1, i));
+            }
+        }
+        String[] ss = new String[ls.size()];
+        ls.toArray(ss);
+        return ss;
+    }
 }
