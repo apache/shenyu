d3056b5d094ac9094b4a10154a9358926c2e3900
Zhang Yonglun <zhangyonglun@apache.org>
2020-09-01 22:51:50 +0800

    fix checkstyle

--- soul-web/src/main/java/org/dromara/soul/web/filter/TimeWebFilter.java
+++ soul-web/src/main/java/org/dromara/soul/web/filter/TimeWebFilter.java
@@ -32,6 +32,7 @@ import reactor.core.publisher.Mono;
  * this is visit time verify filter.
  *
  * @author xiaoyu(Myth)
+ * @deprecated do not use
  */
 @Deprecated
 public class TimeWebFilter extends AbstractWebFilter {
