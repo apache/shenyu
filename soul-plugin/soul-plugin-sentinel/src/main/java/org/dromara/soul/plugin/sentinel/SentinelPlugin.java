21b426c0ca4c4e5aa11955785a913aabb199e2cf
汤煜冬 <583125614@qq.com>
2020-11-10 10:27:09 +0800

    add sentinel plugin (#331)

---
+++ soul-plugin/soul-plugin-sentinel/src/main/java/org/dromara/soul/plugin/sentinel/SentinelPlugin.java
@@ -0,0 +1,59 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements.  See the NOTICE file distributed with
+ *   this work for additional information regarding copyright ownership.
+ *   The ASF licenses this file to You under the Apache License, Version 2.0
+ *   (the "License"); you may not use this file except in compliance with
+ *   the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package org.dromara.soul.plugin.sentinel;
+
+import com.alibaba.csp.sentinel.adapter.reactor.SentinelReactorTransformer;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.soul.common.constant.Constants;
+import org.dromara.soul.common.dto.RuleData;
+import org.dromara.soul.common.dto.SelectorData;
+import org.dromara.soul.common.enums.PluginEnum;
+import org.dromara.soul.plugin.api.SoulPluginChain;
+import org.dromara.soul.plugin.api.context.SoulContext;
+import org.dromara.soul.plugin.base.AbstractSoulPlugin;
+import org.dromara.soul.plugin.sentinel.handler.SentinelRuleHandle;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Sentinel Plugin.
+ *
+ * @author tydhot
+ */
+@Slf4j
+public class SentinelPlugin extends AbstractSoulPlugin {
+
+    @Override
+    protected Mono<Void> doExecute(final ServerWebExchange exchange, final SoulPluginChain chain, final SelectorData selector, final RuleData rule) {
+        final SoulContext soulContext = exchange.getAttribute(Constants.CONTEXT);
+        assert soulContext != null;
+        String resourceName = SentinelRuleHandle.getResourceName(rule);
+        return chain.execute(exchange).transform(new SentinelReactorTransformer<>(resourceName));
+    }
+
+    @Override
+    public String named() {
+        return PluginEnum.SENTINEL.getName();
+    }
+
+    @Override
+    public int getOrder() {
+        return PluginEnum.SENTINEL.getCode();
+    }
+}
