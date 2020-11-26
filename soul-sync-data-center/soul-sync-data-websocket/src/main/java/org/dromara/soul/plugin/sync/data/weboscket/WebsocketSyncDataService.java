768f90ba5ca013a18191e36a97b75156fc7a4453
dengliming <liming.d.pro@gmail.com>
2020-11-26 13:00:25 +0800

    Fix all log error print

--- soul-sync-data-center/soul-sync-data-websocket/src/main/java/org/dromara/soul/plugin/sync/data/weboscket/WebsocketSyncDataService.java
+++ soul-sync-data-center/soul-sync-data-websocket/src/main/java/org/dromara/soul/plugin/sync/data/weboscket/WebsocketSyncDataService.java
@@ -63,7 +63,7 @@ public class WebsocketSyncDataService implements SyncDataService, AutoCloseable 
             try {
                 clients.add(new SoulWebsocketClient(new URI(url), pluginDataSubscriber, metaDataSubscribers, authDataSubscribers));
             } catch (URISyntaxException e) {
-                log.error("websocket url is error :", e);
+                log.error("websocket url({}) is error", url, e);
             }
         }
 
