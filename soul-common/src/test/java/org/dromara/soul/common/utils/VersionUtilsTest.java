0130163cd6bc44a1135febff85ca5561169cf6aa
kuan.lin <kuan.lin@mobilemd.cn>
2020-12-03 17:30:45 +0800

    git commit -m 'build VersionUtilsTest for VersionUtils.java'

--- soul-common/src/test/java/org/dromara/soul/common/utils/VersionUtilsTest.java
+++ soul-common/src/test/java/org/dromara/soul/common/utils/VersionUtilsTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.dromara.soul.common.utils;
 
 import org.junit.Test;
@@ -22,22 +39,11 @@ public class VersionUtilsTest {
     }
 
     /**
-     * cover version from defaultVersion
-     */
-    @Test
-    public void testFromDefault(){
-        String version = VersionUtils.getVersion(Version.class, "2.0.0");
-        System.out.println(version);
-        assertNotNull(version);
-    }
-
-    /**
      * cover find version info from MANIFEST.MF
      */
     @Test
     public void testFromImplementationVersion()throws ClassNotFoundException{
         String version = VersionUtils.getVersion(Class.forName("java.lang.String"), "2.0.2");
-        System.out.println(version);
         assertNotNull(version);
     }
 
@@ -47,14 +53,7 @@ public class VersionUtilsTest {
     @Test
     public void testFromCodeSource()throws ClassNotFoundException{
         String version = VersionUtils.getVersion(Class.forName("ch.qos.logback.classic.db.DBAppender"), "2.0.2");
-        System.out.println(version);
         assertNotNull(version);
     }
-
-    static class Version{
-
-
-    }
-
 
 }
