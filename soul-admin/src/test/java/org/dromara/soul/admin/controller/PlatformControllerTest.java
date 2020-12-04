4120a9dcab23c4868ee5f18bd6b6365a8144a180
wangwenjie <13716689854@163.com>
2020-12-04 15:40:36 +0800

    * update mock for PlatformController

--- soul-admin/src/test/java/org/dromara/soul/admin/controller/PlatformControllerTest.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/controller/PlatformControllerTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.dromara.soul.admin.controller;
 
 import org.dromara.soul.admin.service.DashboardUserService;
@@ -26,7 +43,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 /**
- * test case for PlatformController
+ * test case for PlatformController.
  *
  * @author wangwenjie
  */
@@ -45,13 +62,13 @@ public class PlatformControllerTest {
     private EnumService enumService;
 
     /**
-     * loginDashboardUser mock data
+     * loginDashboardUser mock data.
      */
     private final DashboardUserVO dashboardUserVO = new DashboardUserVO("1", "admin", "123456", 1, true,
             DateUtils.localDateTimeToString(LocalDateTime.now()), DateUtils.localDateTimeToString(LocalDateTime.now()));
 
     /**
-     * init mockmvc
+     * init mockmvc.
      */
     @Before
     public void setUp() {
@@ -59,7 +76,7 @@ public class PlatformControllerTest {
     }
 
     /**
-     * test method loginDashboardUser
+     * test method loginDashboardUser.
      */
     @Test
     public void testLoginDashboardUser() throws Exception {
@@ -75,7 +92,7 @@ public class PlatformControllerTest {
     }
 
     /**
-     * test method queryEnums
+     * test method queryEnums.
      */
     @Test
     public void testQueryEnums() throws Exception {
