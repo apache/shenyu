f69d3a76f6de4848bfc110dbb1a331c87bf40e49
cottom <cottom.zw@outlook.com>
2020-12-05 03:12:41 +0800

    [soul-admin] fix meta constructor

--- soul-admin/src/test/java/org/dromara/soul/admin/listener/nacos/NacosMockConfigService.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/listener/nacos/NacosMockConfigService.java
@@ -20,51 +20,54 @@ package org.dromara.soul.admin.listener.nacos;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.api.exception.NacosException;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class NacosMockConfigService implements ConfigService {
 
-  private static final Map<String, String> EMPTY = new HashMap<>();
+    private static final Map<String, String> EMPTY = new HashMap<>();
 
-  private Map<String, Map<String, String>> store = new HashMap<>();
+    private Map<String, Map<String, String>> store = new HashMap<>();
 
-  @Override
-  public String getConfig(final String s, final String s1, final long l) throws NacosException {
-    return store.getOrDefault(s, EMPTY).getOrDefault(s1, "{}");
-  }
+    @Override
+    public String getConfig(final String s, final String s1, final long l) throws NacosException {
+        return store.getOrDefault(s, EMPTY).getOrDefault(s1, "{}");
+    }
 
-  @Override
-  public String getConfigAndSignListener(
-      final String s, final String s1, final long l, final Listener listener)
-      throws NacosException {
+    @Override
+    public String getConfigAndSignListener(
+            final String s, final String s1, final long l, final Listener listener)
+            throws NacosException {
 
-    return null;
-  }
+        return null;
+    }
 
-  @Override
-  public void addListener(final String s, final String s1, final Listener listener)
-      throws NacosException {}
+    @Override
+    public void addListener(final String s, final String s1, final Listener listener)
+            throws NacosException {
+    }
 
-  @Override
-  public boolean publishConfig(final String key, final String group, final String result)
-      throws NacosException {
-    Map<String, String> row = store.getOrDefault(key, new HashMap<>());
-    row.put(group, result);
-    store.put(key, row);
-    return true;
-  }
+    @Override
+    public boolean publishConfig(final String key, final String group, final String result)
+            throws NacosException {
+        Map<String, String> row = store.getOrDefault(key, new HashMap<>());
+        row.put(group, result);
+        store.put(key, row);
+        return true;
+    }
 
-  @Override
-  public boolean removeConfig(final String s, final String s1) throws NacosException {
-    return false;
-  }
+    @Override
+    public boolean removeConfig(final String s, final String s1) throws NacosException {
+        return false;
+    }
 
-  @Override
-  public void removeListener(final String s, final String s1, final Listener listener) {}
+    @Override
+    public void removeListener(final String s, final String s1, final Listener listener) {
+    }
 
-  @Override
-  public String getServerStatus() {
-    return null;
-  }
+    @Override
+    public String getServerStatus() {
+        return null;
+    }
 }
