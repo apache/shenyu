cf7fdbcb5670ffd8ab4d086e525744407a863b76
dengliming <liming.d.pro@gmail.com>
2020-12-05 14:09:42 +0800

    Update PluginHandleControllerTest.java

--- soul-admin/src/test/java/org/dromara/soul/admin/controller/PluginHandleControllerTest.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/controller/PluginHandleControllerTest.java
@@ -37,11 +37,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -50,10 +48,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Test case for PluginHandleController.
  *
- * @author ：charlesgongC
+ * @author charlesgongC
  */
 @RunWith(SpringRunner.class)
-public class PluginHandleControllerTest {
+public final class PluginHandleControllerTest {
 
     private MockMvc mockMvc;
 
