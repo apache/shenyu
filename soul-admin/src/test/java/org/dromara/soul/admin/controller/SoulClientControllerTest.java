6c1cf807766f2aecefe0766608005b859192206c
dengliming <liming.d.pro@gmail.com>
2020-12-09 22:14:36 +0800

    Delete blank line

--- soul-admin/src/test/java/org/dromara/soul/admin/controller/SoulClientControllerTest.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/controller/SoulClientControllerTest.java
@@ -32,7 +32,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
