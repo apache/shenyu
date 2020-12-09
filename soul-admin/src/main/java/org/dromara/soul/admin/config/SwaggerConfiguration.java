3f0b55c08dd16225ede9bbf157f2e429eb9e096d
xwzwmt <18800206482@163.com>
2020-12-09 09:41:28 +0800

    handle license error

--- soul-admin/src/main/java/org/dromara/soul/admin/config/SwaggerConfiguration.java
+++ soul-admin/src/main/java/org/dromara/soul/admin/config/SwaggerConfiguration.java
@@ -20,8 +20,6 @@ package org.dromara.soul.admin.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
--- soul-admin/src/test/java/org/dromara/soul/admin/config/DataSyncConfigurationTest.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/config/DataSyncConfigurationTest.java
@@ -5,20 +5,45 @@ import org.I0Itec.zkclient.ZkClient;
 import org.apache.curator.test.TestingServer;
 import org.dromara.soul.admin.AbstractConfigurationTest;
 import org.dromara.soul.admin.listener.nacos.NacosMockConfigService;
-import org.dromara.soul.admin.service.*;
+import org.dromara.soul.admin.service.MetaDataService;
+import org.dromara.soul.admin.service.PluginService;
+import org.dromara.soul.admin.service.RuleService;
+import org.dromara.soul.admin.service.SelectorService;
+import org.dromara.soul.admin.service.SyncDataService;
 import org.dromara.soul.admin.service.impl.AppAuthServiceImpl;
 import org.dromara.soul.admin.service.sync.SyncDataServiceImpl;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationEventPublisher;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import static org.junit.Assert.assertNotNull;
 
 /**
- * The TestCase for DataSyncConfiguration
+ * The TestCase for DataSyncConfiguration.
  *
  * @author xwzwmt
  */
@@ -42,7 +67,7 @@ public final class DataSyncConfigurationTest extends AbstractConfigurationTest {
     private SelectorService selectorService;
 
     @Mock
-    private RuleService ruleServicee;
+    private RuleService ruleService;
 
     @Mock
     private ApplicationEventPublisher eventPublisher;
@@ -62,45 +87,46 @@ public final class DataSyncConfigurationTest extends AbstractConfigurationTest {
 
     @Test
     public void testHttpLongPollingDataChangedListener() {
-        final HttpSyncProperties httpSyncProperties=new HttpSyncProperties();
-        DataSyncConfiguration.HttpLongPollingListener httpLongPollingListener=new DataSyncConfiguration.HttpLongPollingListener();
+        final HttpSyncProperties httpSyncProperties = new HttpSyncProperties();
+        DataSyncConfiguration.HttpLongPollingListener httpLongPollingListener = new DataSyncConfiguration.HttpLongPollingListener();
         assertNotNull(httpLongPollingListener.httpLongPollingDataChangedListener(httpSyncProperties));
     }
 
     @Test
-    public void testZookeeperDataChangedListener(){
-        DataSyncConfiguration.ZookeeperListener zookeeperListener=new DataSyncConfiguration.ZookeeperListener();
+    public void testZookeeperDataChangedListener() {
+        DataSyncConfiguration.ZookeeperListener zookeeperListener = new DataSyncConfiguration.ZookeeperListener();
         assertNotNull(zookeeperListener.zookeeperDataChangedListener(zkClient));
     }
 
     @Test
-    public void testZookeeperDataInit(){
-        final SyncDataService syncDataService=new SyncDataServiceImpl(appAuthService,pluginService,selectorService,
-                ruleServicee,eventPublisher,metaDataService);
-        DataSyncConfiguration.ZookeeperListener zookeeperListener=new DataSyncConfiguration.ZookeeperListener();
-        assertNotNull(zookeeperListener.zookeeperDataInit(zkClient,syncDataService));
+    public void testZookeeperDataInit() {
+        final SyncDataService syncDataService = new SyncDataServiceImpl(appAuthService, pluginService, selectorService,
+                ruleService, eventPublisher, metaDataService);
+        DataSyncConfiguration.ZookeeperListener zookeeperListener = new DataSyncConfiguration.ZookeeperListener();
+        assertNotNull(zookeeperListener.zookeeperDataInit(zkClient, syncDataService));
     }
 
     @Test
-    public void testNacosDataChangedListener(){
-        DataSyncConfiguration.NacosListener nacosListener=new DataSyncConfiguration.NacosListener();
+    public void testNacosDataChangedListener() {
+        DataSyncConfiguration.NacosListener nacosListener = new DataSyncConfiguration.NacosListener();
         assertNotNull(nacosListener.nacosDataChangedListener(configService));
     }
+
     @Test
-    public void testWebsocketDataChangedListener(){
-        DataSyncConfiguration.WebsocketListener websocketListener=new DataSyncConfiguration.WebsocketListener();
+    public void testWebsocketDataChangedListener() {
+        DataSyncConfiguration.WebsocketListener websocketListener = new DataSyncConfiguration.WebsocketListener();
         assertNotNull(websocketListener.websocketDataChangedListener());
     }
 
     @Test
-    public void testWebsocketCollector(){
-        DataSyncConfiguration.WebsocketListener websocketListener=new DataSyncConfiguration.WebsocketListener();
+    public void testWebsocketCollector() {
+        DataSyncConfiguration.WebsocketListener websocketListener = new DataSyncConfiguration.WebsocketListener();
         assertNotNull(websocketListener.websocketCollector());
     }
 
     @Test
-    public void testServerEndpointExporter(){
-        DataSyncConfiguration.WebsocketListener websocketListener=new DataSyncConfiguration.WebsocketListener();
+    public void testServerEndpointExporter() {
+        DataSyncConfiguration.WebsocketListener websocketListener = new DataSyncConfiguration.WebsocketListener();
         assertNotNull(websocketListener.serverEndpointExporter());
     }
 
