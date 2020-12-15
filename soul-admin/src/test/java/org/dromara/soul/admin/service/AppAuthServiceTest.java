58efafdd5ed9620810a72c7c05a3f80f91b628f4
yiwenlong <wlong.yi@gmail.com>
2020-12-16 00:59:43 +0800

    Refactor: Move the build do object method to its own classes. Try make the service simple and clean, so that it can focus on service follow.

--- soul-admin/src/test/java/org/dromara/soul/admin/service/AppAuthServiceTest.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/service/AppAuthServiceTest.java
@@ -282,7 +282,7 @@ public final class AppAuthServiceTest {
     }
 
     private AppAuthDO buildAppAuthDO() {
-        AppAuthDO appAuthDO = AppAuthDO.buildAppAuthDO(buildAppAuthDTO());
+        AppAuthDO appAuthDO = AppAuthDO.buildWith(buildAppAuthDTO());
         Timestamp now = Timestamp.valueOf(LocalDateTime.now());
         appAuthDO.setDateCreated(now);
         appAuthDO.setDateUpdated(now);
