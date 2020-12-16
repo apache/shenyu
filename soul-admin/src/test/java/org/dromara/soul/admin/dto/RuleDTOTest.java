5d2d483929f79cb68f1cff149765a92a3ec32f33
jiangjining <3@admin.cn>
2020-12-17 01:55:56 +0800

    Change style of license.

--- soul-admin/src/test/java/org/dromara/soul/admin/dto/RuleDTOTest.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/dto/RuleDTOTest.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.dromara.soul.admin.dto;
 
 import org.assertj.core.util.Lists;
