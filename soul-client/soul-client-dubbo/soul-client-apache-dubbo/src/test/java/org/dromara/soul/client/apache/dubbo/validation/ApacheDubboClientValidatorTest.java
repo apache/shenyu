366005c9c77a35fbca61de2976e5fbc3c320222d
Kevin Clair <704714211@qq.com>
2020-12-17 12:08:37 +0800

    Dubbo support ValidationException#471 (#736)
    * Add dubbo tag route, associated issue#471
    
    * Checkstyle update.
    
    * Add validation.
    
    * Add GenericFilter.
    
    * delete files.
    
    * Add apache dubbo supprot SoulException.
    
    * Add alibaba dubbo supprot SoulException.
    
    * check style.
    
    * delete maven dependency
    
    * update default time out.
    
    * Make alibaba Dubbo support ValidationException.
    
    * Make apache Dubbo support ValidationException.
    
    * check style.
    
    * Add test case for AlibabaDubboClientValidator.
    
    * Add test case for ApacheDubboClientValidator.
    
    * check style.
    
    Co-authored-by: mingjie.song <mingjie.song@mihoyo.com>

---
+++ soul-client/soul-client-dubbo/soul-client-apache-dubbo/src/test/java/org/dromara/soul/client/apache/dubbo/validation/ApacheDubboClientValidatorTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dromara.soul.client.apache.dubbo.validation;
+
+import org.apache.dubbo.common.URL;
+import org.dromara.soul.client.apache.dubbo.validation.service.TestService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * ApacheDubboClientValidatorTest.
+ *
+ * @author KevinClair
+ */
+public class ApacheDubboClientValidatorTest {
+
+    /**
+     * test method {@link ApacheDubboClientValidator#validate(java.lang.String, java.lang.Class[], java.lang.Object[])}.
+     */
+    @Test
+    public void validate() {
+        URL url = URL.valueOf("dubbo://127.0.0.1:20880/org.dromara.soul"
+                + ".client.apache.dubbo.validation.service.TestService"
+                + "?accepts=500&anyhost=true&application=soul-proxy"
+                + "&bind.ip=127.0.0.1&bind.port=20880&deprecated=false"
+                + "&dubbo=2.0.2&dynamic=true&generic=false"
+                + "&interface=org.dromara.soul.client.apache.dubbo.validation.service.TestService"
+                + "&keep.alive=true&methods=test&pid=67352&qos.enable=false&release=2.7.0"
+                + "&side=provider&threadpool=fixed&threads=500&timeout=20000"
+                + "&timestamp=1608119259859&validation=soulValidation");
+        ApacheDubboClientValidator apacheDubboClientValidator = new ApacheDubboClientValidator(url);
+        try {
+            apacheDubboClientValidator.validate("test",
+                    new Class[]{TestService.TestObject.class},
+                    new Object[]{TestService.TestObject.builder().age(null).build()});
+        } catch (Exception e) {
+            assertEquals("age cannot be null.", e.getMessage());
+        }
+    }
+}
