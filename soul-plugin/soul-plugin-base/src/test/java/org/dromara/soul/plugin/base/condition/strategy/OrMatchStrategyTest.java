e0b93d54c646768df2200f5bef42c4cc9e5b4c94
BetterWp <wp513920@qq.com>
2020-12-18 15:05:23 +0800

    OrMatchStrategyTest class add final

--- soul-plugin/soul-plugin-base/src/test/java/org/dromara/soul/plugin/base/condition/strategy/OrMatchStrategyTest.java
+++ soul-plugin/soul-plugin-base/src/test/java/org/dromara/soul/plugin/base/condition/strategy/OrMatchStrategyTest.java
@@ -36,7 +36,7 @@ import java.util.List;
  * @author BetterWp
  */
 @RunWith(MockitoJUnitRunner.class)
-public class OrMatchStrategyTest {
+public final class OrMatchStrategyTest {
 
     private ServerWebExchange exchange;
 
