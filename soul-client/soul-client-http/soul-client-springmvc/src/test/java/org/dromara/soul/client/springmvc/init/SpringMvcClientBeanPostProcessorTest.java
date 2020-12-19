fdd44d9fa4bbf0257e1a50aff58430253d5ced91
onlyonezhongjinhui <88058373@qq.com>
2020-12-19 16:56:38 +0800

    Improve code coverage for SpringMvcClientBeanPostProcessor in soul-client-springmvc

--- soul-client/soul-client-http/soul-client-springmvc/src/test/java/org/dromara/soul/client/springmvc/init/SpringMvcClientBeanPostProcessorTest.java
+++ soul-client/soul-client-http/soul-client-springmvc/src/test/java/org/dromara/soul/client/springmvc/init/SpringMvcClientBeanPostProcessorTest.java
@@ -19,7 +19,6 @@ package org.dromara.soul.client.springmvc.init;
 
 import org.dromara.soul.client.springmvc.annotation.SoulSpringMvcClient;
 import org.dromara.soul.client.springmvc.config.SoulSpringMvcConfig;
-import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,37 +37,173 @@ import org.springframework.web.bind.annotation.RestController;
 @RunWith(MockitoJUnitRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public final class SpringMvcClientBeanPostProcessorTest {
-    private static SpringMvcClientBeanPostProcessor springMvcClientBeanPostProcessor;
 
-    private final SpringMvcClientTestBean springMvcClientTestBean = new SpringMvcClientTestBean();
+    private final SpringMvcClientTestBeanSpecified springMvcClientTestBeanSpecified = new SpringMvcClientTestBeanSpecified();
 
-    @BeforeClass
-    public static void init() {
-        SoulSpringMvcConfig soulSpringMvcConfig = new SoulSpringMvcConfig();
-        soulSpringMvcConfig.setAdminUrl("http://127.0.0.1:58080");
-        soulSpringMvcConfig.setAppName("test-mvc");
-        soulSpringMvcConfig.setContextPath("test");
-        soulSpringMvcConfig.setPort(58889);
-        springMvcClientBeanPostProcessor = new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    private final SpringMvcClientTestBeanWildcardMatching springMvcClientTestBeanWildcardMatching = new SpringMvcClientTestBeanWildcardMatching();
 
+    private final SpringMvcClientTestBeanWithRuleName springMvcClientTestBeanWithRuleName = new SpringMvcClientTestBeanWithRuleName();
+
+    @Test(expected = RuntimeException.class)
+    public void testCreateWithContextPathIsNull() {
+        createBeanPostProcessorWithContextPathIsNull();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCreateWithContextPathIsEmpty() {
+        createBeanPostProcessorWithContextPathIsEmpty();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCreateWithAdminUrlIsNull() {
+        createBeanPostProcessorWithAdminUrlIsNull();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCreateWithAdminUrlIsEmpty() {
+        createBeanPostProcessorWithAdminUrlIsEmpty();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCreateWithPortIsNull() {
+        createBeanPostProcessorWithPortIsNull();
     }
 
     @Test
     public void testSoulBeanProcess() {
-        springMvcClientBeanPostProcessor.postProcessAfterInitialization(springMvcClientTestBean, "springMvcClientTestBean");
+        createBeanPostProcessor().postProcessAfterInitialization(springMvcClientTestBeanSpecified, "SpringMvcClientTestBeanSpecified");
+    }
+
+    @Test
+    public void testSoulBeanProcessWithoutHost() {
+        createBeanPostProcessorWithoutHost().postProcessAfterInitialization(springMvcClientTestBeanSpecified, "SpringMvcClientTestBeanSpecified");
+    }
+
+    @Test
+    public void testSoulBeanProcessWithEmptyHost() {
+        createBeanPostProcessorWithEmptyHost().postProcessAfterInitialization(springMvcClientTestBeanSpecified, "SpringMvcClientTestBeanSpecified");
+    }
+
+    @Test
+    public void testSoulBeanProcessWithoutFull() {
+        createBeanPostProcessorWithoutFull().postProcessAfterInitialization(springMvcClientTestBeanSpecified, "SpringMvcClientTestBeanSpecified");
+    }
+
+    @Test
+    public void testSoulBeanProcessWildcardMatching() {
+        createBeanPostProcessorWithoutFull().postProcessAfterInitialization(springMvcClientTestBeanWildcardMatching, "SpringMvcClientTestBeanWildcardMatching");
+    }
+
+    @Test
+    public void testBeanProcessWithRuleName() {
+        createBeanPostProcessorWithoutFull().postProcessAfterInitialization(springMvcClientTestBeanWithRuleName, "springMvcClientTestBeanWithRuleName");
     }
 
     @Test
     public void testNormalBeanProcess() {
-        springMvcClientBeanPostProcessor.postProcessAfterInitialization(new Object(), "normalBean");
+        createBeanPostProcessor().postProcessAfterInitialization(new Object(), "normalBean");
+    }
+
+    private void createBeanPostProcessorWithContextPathIsNull() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setContextPath(null);
+        new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private void createBeanPostProcessorWithContextPathIsEmpty() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setContextPath("");
+        new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private void createBeanPostProcessorWithAdminUrlIsNull() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setAdminUrl(null);
+        new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private void createBeanPostProcessorWithAdminUrlIsEmpty() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setAdminUrl("");
+        new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private void createBeanPostProcessorWithPortIsNull() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setPort(null);
+        new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private SpringMvcClientBeanPostProcessor createBeanPostProcessorWithoutFull() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setFull(false);
+        return new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private SpringMvcClientBeanPostProcessor createBeanPostProcessorWithoutHost() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setFull(false);
+        soulSpringMvcConfig.setHost(null);
+        return new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private SpringMvcClientBeanPostProcessor createBeanPostProcessorWithEmptyHost() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        soulSpringMvcConfig.setFull(false);
+        soulSpringMvcConfig.setHost("");
+        return new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private SpringMvcClientBeanPostProcessor createBeanPostProcessor() {
+        SoulSpringMvcConfig soulSpringMvcConfig = createSoulSpringMvcConfigWithFillAllField();
+        return new SpringMvcClientBeanPostProcessor(soulSpringMvcConfig);
+    }
+
+    private SoulSpringMvcConfig createSoulSpringMvcConfigWithFillAllField() {
+        SoulSpringMvcConfig soulSpringMvcConfig = new SoulSpringMvcConfig();
+        soulSpringMvcConfig.setAdminUrl("http://127.0.0.1:58080");
+        soulSpringMvcConfig.setContextPath("test");
+        soulSpringMvcConfig.setAppName("test-mvc");
+        soulSpringMvcConfig.setFull(true);
+        soulSpringMvcConfig.setHost("127.0.0.1");
+        soulSpringMvcConfig.setPort(58889);
+        return soulSpringMvcConfig;
     }
 
     @RestController
     @RequestMapping("/order")
     @SoulSpringMvcClient(path = "/order")
-    static class SpringMvcClientTestBean {
+    static class SpringMvcClientTestBeanSpecified {
         @PostMapping("/save")
         @SoulSpringMvcClient(path = "/save")
+        public String save(@RequestBody final String body) {
+            return "" + body;
+        }
+    }
+
+    @RestController
+    @RequestMapping("/goods")
+    @SoulSpringMvcClient(path = "/goods/*")
+    static class SpringMvcClientTestBeanWildcardMatching {
+        @PostMapping("/save")
+        @SoulSpringMvcClient(path = "/save")
+        public String save(@RequestBody final String body) {
+            return "" + body;
+        }
+
+        @PostMapping("/update")
+        @SoulSpringMvcClient(path = "/update")
+        public String update(@RequestBody final String body) {
+            return "" + body;
+        }
+    }
+
+    @RestController
+    @RequestMapping("/transaction")
+    @SoulSpringMvcClient(path = "/transaction")
+    static class SpringMvcClientTestBeanWithRuleName {
+        @PostMapping("/save")
+        @SoulSpringMvcClient(path = "/save", ruleName = "test transaction save rule name")
         public String save(@RequestBody final String body) {
             return "" + body;
         }
