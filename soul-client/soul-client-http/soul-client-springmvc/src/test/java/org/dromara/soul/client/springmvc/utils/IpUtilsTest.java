a772d37ab44f37e1bb201d75c703b56ce9738d8c
holddie <holddie@163.com>
2020-12-21 11:38:21 +0800

    feature: Remove an empty line.

--- soul-client/soul-client-http/soul-client-springmvc/src/test/java/org/dromara/soul/client/springmvc/utils/IpUtilsTest.java
+++ soul-client/soul-client-http/soul-client-springmvc/src/test/java/org/dromara/soul/client/springmvc/utils/IpUtilsTest.java
@@ -24,7 +24,6 @@ import org.mockito.MockedStatic;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
