21fae1f4e24724afb66d57ff1eefef4892390802
wuudongdong <wuudongdong@gmail.com>
2020-12-23 23:52:52 +0800

    use `assertEquals` to replace `assertNotNull` and verify `session.getBasicRemote().sendText` call times.

--- soul-admin/src/test/java/org/dromara/soul/admin/listener/websocket/WebsocketCollectorTest.java
+++ soul-admin/src/test/java/org/dromara/soul/admin/listener/websocket/WebsocketCollectorTest.java
@@ -17,22 +17,25 @@
 
 package org.dromara.soul.admin.listener.websocket;
 
-import lombok.extern.slf4j.Slf4j;
 import org.dromara.soul.admin.service.SyncDataService;
 import org.dromara.soul.admin.spring.SpringBeanUtils;
 import org.dromara.soul.common.enums.DataEventTypeEnum;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
+import java.io.IOException;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -41,23 +44,19 @@ import static org.mockito.Mockito.when;
  *
  * @author wuudongdong
  */
-@Slf4j
+@RunWith(MockitoJUnitRunner.class)
 public final class WebsocketCollectorTest {
 
     private WebsocketCollector websocketCollector;
 
+    @Mock
     private Session session;
+
+    @Mock
+    private SyncDataService syncDataService;
 
     @Before
     public void setUp() {
-        ConfigurableApplicationContext context = mock(ConfigurableApplicationContext.class);
-        SpringBeanUtils.getInstance().setCfgContext(context);
-        SyncDataService syncDataService = mock(SyncDataService.class);
-        when(SpringBeanUtils.getInstance().getBean(SyncDataService.class)).thenReturn(syncDataService);
-        when(syncDataService.syncAll(DataEventTypeEnum.MYSELF)).thenReturn(true);
-        session = mock(Session.class);
-        RemoteEndpoint.Basic basic = mock(RemoteEndpoint.Basic.class);
-        when(session.getBasicRemote()).thenReturn(basic);
         websocketCollector = new WebsocketCollector();
     }
 
@@ -70,9 +69,14 @@ public final class WebsocketCollectorTest {
 
     @Test
     public void testOnMessage() {
+        ConfigurableApplicationContext context = mock(ConfigurableApplicationContext.class);
+        SpringBeanUtils.getInstance().setCfgContext(context);
+        when(SpringBeanUtils.getInstance().getBean(SyncDataService.class)).thenReturn(syncDataService);
+        when(syncDataService.syncAll(DataEventTypeEnum.MYSELF)).thenReturn(true);
         websocketCollector.onOpen(session);
         websocketCollector.onMessage(DataEventTypeEnum.MYSELF.name(), session);
-        assertNotNull(getSession());
+        assertEquals(session, getSession());
+        Mockito.verify(syncDataService, Mockito.times(1)).syncAll(DataEventTypeEnum.MYSELF);
         websocketCollector.onClose(session);
     }
 
@@ -96,13 +100,18 @@ public final class WebsocketCollectorTest {
     }
 
     @Test
-    public void testSend() {
+    public void testSend() throws IOException {
+        RemoteEndpoint.Basic basic = mock(RemoteEndpoint.Basic.class);
+        when(session.getBasicRemote()).thenReturn(basic);
         websocketCollector.onOpen(session);
         assertEquals(1L, getSessionSetSize());
-        websocketCollector.onMessage(DataEventTypeEnum.MYSELF.name(), session);
         WebsocketCollector.send(null, DataEventTypeEnum.MYSELF);
-        WebsocketCollector.send("test", DataEventTypeEnum.MYSELF);
-        WebsocketCollector.send("test", DataEventTypeEnum.CREATE);
+        Mockito.verify(basic, Mockito.times(0)).sendText(null);
+        ReflectionTestUtils.setField(WebsocketCollector.class, "session", session);
+        WebsocketCollector.send("test_message_1", DataEventTypeEnum.MYSELF);
+        Mockito.verify(basic, Mockito.times(1)).sendText("test_message_1");
+        WebsocketCollector.send("test_message_2", DataEventTypeEnum.CREATE);
+        Mockito.verify(basic, Mockito.times(1)).sendText("test_message_2");
         websocketCollector.onClose(session);
     }
 
