13f80a75c8cc4b1eb42a03149dba9981d0a509b3
zhoubin <zhoubin1-jk@360shuke.com>
2021-01-05 15:53:36 +0800

    add test AlibabaDubboPluginConfiguration class

---
+++ soul-spring-boot-starter/soul-spring-boot-starter-plugin/soul-spring-boot-starter-plugin-alibaba-dubbo/src/test/java/org/dromara/soul/spring/boot/starter/plugin/alibaba/dubbo/AlibabaDubboPluginConfigurationTest.java
@@ -0,0 +1,60 @@
+package org.dromara.soul.spring.boot.starter.plugin.alibaba.dubbo;
+
+import org.dromara.soul.common.enums.PluginEnum;
+import org.dromara.soul.plugin.alibaba.dubbo.AlibabaDubboPlugin;
+import org.dromara.soul.plugin.alibaba.dubbo.handler.AlibabaDubboPluginDataHandler;
+import org.dromara.soul.plugin.alibaba.dubbo.param.BodyParamPlugin;
+import org.dromara.soul.plugin.alibaba.dubbo.response.DubboResponsePlugin;
+import org.dromara.soul.plugin.alibaba.dubbo.subscriber.AlibabaDubboMetaDataSubscriber;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test case for {@link AlibabaDubboPluginConfiguration}.
+ *
+ * @author: ZhouBin
+ * @date: 2021/1/4 20:19
+*/
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes = {
+                AlibabaDubboPluginConfiguration.class,
+                AlibabaDubboPluginConfigurationTest.class
+        },
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@EnableAutoConfiguration
+public class AlibabaDubboPluginConfigurationTest {
+
+    @Autowired
+    private AlibabaDubboPlugin alibabaDubboPlugin;
+    @Autowired
+    private BodyParamPlugin bodyParamPlugin;
+    @Autowired
+    private DubboResponsePlugin dubboResponsePlugin;
+    @Autowired
+    private AlibabaDubboPluginDataHandler alibabaDubboPluginDataHandler;
+    @Autowired
+    private AlibabaDubboMetaDataSubscriber alibabaDubboMetaDataSubscriber;
+
+    @Test
+    public void testAlibabaDubboPlugin() {
+        Assert.assertEquals(PluginEnum.DUBBO.getCode(), alibabaDubboPlugin.getOrder());
+        Assert.assertEquals(PluginEnum.DUBBO.getName(), alibabaDubboPlugin.named());
+
+        Assert.assertEquals(PluginEnum.DUBBO.getCode() - 1, bodyParamPlugin.getOrder());
+        Assert.assertEquals("alibaba-dubbo-body-param", bodyParamPlugin.named());
+
+        Assert.assertEquals(PluginEnum.RESPONSE.getCode(), dubboResponsePlugin.getOrder());
+        Assert.assertEquals(PluginEnum.RESPONSE.getName(), dubboResponsePlugin.named());
+
+        Assert.assertEquals(PluginEnum.DUBBO.getName(), alibabaDubboPluginDataHandler.pluginNamed());
+
+        Assert.assertNotNull(alibabaDubboMetaDataSubscriber);
+    }
+}
