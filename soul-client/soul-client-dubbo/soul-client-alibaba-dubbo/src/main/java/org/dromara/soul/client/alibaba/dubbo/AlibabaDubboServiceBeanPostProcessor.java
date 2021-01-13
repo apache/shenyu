5fecf5ab6b689e428bb4696c27ac9e8b11e4addc
gaoxiangyang <xiangyang.gao@ishansong.com>
2021-01-13 17:23:47 +0800

    https://github.com/dromara/soul/issues/967

--- soul-client/soul-client-dubbo/soul-client-alibaba-dubbo/src/main/java/org/dromara/soul/client/alibaba/dubbo/AlibabaDubboServiceBeanPostProcessor.java
+++ soul-client/soul-client-dubbo/soul-client-alibaba-dubbo/src/main/java/org/dromara/soul/client/alibaba/dubbo/AlibabaDubboServiceBeanPostProcessor.java
@@ -40,6 +40,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 /**
@@ -55,6 +56,8 @@ public class AlibabaDubboServiceBeanPostProcessor implements ApplicationListener
     private final ExecutorService executorService;
 
     private final String url;
+
+    private AtomicBoolean isInit = new AtomicBoolean(false);
 
     public AlibabaDubboServiceBeanPostProcessor(final DubboConfig dubboConfig) {
         String contextPath = dubboConfig.getContextPath();
@@ -134,7 +137,7 @@ public class AlibabaDubboServiceBeanPostProcessor implements ApplicationListener
 
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent contextRefreshedEvent) {
-        if (Objects.nonNull(contextRefreshedEvent.getApplicationContext().getParent())) {
+        if (!isInit.compareAndSet(false, true)) {
             return;
         }
         // Fix bug(https://github.com/dromara/soul/issues/415), upload dubbo metadata on ContextRefreshedEvent
