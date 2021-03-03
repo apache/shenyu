7a30dec2ec33d5022561c93f77d0fbfb5e862c1d
holddie <holddie@163.com>
2021-03-03 14:18:38 +0800

    feature: add redirect plugin.

---
+++ soul-plugin/soul-plugin-redirect/src/main/java/org/dromara/soul/plugin/redirect/RedirectPlugin.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dromara.soul.plugin.redirect;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.dromara.soul.common.constant.Constants;
+import org.dromara.soul.common.dto.RuleData;
+import org.dromara.soul.common.dto.SelectorData;
+import org.dromara.soul.common.dto.convert.RedirectHandle;
+import org.dromara.soul.common.enums.PluginEnum;
+import org.dromara.soul.common.enums.RpcTypeEnum;
+import org.dromara.soul.common.utils.GsonUtils;
+import org.dromara.soul.plugin.api.SoulPluginChain;
+import org.dromara.soul.plugin.api.context.SoulContext;
+import org.dromara.soul.plugin.base.AbstractSoulPlugin;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+/**
+ * Redirect Plugin.
+ *
+ * @author HoldDie
+ */
+@Slf4j
+public class RedirectPlugin extends AbstractSoulPlugin {
+    @Override
+    public int getOrder() {
+        return PluginEnum.REDIRECT.getCode();
+    }
+
+    @Override
+    public String named() {
+        return PluginEnum.REDIRECT.getName();
+    }
+
+    @Override
+    public Boolean skip(ServerWebExchange exchange) {
+        final SoulContext body = exchange.getAttribute(Constants.CONTEXT);
+        return Objects.equals(Objects.requireNonNull(body).getRpcType(), RpcTypeEnum.DUBBO.getName());
+    }
+
+    @Override
+    protected Mono<Void> doExecute(ServerWebExchange exchange, SoulPluginChain chain, SelectorData selector, RuleData rule) {
+        String handle = rule.getHandle();
+        final RedirectHandle redirectHandle = GsonUtils.getInstance().fromJson(handle, RedirectHandle.class);
+        if (Objects.isNull(redirectHandle) || StringUtils.isBlank(redirectHandle.getRedirectURI())) {
+            log.error("uri redirect rule can not configuration: {}", handle);
+            return chain.execute(exchange);
+        }
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.PERMANENT_REDIRECT);
+        response.getHeaders().add(HttpHeaders.LOCATION, redirectHandle.getRedirectURI());
+        return response.setComplete();
+    }
+}
---
+++ soul-plugin/soul-plugin-redirect/src/test/java/org/dromara/soul/plugin/redirect/RedirectPluginTest.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dromara.soul.plugin.redirect;
+
+import org.dromara.soul.common.constant.Constants;
+import org.dromara.soul.common.dto.RuleData;
+import org.dromara.soul.common.dto.SelectorData;
+import org.dromara.soul.common.enums.PluginEnum;
+import org.dromara.soul.common.enums.RpcTypeEnum;
+import org.dromara.soul.plugin.api.SoulPluginChain;
+import org.dromara.soul.plugin.api.context.SoulContext;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.mock.web.server.MockServerWebExchange;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * test case for {@link RedirectPlugin}.
+ *
+ * @author HoldDie
+ */
+@RunWith(MockitoJUnitRunner.class)
+public final class RedirectPluginTest {
+
+    private RedirectPlugin redirectPlugin;
+
+    private ServerWebExchange exchange;
+
+    @Mock
+    private SoulPluginChain chain;
+
+    @Before
+    public void setUp() {
+        exchange = MockServerWebExchange.from(MockServerHttpRequest.get("localhost").build());
+        redirectPlugin = new RedirectPlugin();
+    }
+
+    @Test
+    public void testRedirectPlugin() {
+        RuleData ruleData = new RuleData();
+        when(chain.execute(exchange)).thenReturn(Mono.empty());
+        SelectorData selectorData = mock(SelectorData.class);
+        StepVerifier.create(redirectPlugin.doExecute(exchange, chain, selectorData, ruleData)).expectSubscription().verifyComplete();
+        ruleData.setHandle("{\"redirectURI\":\"/test\"}");
+        StepVerifier.create(redirectPlugin.doExecute(exchange, chain, selectorData, ruleData)).expectSubscription().verifyComplete();
+    }
+
+    @Test
+    public void testGetOrder() {
+        final int result = redirectPlugin.getOrder();
+        assertThat(PluginEnum.REDIRECT.getCode(), Matchers.is(result));
+    }
+
+    @Test
+    public void testNamed() {
+        final String result = redirectPlugin.named();
+        assertThat(PluginEnum.REDIRECT.getName(), Matchers.is(result));
+    }
+
+    @Test
+    public void testSkip() {
+        SoulContext context = mock(SoulContext.class);
+        when(context.getRpcType()).thenReturn(RpcTypeEnum.DUBBO.getName());
+        exchange.getAttributes().put(Constants.CONTEXT, context);
+        final Boolean result = redirectPlugin.skip(exchange);
+        assertThat(result, Matchers.is(true));
+    }
+}
