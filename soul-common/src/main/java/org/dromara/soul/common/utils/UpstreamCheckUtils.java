111e49d10c307aa7076476a495af5936fc427511
ywengineer <ywengineer@gmail.com>
2021-04-09 17:21:57 +0800

    UpStreamCheckUtils detection failing when upstream using hostname without port (#1236)
    * 修复当插件selector的upstream使用主机名时, UpstreamCheckUtils检测不通过
    
    * * support https upstream check.
    
    * * codestyle
    
    * * ci passed
    
    Co-authored-by: dengliming <liming.d.pro@gmail.com>

--- soul-common/src/main/java/org/dromara/soul/common/utils/UpstreamCheckUtils.java
+++ soul-common/src/main/java/org/dromara/soul/common/utils/UpstreamCheckUtils.java
@@ -17,13 +17,14 @@
 
 package org.dromara.soul.common.utils;
 
+import org.apache.commons.lang3.StringUtils;
+import org.dromara.soul.common.constant.Constants;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.regex.Pattern;
-import org.apache.commons.lang3.StringUtils;
-import org.dromara.soul.common.constant.Constants;
 
 /**
  * The type Uri utils.
@@ -33,9 +34,11 @@ import org.dromara.soul.common.constant.Constants;
 public class UpstreamCheckUtils {
 
     private static final Pattern PATTERN = Pattern
-            .compile("(http://|https://)?(?:(?:[0,1]?\\d?\\d|2[0-4]\\d|25[0-5])\\.){3}(?:[0,1]?\\d?\\d|2[0-4]\\d|25[0-5]):\\d{0,5}");
+            .compile("(http://|https://)?(?:(?:[0,1]?\\d?\\d|2[0-4]\\d|25[0-5])\\.){3}(?:[0,1]?\\d?\\d|2[0-4]\\d|25[0-5])(:\\d{0,5})?");
 
     private static final String HTTP = "http";
+
+    private static final String HTTPS = "https";
 
     /**
      * Check url boolean.
@@ -47,17 +50,19 @@ public class UpstreamCheckUtils {
         if (StringUtils.isBlank(url)) {
             return false;
         }
-        if (checkIP(url)) {
-            String[] hostPort;
-            if (url.startsWith(HTTP)) {
-                final String[] http = StringUtils.split(url, "\\/\\/");
-                hostPort = StringUtils.split(http[1], Constants.COLONS);
-            } else {
-                hostPort = StringUtils.split(url, Constants.COLONS);
-            }
-            return isHostConnector(hostPort[0], Integer.parseInt(hostPort[1]));
+        String[] hostPort;
+        if (url.startsWith(HTTP)) {
+            final String[] http = StringUtils.split(url, "\\/\\/");
+            hostPort = StringUtils.split(http[1], Constants.COLONS);
         } else {
-            return isHostReachable(url);
+            hostPort = StringUtils.split(url, Constants.COLONS);
+        }
+        final boolean isHttps = url.startsWith(HTTPS);
+        final int port = hostPort.length > 1 ? Integer.parseInt(hostPort[1]) : isHttps ? 443 : 80;
+        if (checkIP(hostPort[0]) || isHttps) {
+            return isHostConnector(hostPort[0], port);
+        } else {
+            return isHostReachable(hostPort[0]);
         }
     }
 
