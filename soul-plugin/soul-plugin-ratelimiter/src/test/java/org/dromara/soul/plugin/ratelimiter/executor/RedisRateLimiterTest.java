c1ee766dfde5bcb04db96d2b3a8fbfbf8c92e324
SIGN <13169103974@163.com>
2021-05-02 13:35:51 +0800

    fix RateLimiterAlgorithm#callback execution sequence

--- soul-plugin/soul-plugin-ratelimiter/src/test/java/org/dromara/soul/plugin/ratelimiter/executor/RedisRateLimiterTest.java
+++ soul-plugin/soul-plugin-ratelimiter/src/test/java/org/dromara/soul/plugin/ratelimiter/executor/RedisRateLimiterTest.java
@@ -32,7 +32,6 @@ import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.adapter.DefaultServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
