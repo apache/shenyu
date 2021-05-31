dc58e1a3ce230bff865afe6bac6d2b0eca40560d
kyriehong <kyriehong@didiglobal.com>
2021-05-31 19:23:49 +0800

    add apache license 2.0 header and remove empty line

--- shenyu-sync-data-center/shenyu-sync-data-websocket/src/test/java/org/apache/shenyu/plugin/sync/data/websocket/handler/WebsocketDataHandlerTest.java
+++ shenyu-sync-data-center/shenyu-sync-data-websocket/src/test/java/org/apache/shenyu/plugin/sync/data/websocket/handler/WebsocketDataHandlerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.shenyu.plugin.sync.data.websocket.handler;
 
 import static org.mockito.Mockito.mock;
@@ -19,16 +35,16 @@ import org.mockito.Mockito;
 
 
 public class WebsocketDataHandlerTest {
-    PluginDataSubscriber pluginDataSubscriber;
-    WebsocketDataHandler websocketDataHandler;
+
+    private PluginDataSubscriber pluginDataSubscriber;
+
+    private WebsocketDataHandler websocketDataHandler;
 
     @Before
     public void testWebsocketDataHandler() {
         pluginDataSubscriber = mock(PluginDataSubscriber.class);
-
         List<AuthDataSubscriber> authDataSubscribers = new LinkedList<>();
         List<MetaDataSubscriber> metaDataSubscribers = new LinkedList<>();
-
         websocketDataHandler = new WebsocketDataHandler(pluginDataSubscriber, metaDataSubscribers, authDataSubscribers);
     }
 
