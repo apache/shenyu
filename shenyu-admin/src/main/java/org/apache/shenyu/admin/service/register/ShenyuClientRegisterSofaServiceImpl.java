e2771733caf82befcd2aef08c9eb5fc30fd4f0eb
KevinClair <704714211@qq.com>
2021-06-20 16:22:30 +0800

    Add sofa shen yu client register service.

---
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/service/register/ShenyuClientRegisterSofaServiceImpl.java
@@ -0,0 +1,119 @@
+package org.apache.shenyu.admin.service.register;
+
+import org.apache.shenyu.admin.listener.DataChangedEvent;
+import org.apache.shenyu.admin.mapper.MetaDataMapper;
+import org.apache.shenyu.admin.mapper.PluginMapper;
+import org.apache.shenyu.admin.mapper.RuleMapper;
+import org.apache.shenyu.admin.model.dto.SelectorDTO;
+import org.apache.shenyu.admin.model.entity.MetaDataDO;
+import org.apache.shenyu.admin.model.entity.PluginDO;
+import org.apache.shenyu.admin.model.entity.RuleDO;
+import org.apache.shenyu.admin.model.entity.SelectorDO;
+import org.apache.shenyu.admin.service.RuleService;
+import org.apache.shenyu.admin.service.SelectorService;
+import org.apache.shenyu.admin.transfer.MetaDataTransfer;
+import org.apache.shenyu.admin.utils.ShenyuResultMessage;
+import org.apache.shenyu.common.enums.*;
+import org.apache.shenyu.common.utils.UUIDUtils;
+import org.apache.shenyu.register.common.dto.MetaDataRegisterDTO;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.Objects;
+
+/**
+ * sofa service register.
+ *
+ * @author KevinClair
+ **/
+@Service("sofa")
+public class ShenyuClientRegisterSofaServiceImpl extends AbstractShenyuClientRegisterService {
+
+    private final MetaDataMapper metaDataMapper;
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    private final SelectorService selectorService;
+
+    private final RuleService ruleService;
+
+    private final RuleMapper ruleMapper;
+
+    private final PluginMapper pluginMapper;
+
+    public ShenyuClientRegisterSofaServiceImpl(MetaDataMapper metaDataMapper, ApplicationEventPublisher eventPublisher, SelectorService selectorService, RuleService ruleService, RuleMapper ruleMapper, PluginMapper pluginMapper) {
+        this.metaDataMapper = metaDataMapper;
+        this.eventPublisher = eventPublisher;
+        this.selectorService = selectorService;
+        this.ruleService = ruleService;
+        this.ruleMapper = ruleMapper;
+        this.pluginMapper = pluginMapper;
+    }
+
+    @Override
+    public synchronized String register(final MetaDataRegisterDTO dto) {
+        MetaDataDO metaDataDO = metaDataMapper.findByPath(dto.getPath());
+        if (Objects.nonNull(metaDataDO)
+                && (!metaDataDO.getMethodName().equals(dto.getMethodName())
+                || !metaDataDO.getServiceName().equals(dto.getServiceName()))) {
+            return "you path already exist!";
+        }
+        final MetaDataDO exist = metaDataMapper.findByServiceNameAndMethod(dto.getServiceName(), dto.getMethodName());
+        saveOrUpdateMetaData(exist, dto);
+        String selectorId = handlerSelector(dto);
+        handlerRule(selectorId, dto, exist);
+        return ShenyuResultMessage.SUCCESS;
+    }
+
+    @Override
+    public void saveOrUpdateMetaData(final MetaDataDO exist, final MetaDataRegisterDTO metaDataDTO) {
+        DataEventTypeEnum eventType;
+        MetaDataDO metaDataDO = MetaDataTransfer.INSTANCE.mapRegisterDTOToEntity(metaDataDTO);
+        if (Objects.isNull(exist)) {
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            metaDataDO.setId(UUIDUtils.getInstance().generateShortUuid());
+            metaDataDO.setDateCreated(currentTime);
+            metaDataDO.setDateUpdated(currentTime);
+            metaDataMapper.insert(metaDataDO);
+            eventType = DataEventTypeEnum.CREATE;
+        } else {
+            metaDataDO.setId(exist.getId());
+            metaDataMapper.update(metaDataDO);
+            eventType = DataEventTypeEnum.UPDATE;
+        }
+        // publish MetaData's event
+        eventPublisher.publishEvent(new DataChangedEvent(ConfigGroupEnum.META_DATA, eventType,
+                Collections.singletonList(MetaDataTransfer.INSTANCE.mapToData(metaDataDO))));
+    }
+
+    @Override
+    public String handlerSelector(final MetaDataRegisterDTO metaDataDTO) {
+        SelectorDO selectorDO = selectorService.findByName(metaDataDTO.getContextPath());
+        if (Objects.nonNull(selectorDO)){
+            return selectorDO.getId();
+        }
+        String contextPath = metaDataDTO.getContextPath();
+        SelectorDTO selectorDTO = buildDefaultSelectorDTO(contextPath);
+        selectorDTO.setPluginId(getPluginId(PluginEnum.SOFA.getName()));
+        selectorDTO.setHandle(metaDataDTO.getAppName());
+        selectorDTO.setSelectorConditions(buildDefaultSelectorConditionDTO(contextPath));
+        return selectorService.register(selectorDTO);
+    }
+
+    @Override
+    public String getPluginId(final String pluginName) {
+        final PluginDO pluginDO = pluginMapper.selectByName(pluginName);
+        Objects.requireNonNull(pluginDO);
+        return pluginDO.getId();
+    }
+
+    @Override
+    public void handlerRule(final String selectorId, final MetaDataRegisterDTO metaDataDTO, final MetaDataDO exist) {
+        RuleDO existRule = ruleMapper.findByName(metaDataDTO.getPath());
+        if (Objects.isNull(exist) || Objects.isNull(existRule)) {
+            ruleService.register(registerRpcRule(selectorId, metaDataDTO.getPath(), PluginEnum.SOFA.getName(), metaDataDTO.getRuleName()));
+        }
+    }
+}
