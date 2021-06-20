542f0156c93289dcaeed4b761dda5940118e2eee
KevinClair <704714211@qq.com>
2021-06-20 16:22:21 +0800

    Add motan shen yu client register service.

---
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/service/register/ShenyuClientRegisterMotanServiceImpl.java
@@ -0,0 +1,169 @@
+package org.apache.shenyu.admin.service.register;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shenyu.admin.listener.DataChangedEvent;
+import org.apache.shenyu.admin.mapper.MetaDataMapper;
+import org.apache.shenyu.admin.mapper.PluginMapper;
+import org.apache.shenyu.admin.mapper.RuleMapper;
+import org.apache.shenyu.admin.mapper.SelectorMapper;
+import org.apache.shenyu.admin.model.dto.SelectorDTO;
+import org.apache.shenyu.admin.model.entity.MetaDataDO;
+import org.apache.shenyu.admin.model.entity.PluginDO;
+import org.apache.shenyu.admin.model.entity.RuleDO;
+import org.apache.shenyu.admin.model.entity.SelectorDO;
+import org.apache.shenyu.admin.service.RuleService;
+import org.apache.shenyu.admin.service.SelectorService;
+import org.apache.shenyu.admin.service.impl.UpstreamCheckService;
+import org.apache.shenyu.admin.transfer.MetaDataTransfer;
+import org.apache.shenyu.admin.utils.ShenyuResultMessage;
+import org.apache.shenyu.common.dto.SelectorData;
+import org.apache.shenyu.common.dto.convert.DivideUpstream;
+import org.apache.shenyu.common.enums.*;
+import org.apache.shenyu.common.utils.GsonUtils;
+import org.apache.shenyu.common.utils.UUIDUtils;
+import org.apache.shenyu.register.common.dto.MetaDataRegisterDTO;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * motan service register.
+ *
+ * @author KevinClair
+ **/
+@Service("motan")
+public class ShenyuClientRegisterMotanServiceImpl extends AbstractShenyuClientRegisterService {
+
+    private final MetaDataMapper metaDataMapper;
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    private final SelectorService selectorService;
+
+    private final RuleService ruleService;
+
+    private final RuleMapper ruleMapper;
+
+    private final UpstreamCheckService upstreamCheckService;
+
+    private final SelectorMapper selectorMapper;
+
+    private final PluginMapper pluginMapper;
+
+    public ShenyuClientRegisterMotanServiceImpl(MetaDataMapper metaDataMapper, ApplicationEventPublisher eventPublisher, SelectorService selectorService, RuleService ruleService, RuleMapper ruleMapper, UpstreamCheckService upstreamCheckService, SelectorMapper selectorMapper, PluginMapper pluginMapper) {
+        this.metaDataMapper = metaDataMapper;
+        this.eventPublisher = eventPublisher;
+        this.selectorService = selectorService;
+        this.ruleService = ruleService;
+        this.ruleMapper = ruleMapper;
+        this.upstreamCheckService = upstreamCheckService;
+        this.selectorMapper = selectorMapper;
+        this.pluginMapper = pluginMapper;
+    }
+
+    @Override
+    public String register(final MetaDataRegisterDTO dto) {
+        MetaDataDO exist = metaDataMapper.findByPath(dto.getPath());
+        saveOrUpdateMetaData(exist, dto);
+        String selectorId = handlerSelector(dto);
+        handlerRule(selectorId, dto, exist);
+        return ShenyuResultMessage.SUCCESS;
+    }
+
+    @Override
+    public void saveOrUpdateMetaData(final MetaDataDO exist, final MetaDataRegisterDTO metaDataDTO) {
+        DataEventTypeEnum eventType;
+        MetaDataDO metaDataDO = MetaDataTransfer.INSTANCE.mapRegisterDTOToEntity(metaDataDTO);
+        if (Objects.isNull(exist)) {
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            metaDataDO.setId(UUIDUtils.getInstance().generateShortUuid());
+            metaDataDO.setDateCreated(currentTime);
+            metaDataDO.setDateUpdated(currentTime);
+            metaDataMapper.insert(metaDataDO);
+            eventType = DataEventTypeEnum.CREATE;
+        } else {
+            metaDataDO.setId(exist.getId());
+            metaDataMapper.update(metaDataDO);
+            eventType = DataEventTypeEnum.UPDATE;
+        }
+        // publish MetaData's event
+        eventPublisher.publishEvent(new DataChangedEvent(ConfigGroupEnum.META_DATA, eventType,
+                Collections.singletonList(MetaDataTransfer.INSTANCE.mapToData(metaDataDO))));
+    }
+
+    @Override
+    public String handlerSelector(final MetaDataRegisterDTO dto) {
+        String contextPath = dto.getContextPath();
+        if (StringUtils.isEmpty(contextPath)) {
+            contextPath = buildContextPath(dto.getPath());
+        }
+        SelectorDO selectorDO = selectorService.findByName(contextPath);
+        String selectorId;
+        String uri = String.join(":", dto.getHost(), String.valueOf(dto.getPort()));
+        if (Objects.isNull(selectorDO)) {
+            selectorId = registerSelector(contextPath, uri);
+        } else {
+            selectorId = selectorDO.getId();
+            //update upstream
+            String handle = selectorDO.getHandle();
+            String handleAdd;
+            DivideUpstream addDivideUpstream = buildDivideUpstream(uri);
+            final SelectorData selectorData = selectorService.buildByName(contextPath);
+            // fetch UPSTREAM_MAP data from db
+            upstreamCheckService.fetchUpstreamData();
+            if (StringUtils.isBlank(handle)) {
+                handleAdd = GsonUtils.getInstance().toJson(Collections.singletonList(addDivideUpstream));
+            } else {
+                List<DivideUpstream> exist = GsonUtils.getInstance().fromList(handle, DivideUpstream.class);
+                for (DivideUpstream upstream : exist) {
+                    if (upstream.getUpstreamUrl().equals(addDivideUpstream.getUpstreamUrl())) {
+                        return selectorId;
+                    }
+                }
+                exist.add(addDivideUpstream);
+                handleAdd = GsonUtils.getInstance().toJson(exist);
+            }
+            selectorDO.setHandle(handleAdd);
+            selectorData.setHandle(handleAdd);
+            // update db
+            selectorMapper.updateSelective(selectorDO);
+            // submit upstreamCheck
+            upstreamCheckService.submit(contextPath, addDivideUpstream);
+            // publish change event.
+            eventPublisher.publishEvent(new DataChangedEvent(ConfigGroupEnum.SELECTOR, DataEventTypeEnum.UPDATE,
+                    Collections.singletonList(selectorData)));
+        }
+        return selectorId;
+    }
+
+    private String registerSelector(final String contextPath, final String uri) {
+        SelectorDTO selectorDTO = buildDefaultSelectorDTO(contextPath);
+        selectorDTO.setPluginId(getPluginId(PluginEnum.DIVIDE.getName()));
+        //is divide
+        DivideUpstream divideUpstream = buildDivideUpstream(uri);
+        String handler = GsonUtils.getInstance().toJson(Collections.singletonList(divideUpstream));
+        selectorDTO.setHandle(handler);
+        upstreamCheckService.submit(selectorDTO.getName(), divideUpstream);
+        selectorDTO.setSelectorConditions(buildDefaultSelectorConditionDTO(contextPath));
+        return selectorService.register(selectorDTO);
+    }
+
+    @Override
+    public String getPluginId(final String pluginName) {
+        final PluginDO pluginDO = pluginMapper.selectByName(pluginName);
+        Objects.requireNonNull(pluginDO);
+        return pluginDO.getId();
+    }
+
+    @Override
+    public void handlerRule(final String selectorId, final MetaDataRegisterDTO metaDataDTO, final MetaDataDO exist) {
+        RuleDO existRule = ruleMapper.findByName(metaDataDTO.getPath());
+        if (Objects.isNull(exist) || Objects.isNull(existRule)) {
+            ruleService.register(registerRpcRule(selectorId, metaDataDTO.getPath(), PluginEnum.MOTAN.getName(), metaDataDTO.getRuleName()));
+        }
+    }
+}
