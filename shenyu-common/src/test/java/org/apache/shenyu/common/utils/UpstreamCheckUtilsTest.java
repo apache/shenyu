48cd0a4a77e55e398aa3853b5675cdbdfdb90324
wzg547228197 <wzg547228197@gmail.com>
2021-06-22 23:47:17 +0800

    fix check url error

--- shenyu-common/src/test/java/org/apache/shenyu/common/utils/UpstreamCheckUtilsTest.java
+++ shenyu-common/src/test/java/org/apache/shenyu/common/utils/UpstreamCheckUtilsTest.java
@@ -73,6 +73,7 @@ public final class UpstreamCheckUtilsTest {
         assertTrue(UpstreamCheckUtils.checkUrl("127.0.0.1:" + port));
         assertFalse(UpstreamCheckUtils.checkUrl("http://127.0.0.1:" + (port == 0 ? port + 1 : port - 1)));
         assertTrue(UpstreamCheckUtils.checkUrl("http://127.0.0.1:" + port));
-        assertTrue(UpstreamCheckUtils.checkUrl("https://dromara.org"));
+        assertTrue(UpstreamCheckUtils.checkUrl("https://dromara.com"));
+        assertTrue(UpstreamCheckUtils.checkUrl("dromara.com"));
     }
 }
