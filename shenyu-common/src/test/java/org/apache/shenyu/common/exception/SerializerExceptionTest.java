38b63d28b245dbd4070f054baff056091020f171
lahmxu <31627887+lahmXu@users.noreply.github.com>
2021-08-01 09:36:32 +0800

    #1824 Add test for org.apache.shenyu.common.exception.* (#1848)
    * feat: add test
    
    * feat: add test
    
    Co-authored-by: lahmxu <lahmxj@gmail.com>

---
+++ shenyu-common/src/test/java/org/apache/shenyu/common/exception/SerializerExceptionTest.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.common.exception;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test case for {@link SerializerException}.
+ */
+public final class SerializerExceptionTest {
+
+    @Test
+    public void testAcquireByThrowable() {
+        String message = "error throwable";
+        Throwable throwable = new Throwable(message);
+        SerializerException serializerException = new SerializerException(throwable);
+
+        assertTrue(serializerException instanceof SerializerException);
+        assertEquals(serializerException.getCause().getMessage(), message);
+        assertEquals(serializerException.getCause(), throwable);
+    }
+
+    @Test
+    public void testAcquireByMessage() {
+        String message = "error";
+        SerializerException serializerException = new SerializerException(message);
+
+        assertTrue(serializerException instanceof SerializerException);
+        assertEquals(serializerException.getMessage(), message);
+    }
+
+    @Test
+    public void testAcquireByMessageAndThrowable() {
+        String message = "error message";
+        String throwableMessage = "error throwable";
+        Throwable throwable = new Throwable(throwableMessage);
+        SerializerException serializerException = new SerializerException(message, throwable);
+
+        assertTrue(serializerException instanceof SerializerException);
+        assertEquals(serializerException.getMessage(), message);
+        assertEquals(serializerException.getCause().getMessage(), throwableMessage);
+        assertEquals(serializerException.getCause(), throwable);
+    }
+}
