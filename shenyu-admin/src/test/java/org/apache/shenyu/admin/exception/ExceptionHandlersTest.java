bc1b01ef8a234407b8b587afeced7e6d519c4405
technoboy <technoboy@apache.org>
2021-08-02 23:10:45 +0800

    [ISSUE #1849] shenyu-admin a part of classes test code

--- shenyu-admin/src/test/java/org/apache/shenyu/admin/exception/ExceptionHandlersTest.java
+++ shenyu-admin/src/test/java/org/apache/shenyu/admin/exception/ExceptionHandlersTest.java
@@ -21,11 +21,27 @@ import org.apache.shenyu.admin.model.result.ShenyuAdminResult;
 import org.apache.shenyu.admin.utils.ShenyuResultMessage;
 import org.apache.shenyu.common.exception.CommonErrorCode;
 import org.apache.shenyu.common.exception.ShenyuException;
+import org.apache.shiro.authz.UnauthorizedException;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test case for {@link ExceptionHandlers}.
@@ -61,5 +77,68 @@ public final class ExceptionHandlersTest {
         ShenyuAdminResult result = exceptionHandlersUnderTest.serverExceptionHandler(duplicateKeyException);
         assertEquals(result.getCode().intValue(), CommonErrorCode.ERROR);
         assertEquals(result.getMessage(), ShenyuResultMessage.UNIQUE_INDEX_CONFLICT_ERROR);
+    }
+
+    @Test
+    public void testShiroExceptionHandler() {
+        UnauthorizedException unauthorizedException = mock(UnauthorizedException.class);
+        ShenyuAdminResult result = exceptionHandlersUnderTest.shiroExceptionHandler(unauthorizedException);
+        assertEquals(result.getCode().intValue(), CommonErrorCode.TOKEN_NO_PERMISSION);
+        assertEquals(result.getMessage(), ShenyuResultMessage.TOKEN_HAS_NO_PERMISSION);
+    }
+
+    @Test
+    public void testNullPointExceptionHandler() {
+        NullPointerException nullPointerException = mock(NullPointerException.class);
+        ShenyuAdminResult result = exceptionHandlersUnderTest.nullPointExceptionHandler(nullPointerException);
+        assertEquals(result.getCode().intValue(), CommonErrorCode.NOT_FOUND_EXCEPTION);
+        assertEquals(result.getMessage(), ShenyuResultMessage.NOT_FOUND_EXCEPTION);
+    }
+
+    @Test
+    public void testHandleHttpRequestMethodNotSupportedException() {
+        HttpRequestMethodNotSupportedException exception = mock(HttpRequestMethodNotSupportedException.class);
+        ShenyuAdminResult result = exceptionHandlersUnderTest.handleHttpRequestMethodNotSupportedException(exception);
+        assertEquals(result.getCode().intValue(), CommonErrorCode.ERROR);
+        assertThat(result.getMessage(), containsString("method is not supported for this request. Supported methods are"));
+    }
+
+    @Test
+    public void testHandleMethodArgumentNotValidException() {
+        MethodArgumentNotValidException exception = mock(MethodArgumentNotValidException.class);
+        BindingResult bindingResult = mock(BindingResult.class);
+        when(exception.getBindingResult()).thenReturn(bindingResult);
+        List<FieldError> fieldErrors = mock(List.class);
+        when(bindingResult.getFieldErrors()).thenReturn(fieldErrors);
+        ShenyuAdminResult result = exceptionHandlersUnderTest.handleMethodArgumentNotValidException(exception);
+        assertEquals(result.getCode().intValue(), CommonErrorCode.ERROR);
+        assertThat(result.getMessage(), containsString("Request error! invalid argument"));
+    }
+
+    @Test
+    public void testHandleMissingServletRequestParameterException() {
+        MissingServletRequestParameterException exception = mock(MissingServletRequestParameterException.class);
+        ShenyuAdminResult result = exceptionHandlersUnderTest.handleMissingServletRequestParameterException(exception);
+        assertEquals(result.getCode().intValue(), CommonErrorCode.ERROR);
+        assertThat(result.getMessage(), containsString("parameter is missing"));
+    }
+
+    @Test
+    public void testHandleMethodArgumentTypeMismatchException() {
+        MethodArgumentTypeMismatchException exception = mock(MethodArgumentTypeMismatchException.class);
+        Class clazz = MethodArgumentTypeMismatchException.class;
+        when(exception.getRequiredType()).thenReturn(clazz);
+        ShenyuAdminResult result = exceptionHandlersUnderTest.handleMethodArgumentTypeMismatchException(exception);
+        assertEquals(result.getCode().intValue(), CommonErrorCode.ERROR);
+        assertThat(result.getMessage(), containsString("should be of type"));
+    }
+
+    @Test
+    public void testHandleConstraintViolationException() {
+        ConstraintViolationException exception = mock(ConstraintViolationException.class);
+        Set<ConstraintViolation<?>> violations = mock(Set.class);
+        when(exception.getConstraintViolations()).thenReturn(violations);
+        ShenyuAdminResult result = exceptionHandlersUnderTest.handleConstraintViolationException(exception);
+        assertEquals(result.getCode().intValue(), CommonErrorCode.ERROR);
     }
 }
