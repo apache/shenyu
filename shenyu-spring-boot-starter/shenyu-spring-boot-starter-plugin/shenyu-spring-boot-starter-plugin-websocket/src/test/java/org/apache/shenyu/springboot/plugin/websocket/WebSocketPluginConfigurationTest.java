f3335a0b70cb4660b6928a7fecdb153c7e03a5bc
xiaoyu <549477611@qq.com>
2021-08-27 21:11:21 +0800

    [type: refactor] refactor websocket as independent plugin

--- shenyu-spring-boot-starter/shenyu-spring-boot-starter-plugin/shenyu-spring-boot-starter-plugin-websocket/src/test/java/org/apache/shenyu/springboot/plugin/websocket/WebSocketPluginConfigurationTest.java
+++ shenyu-spring-boot-starter/shenyu-spring-boot-starter-plugin/shenyu-spring-boot-starter-plugin-websocket/src/test/java/org/apache/shenyu/springboot/plugin/websocket/WebSocketPluginConfigurationTest.java
@@ -36,7 +36,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WebSocketPluginConfigurationTest {
 
     @Test
-    public void testDividePlugin() {
+    public void testWebSocketPlugin() {
         new ApplicationContextRunner()
             .withConfiguration(
                 AutoConfigurations.of(
