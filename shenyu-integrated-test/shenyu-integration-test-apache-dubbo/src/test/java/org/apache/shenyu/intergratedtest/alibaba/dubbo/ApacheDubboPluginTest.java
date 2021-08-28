4913e298c2799b4588768b7c2a1bac84acdc7807
strawberry-crisis <qiaodige@hotmail.com>
2021-08-28 10:21:31 +0800

    add dubbo intergration tests

---
+++ shenyu-integrated-test/shenyu-integration-test-apache-dubbo/src/test/java/org/apache/shenyu/intergratedtest/alibaba/dubbo/ApacheDubboPluginTest.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.intergratedtest.alibaba.dubbo;
+
+import org.apache.shenyu.integratedtest.common.AbstractTest;
+import org.apache.shenyu.integratedtest.common.dto.DubboTest;
+import org.apache.shenyu.integratedtest.common.helper.HttpHelper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ApacheDubboPluginTest extends AbstractTest {
+
+    @Test
+    public void testFindById() throws IOException {
+        DubboTest dubboTest = new DubboTest("hello", "shenyu");
+        dubboTest = HttpHelper.INSTANCE.postGateway("/dubbo/findById", dubboTest.getId(), DubboTest.class);
+        assertEquals("hello world shenyu apache Dubbo, findById", dubboTest.getName());
+    }
+}
