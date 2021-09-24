01a0ce254f45431cb482d7e6894a223261de08ed
hezhaoming <hezhaoming@jkinvest.cn>
2021-09-24 11:07:26 +0800

    feat: Modify method Name

--- shenyu-web/src/test/java/org/apache/shenyu/web/loader/ShenyuPluginPathBuilderTest.java
+++ shenyu-web/src/test/java/org/apache/shenyu/web/loader/ShenyuPluginPathBuilderTest.java
@@ -33,10 +33,9 @@ public final class ShenyuPluginPathBuilderTest {
      */
     @Test
     public void testGetPluginPathByCustomPath() {
-        File pluginPath = ShenyuPluginPathBuilder.getPluginPath("D:\\testpath");
+        File pluginPath = ShenyuPluginPathBuilder.getPluginPath("D:/testpath");
         Assert.assertNotNull(pluginPath);
     }
-
 
     /**
      * test  for plugin-ext.
@@ -44,6 +43,7 @@ public final class ShenyuPluginPathBuilderTest {
      */
     @Test
     public void testGetPluginPathByPluginExt() {
+        System.setProperty("plugin-ext", "D:/testUrl");
         File pluginPath = ShenyuPluginPathBuilder.getPluginPath("");
         Assert.assertNotNull(pluginPath);
     }
