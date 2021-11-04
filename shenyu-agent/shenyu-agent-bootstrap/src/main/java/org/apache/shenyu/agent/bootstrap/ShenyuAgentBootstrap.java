7e4240255c19b1f95c1f4db57673521ff1c1a23a
YuDong Tang <583125614@qq.com>
2021-11-04 19:15:41 +0800

    [ISSUE #1978] add agent class loader (#2298)
    * agent loader
    
    * agent loader

--- shenyu-agent/shenyu-agent-bootstrap/src/main/java/org/apache/shenyu/agent/bootstrap/ShenyuAgentBootstrap.java
+++ shenyu-agent/shenyu-agent-bootstrap/src/main/java/org/apache/shenyu/agent/bootstrap/ShenyuAgentBootstrap.java
@@ -17,7 +17,10 @@
 
 package org.apache.shenyu.agent.bootstrap;
 
+import org.apache.shenyu.agent.bootstrap.classloader.AgentClassLoader;
+
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Method;
 
 /**
  * The type Shenyu agent bootstrap.
@@ -29,10 +32,18 @@ public class ShenyuAgentBootstrap {
      *
      * @param arguments arguments
      * @param instrumentation instrumentation
+     * @throws Exception the exception
      */
-    public static void premain(final String arguments, final Instrumentation instrumentation) {
-        // todo create agent classloader and switch to it
-        // todo use reflect invoke AgentInstaller's method installBytebuddyAgent
-        // todo turn back to prev classloader back
+    public static void premain(final String arguments, final Instrumentation instrumentation) throws Exception {
+        AgentClassLoader agentClassLoader = AgentClassLoader.createAgentClassloader();
+        Class<?> agentInstallerClass = agentClassLoader.loadClass("org.apache.shenyu.agent.AgentInstaller");
+        Method agentInstallerMethod = agentInstallerClass.getMethod("installBytebuddyAgent", Instrumentation.class);
+        ClassLoader originClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(agentClassLoader);
+        try {
+            agentInstallerMethod.invoke(null, instrumentation);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originClassLoader);
+        }
     }
 }
