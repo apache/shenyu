f91ab02a9daff05a1d19c10c679f222b73ee2dc3
Qicz <qiczzhu@gmail.com>
2021-11-10 22:59:53 +0800

    add code comment.

--- shenyu-plugin/shenyu-plugin-dubbo/shenyu-plugin-dubbo-common/src/main/java/org/apache/shenyu/plugin/dubbo/common/cache/DubboParam.java
+++ shenyu-plugin/shenyu-plugin-dubbo/shenyu-plugin-dubbo-common/src/main/java/org/apache/shenyu/plugin/dubbo/common/cache/DubboParam.java
@@ -22,72 +22,163 @@ package org.apache.shenyu.plugin.dubbo.common.cache;
  */
 public class DubboParam {
 
+    /**
+     * the group.
+     */
     private String group;
 
+    /**
+     * the version.
+     */
     private String version;
 
+    /**
+     * the loadbalance.
+     */
     private String loadbalance;
 
+    /**
+     * the retries.
+     */
     private Integer retries;
 
+    /**
+     * the timeout.
+     */
     private Integer timeout;
 
+    /**
+     * the url.
+     */
     private String url;
 
+    /**
+     * the sent.
+     */
     private Boolean sent;
 
+    /**
+     * Gets group.
+     *
+     * @return the group
+     */
     public String getGroup() {
         return group;
     }
 
+    /**
+     * Sets group.
+     *
+     * @param group the group
+     */
     public void setGroup(final String group) {
         this.group = group;
     }
 
+    /**
+     * Gets version.
+     *
+     * @return the version
+     */
     public String getVersion() {
         return version;
     }
 
+    /**
+     * Sets version.
+     *
+     * @param version the version
+     */
     public void setVersion(final String version) {
         this.version = version;
     }
 
+    /**
+     * Gets loadbalance.
+     *
+     * @return the loadbalance
+     */
     public String getLoadbalance() {
         return loadbalance;
     }
 
+    /**
+     * Sets loadbalance.
+     *
+     * @param loadbalance the loadbalance
+     */
     public void setLoadbalance(final String loadbalance) {
         this.loadbalance = loadbalance;
     }
 
+    /**
+     * Gets retries.
+     *
+     * @return the retries
+     */
     public Integer getRetries() {
         return retries;
     }
 
+    /**
+     * Sets retries.
+     *
+     * @param retries the retries
+     */
     public void setRetries(final Integer retries) {
         this.retries = retries;
     }
 
+    /**
+     * Gets timeout.
+     *
+     * @return the timeout
+     */
     public Integer getTimeout() {
         return timeout;
     }
 
+    /**
+     * Sets timeout.
+     *
+     * @param timeout the timeout
+     */
     public void setTimeout(final Integer timeout) {
         this.timeout = timeout;
     }
 
+    /**
+     * Gets url.
+     *
+     * @return the url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets url.
+     *
+     * @param url the url
+     */
     public void setUrl(final String url) {
         this.url = url;
     }
 
+    /**
+     * Gets sent.
+     *
+     * @return the sent
+     */
     public Boolean getSent() {
         return sent;
     }
 
+    /**
+     * Sets sent.
+     *
+     * @param sent the sent
+     */
     public void setSent(final Boolean sent) {
         this.sent = sent;
     }
