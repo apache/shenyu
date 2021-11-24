725acced1405c3cbc1682c41e6fe068e26495f3a
likeguo <li_keguo@163.com>
2021-11-24 12:50:49 +0800

    clean code:clean warning

--- shenyu-plugin/shenyu-plugin-sofa/src/test/java/org/apache/shenyu/plugin/sofa/proxy/SofaProxyServiceTest.java
+++ shenyu-plugin/shenyu-plugin-sofa/src/test/java/org/apache/shenyu/plugin/sofa/proxy/SofaProxyServiceTest.java
@@ -81,7 +81,7 @@ public final class SofaProxyServiceTest {
     
     @Test
     @SuppressWarnings("all")
-    public void testGenericInvoker() throws  IllegalAccessException {
+    public void testGenericInvoker() throws IllegalAccessException {
         ConsumerConfig consumerConfig = mock(ConsumerConfig.class);
         GenericService genericService = mock(GenericService.class);
         when(consumerConfig.refer()).thenReturn(genericService);
