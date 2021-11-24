19acc0fef148af8b20c7e77fb69adb65d30192dd
haibo.duan <dhaibo1986@live.cn>
2021-11-24 19:22:36 +0800

    [ISSUE #2236]  Add unit test for URIPlugin. fix checkstyle error.

--- shenyu-plugin/shenyu-plugin-uri/src/test/java/org/apache/shenyu/plugin/uri/URIPluginTest.java
+++ shenyu-plugin/shenyu-plugin-uri/src/test/java/org/apache/shenyu/plugin/uri/URIPluginTest.java
@@ -1,5 +1,21 @@
-package org.apache.shenyu.plugin.uri;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package org.apache.shenyu.plugin.uri;
 
 import org.apache.shenyu.common.constant.Constants;
 import org.apache.shenyu.common.enums.PluginEnum;
@@ -30,10 +46,14 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class URIPluginTest {
 
-    MockServerHttpRequest request;
+    private MockServerHttpRequest request;
+    
     private URIPlugin uriPlugin;
+    
     private ServerWebExchange exchange;
+    
     private ShenyuPluginChain chain;
+    
     private ShenyuContext shenyuContext;
 
     @Before
