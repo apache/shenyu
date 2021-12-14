9d8f7f31bd576844f7ce73b4c71fcb1e41f12028
xiaoyu <xiaoyu@apache.org>
2021-12-14 17:05:50 +0800

    [fix: bug] Custom spi config

--- shenyu-common/src/main/java/org/apache/shenyu/common/enums/OperatorEnum.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/enums/OperatorEnum.java
@@ -17,8 +17,6 @@
 
 package org.apache.shenyu.common.enums;
 
-import org.apache.shenyu.common.exception.ShenyuException;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -123,6 +121,6 @@ public enum OperatorEnum {
     public static OperatorEnum getOperatorEnumByAlias(final String alias) {
         return Arrays.stream(OperatorEnum.values())
                 .filter(e -> e.getAlias().equals(alias) && e.support).findFirst()
-                .orElseThrow(() -> new ShenyuException(String.format(" this  operator can not support %s ", alias)));
+                .orElse(null);
     }
 }
