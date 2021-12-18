645303730269332def2d19a3da7c6d5aa781e54a
Qicz <qiczzhu@gmail.com>
2021-12-18 11:56:50 +0800

    [type: refactor] list all common binary media types. (#2616)
    * [type: refactor] list all common binary media types.
    
    * polish

--- shenyu-plugin/shenyu-plugin-response/src/main/java/org/apache/shenyu/plugin/response/strategy/WebClientMessageWriter.java
+++ shenyu-plugin/shenyu-plugin-response/src/main/java/org/apache/shenyu/plugin/response/strategy/WebClientMessageWriter.java
@@ -30,12 +30,20 @@ import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.StringJoiner;
 
 /**
  * The type Web client message writer.
  */
 public class WebClientMessageWriter implements MessageWriter {
+
+    /**
+     * the common binary media type regex.
+     */
+    private static final String COMMON_BIN_MEDIA_TYPE_REGEX;
 
     @Override
     public Mono<Void> writeWith(final ServerWebExchange exchange, final ShenyuPluginChain chain) {
@@ -57,9 +65,7 @@ public class WebClientMessageWriter implements MessageWriter {
             // image, pdf or stream does not do format processing.
             if (clientResponse.headers().contentType().isPresent()) {
                 final String media = clientResponse.headers().contentType().get().toString().toLowerCase();
-                if (media.contains(Constants.IMAGE_MEDIA)
-                        || media.contains(Constants.STREAM_MEDIA)
-                        || media.contains(Constants.PDF_MEDIA)) {
+                if (media.matches(COMMON_BIN_MEDIA_TYPE_REGEX)) {
                     return response.writeWith(clientResponse.body(BodyExtractors.toDataBuffers()))
                             .doOnCancel(() -> clean(exchange));
                 }
@@ -76,5 +82,44 @@ public class WebClientMessageWriter implements MessageWriter {
         if (Objects.nonNull(clientResponse)) {
             clientResponse.bodyToMono(Void.class).subscribe();
         }
+    }
+
+    static {
+        // https://www.iana.org/assignments/media-types/media-types.xhtml
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+        // https://en.wikipedia.org/wiki/Media_type
+        // image => .png .jpg .jpeg .gif .webp
+        // audio => .mp2 .mp3
+        // video => .avi .mp4
+        // application/ogg => ogg
+        // zip => .zip .tar .gz
+        // rar => .rar
+        // word => .doc
+        // excel => .xls
+        // csv => .csv
+        // powerpoint => .ppt
+        // openxmlformats-officedocument => .pptx .xlsx .docx
+        // binary => .bin
+        // pdf => .pdf
+        Set<String> commonBinaryTypes = new HashSet<String>() {
+            {
+                add("image");
+                add("audio");
+                add("video");
+                add("ogg");
+                add("zip");
+                add("rar");
+                add("word");
+                add("excel");
+                add("csv");
+                add("powerpoint");
+                add("openxmlformats-officedocument");
+                add("binary");
+                add("pdf");
+            }
+        };
+        StringJoiner regexBuilder = new StringJoiner("|");
+        commonBinaryTypes.forEach(t -> regexBuilder.add(String.format(".*%s.*", t)));
+        COMMON_BIN_MEDIA_TYPE_REGEX = regexBuilder.toString();
     }
 }
