374c2992e5d9ca884beced19ae920d22a1a45d64
Sinsy <550569627@qq.com>
2021-12-22 14:10:42 +0800

    fix install.sh (#2637)

--- shenyu-dist/shenyu-docker-compose-dist/src/main/resources/install.sh
+++ shenyu-dist/shenyu-docker-compose-dist/src/main/resources/install.sh
@@ -18,21 +18,27 @@
 version=${1}
 storage=${2}
 
+echo "current version: ${version}"
+echo "current storage: ${storage}"
+
 mkdir shenyu-${version}
 
 cd shenyu-${version}
 
 mkdir -p {shenyu-bootstrap,shenyu-admin}/{conf,logs}
 
+echo "download docker-compose configuration"
 curl -sSl https://raw.githubusercontent.com/apache/incubator-shenyu/${version}/shenyu-dist/shenyu-docker-compose-dist/src/main/resources/stand-alone-${storage}/docker-compose.yaml > docker-compose.yaml
 
-# shellcheck disable=SC2205
-if ( "${storage}" = 'mysql') then
+if [ "$storage" = "mysql" ];then
   mkdir -p shenyu-admin/ext-lib
-  (cd shenyu-admin/ext-lib && curl -sSl https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.18/mysql-connector-java-8.0.18.jar > mysql-connector.jar)
+  echo "download mysql-connector.jar"
+  (cd shenyu-admin/ext-lib && curl -o mysql-connector.jar https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.18/mysql-connector-java-8.0.18.jar)
 fi
 
+echo "download shenyu-admin of configuration"
 (cd shenyu-admin/conf/ && curl -OOO https://raw.githubusercontent.com/apache/incubator-shenyu/${version}/shenyu-admin/src/main/resources/{application-mysql.yml,logback.xml,application.yml})
+echo "download shenyu-bootstrap of configuration"
 (cd shenyu-bootstrap/conf/ && curl -OOO https://raw.githubusercontent.com/apache/incubator-shenyu/${version}/shenyu-bootstrap/src/main/resources/{application-local.yml,logback.xml,application.yml})
 
 docker-compose up -d
