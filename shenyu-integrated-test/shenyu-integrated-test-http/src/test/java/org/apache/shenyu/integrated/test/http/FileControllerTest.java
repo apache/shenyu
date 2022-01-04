58c17d2da9c45944e7beededb9b0f123d89a04d9
qinghai777 <2683064908@qq.com>
2022-01-05 01:11:48 +0800

    aboutFileControllerAndTest

--- shenyu-integrated-test/shenyu-integrated-test-http/src/test/java/org/apache/shenyu/integrated/test/http/FileControllerTest.java
+++ shenyu-integrated-test/shenyu-integrated-test-http/src/test/java/org/apache/shenyu/integrated/test/http/FileControllerTest.java
@@ -17,25 +17,70 @@
 
 package org.apache.shenyu.integrated.test.http;
 
-
-import io.grpc.LoadBalancer;
-import org.apache.shenyu.examples.http.controller.FileController;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.MediaType;
 import org.apache.shenyu.integratedtest.common.helper.HttpHelper;
 import org.junit.Test;
-import org.springframework.web.multipart.MultipartFile;
 import static org.junit.Assert.assertEquals;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FileControllerTest {
     @Test
-    public void TestFileUploadWay1() throws Exception {
-         File file=new File("D:/文件/测试文件.txt");
-         FileInputStream input = new FileInputStream(file);
-         MultipartFile multipartFile = new org.springframework.mock.web.MockMultipartFile("file", file.getName(), "text/plain", org.apache.commons.io.IOUtils.toByteArray(input));
-         FileController fileController1=new FileController();
-         FileController fileController2= HttpHelper.INSTANCE.postGateway("/http/file/uploadWay1",fileController1,FileController.class);
-         assertEquals("上传成功",fileController2.fileUploadWay1(multipartFile,"D:/image"));
+    public void testFileUploadWay1() throws Exception {
+        String url = "http://localhost:9195/http/file/uploadWay1";
+        String filePath1 = "D:\\interview\\interview2\\shenyu\\incubator-shenyu\\shenyu-examples\\shenyu-examples-http\\src\\main\\resources\\static\\test.txt";
+        String filePath2 = "D:\\interview\\interview2\\shenyu\\incubator-shenyu\\shenyu-examples\\shenyu-examples-http\\src\\main\\resources\\static\\test";
+        assertEquals("测试成功", post(url, filePath1));
+        assertEquals("测试失败", post(url, filePath2));
     }
 
+    @Test
+    public void testFileUploadWay2() throws Exception {
+        String res1 = HttpHelper.INSTANCE.postGateway("/http/file/uploadWay2?file=测试成功&fileName=test.txt&filePath=shenyu-examples/shenyu-examples-http/src/main/resources/static/",
+                java.lang.String.class);
+        assertEquals("上传成功", res1);
+        String res2 = HttpHelper.INSTANCE.postGateway("/http/file/uploadWay2?file=测试成功&fileName=test.txt&filePath=shenyu/image/ss/", java.lang.String.class);
+        assertEquals("上传失败", res2);
+    }
+
+    @Test
+    public void testFileDownload() throws Exception {
+        String res1 = HttpHelper.INSTANCE.postGateway("/http/file/download?filePath=shenyu-examples/shenyu-examples-http/src/main/resources/static/test.txt", java.lang.String.class);
+        String res2 = HttpHelper.INSTANCE.postGateway("/http/file/download?filePath=shenyu-examples/shenyu-examples-http/src/main/resources/static/test", java.lang.String.class);
+        assertEquals("测试成功", res1);
+        assertEquals("下载失败", res2);
+    }
+
+    /**
+     * Post file.
+     *
+     * @param url the url
+     * @param filePath the filePath
+     * @return the String
+     * @throws IOException the IOException
+     */
+    public String post(final String url, final String filePath) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        File file = new File(filePath);
+        RequestBody fileBody = RequestBody.create(MediaType.parse("application/octet-stream"), file);
+        RequestBody requestBody = new MultipartBody.Builder()
+             .setType(MultipartBody.FORM)
+             .addFormDataPart("application/octet-stream", file.getName(), fileBody)
+             .build();
+        Request request = new Request.Builder()
+             .url(url)
+             .post(requestBody)
+             .build();
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (Exception e) {
+            return "测试失败";
+        }
+        return "测试成功";
+    }
 }
