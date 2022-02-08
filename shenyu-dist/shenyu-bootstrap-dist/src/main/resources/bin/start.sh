ba335b74f6b248d015fa4e52fd885200648bebb4
sinsy <550569627@qq.com>
2022-02-08 17:01:15 +0800

    fix: double log

--- shenyu-dist/shenyu-bootstrap-dist/src/main/resources/bin/start.sh
+++ shenyu-dist/shenyu-bootstrap-dist/src/main/resources/bin/start.sh
@@ -55,10 +55,10 @@ MAIN_CLASS=org.apache.shenyu.bootstrap.ShenyuBootstrapApplication
 if [[ "$1" == "agent" ]]; then
     echo "Starting the $SERVER_NAME with shenyu-agent ..."
     SHENYU_AGENT=${DEPLOY_DIR}/agent/shenyu-agent.jar
-    nohup java ${JAVA_OPTS} -javaagent:${SHENYU_AGENT} -classpath ${CLASS_PATH} ${MAIN_CLASS} >> ${LOG_FILES} 2>&1 &
+    nohup java ${JAVA_OPTS} -javaagent:${SHENYU_AGENT} -classpath ${CLASS_PATH} ${MAIN_CLASS} > /dev/null 2>&1 &
 else
     echo "Starting the $SERVER_NAME ..."
-    nohup java ${JAVA_OPTS} -classpath ${CLASS_PATH} ${MAIN_CLASS} >> ${LOG_FILES} 2>&1 &
+    nohup java ${JAVA_OPTS} -classpath ${CLASS_PATH} ${MAIN_CLASS} >/dev/null 2>&1 &
 fi
 
 sleep 1
