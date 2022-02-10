2ef2441c960596aeacd91ab2a0f7549f8f669a7b
haibo-duan <dhaibo1986@live.cn>
2022-02-10 22:14:31 +0800

    [Task] Update junit4 to junit5  in shenyu-plugin-tars module

--- shenyu-plugin/shenyu-plugin-tars/src/test/java/org/apache/shenyu/plugin/tars/handler/TarsPluginDataHandlerTest.java
+++ shenyu-plugin/shenyu-plugin-tars/src/test/java/org/apache/shenyu/plugin/tars/handler/TarsPluginDataHandlerTest.java
@@ -19,23 +19,23 @@ package org.apache.shenyu.plugin.tars.handler;
 
 import org.apache.shenyu.common.dto.PluginData;
 import org.apache.shenyu.common.enums.PluginEnum;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test case for {@link TarsPluginDataHandler}.
  */
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public final class TarsPluginDataHandlerTest {
     
     private TarsPluginDataHandler tarsPluginDataHandlerUnderTest;
     
-    @Before
+    @BeforeEach
     public void setUp() {
         tarsPluginDataHandlerUnderTest = new TarsPluginDataHandler();
     }
