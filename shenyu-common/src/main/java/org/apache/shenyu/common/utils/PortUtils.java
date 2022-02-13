f5e6a31d00adf6796b58fd9d81484ebe6b5478a0
hailang <hailang@webuy.ai>
2022-02-13 15:39:43 +0800

    fix code style

--- shenyu-common/src/main/java/org/apache/shenyu/common/utils/PortUtils.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/utils/PortUtils.java
@@ -36,14 +36,15 @@ public class PortUtils {
     /**
      * Note: springboot 1.x version has been made compatible.
      * Note: springmvc with external tomcat had been made compatible.
-     * Note: In this way, no matter what container is actually used,
-     * you can get the port that is actually started in the end.
+     * Note: In this way, no matter what container is actually used, you can get the port that is actually started in the end.
+     * Note: see {@code org.springframework.boot.context.embedded.AbstractConfigurableEmbeddedServletContainer#getPort()}
+     * for springboot 1.x;
+     * Note: see {@code org.springframework.boot.web.server.AbstractConfigurableWebServerFactory#getPort()}
+     * for springboot 2.x
      *
      * @param beanFactory beanFactory
      * @return port number
      * @throws ShenyuException when can not find port
-     * @see org.springframework.boot.context.embedded.AbstractConfigurableEmbeddedServletContainer#getPort()
-     * @see org.springframework.boot.web.server.AbstractConfigurableWebServerFactory#getPort()
      */
     @SuppressWarnings("all")
     public static int findPort(final BeanFactory beanFactory) {
