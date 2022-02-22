45d436e53439dd3f7c05dabf6d38b8dcf73a4db8
Sinsy <550569627@qq.com>
2022-02-22 20:45:14 +0800

    [type: refactor ] refactor jwt support multi-level tokens (#2918)
    * refactor jwt
    
    * refactor parse
    
    * delete redundant import

--- shenyu-plugin/shenyu-plugin-jwt/src/test/java/org/apache/shenyu/plugin/jwt/JwtPluginTest.java
+++ shenyu-plugin/shenyu-plugin-jwt/src/test/java/org/apache/shenyu/plugin/jwt/JwtPluginTest.java
@@ -33,20 +33,21 @@ import org.apache.shenyu.plugin.jwt.handle.JwtPluginDataHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -65,12 +66,6 @@ public final class JwtPluginTest {
 
     private RuleData ruleData;
 
-    private String token;
-
-    private List<JwtRuleHandle.Convert> converts;
-
-    private JwtRuleHandle.Convert convert;
-
     private JwtRuleHandle jwtRuleHandle;
 
     @BeforeEach
@@ -79,59 +74,73 @@ public final class JwtPluginTest {
         when(context.getBean(ShenyuResult.class)).thenReturn(new DefaultShenyuResult());
         SpringBeanUtils springBeanUtils = SpringBeanUtils.getInstance();
         springBeanUtils.setApplicationContext(context);
-        final PluginData pluginData =
-                new PluginData("pluginId", "pluginName", "{\"secretKey\":\"shenyu\"}", "0", false);
+        PluginData pluginData = new PluginData("pluginId", "pluginName", "{\"secretKey\":\"shenyu\"}", "0", false);
         JwtPluginDataHandler jwtPluginDataHandler = new JwtPluginDataHandler();
         jwtPluginDataHandler.handlerPlugin(pluginData);
-        exchange = MockServerWebExchange.from(MockServerHttpRequest.get("localhost").build());
-        chain = mock(ShenyuPluginChain.class);
-        when(this.chain.execute(exchange)).thenReturn(Mono.empty());
         selectorData = mock(SelectorData.class);
-        ruleData = mock(RuleData.class);
+        ruleData = new RuleData();
         jwtPluginUnderTest = new JwtPlugin();
-        converts = new ArrayList<>();
-        convert = mock(JwtRuleHandle.Convert.class);
-        String secreteKey = "shenyu";
+        final String secreteKey = "shenyu";
         Map<String, Object> map = new HashMap<>();
         map.put("userId", 1);
+        Map<String, Object> multi = new HashMap<>();
+        multi.put("shenyu", "1.2.3");
+        map.put("web", multi);
         Date date = new Date();
         date.setTime(1636371125000L);
-        token = Jwts.builder()
+        String token = Jwts.builder()
                 .setIssuedAt(date)
                 .setExpiration(new Date())
                 .setClaims(map)
-                .signWith(SignatureAlgorithm.HS256, secreteKey)
+                .signWith(SignatureAlgorithm.HS256, secreteKey.getBytes(StandardCharsets.UTF_8))
                 .compact();
-        jwtRuleHandle = mock(JwtRuleHandle.class);
+        jwtRuleHandle = new JwtRuleHandle();
 
+        exchange = MockServerWebExchange.from(MockServerHttpRequest
+                .get("localhost")
+                .header("token", token)
+                .build());
+        chain = mock(ShenyuPluginChain.class);
     }
 
     @Test
     public void testSecreteKey() {
-        ServerHttpRequest newRequest = exchange.getRequest().mutate().header("token", this.token).build();
-        ServerWebExchange webExchange = exchange.mutate().request(newRequest).build();
-        Mono<Void> mono = jwtPluginUnderTest.doExecute(webExchange, chain, selectorData, ruleData);
-        StepVerifier.create(mono).expectSubscription().verifyComplete();
-    }
-
-    @Test
-    public void testCompatible() {
-        ServerHttpRequest newRequest = exchange.getRequest().mutate().header("token", this.token).build();
-        ServerWebExchange webExchange = exchange.mutate().request(newRequest).build();
-        Mono<Void> mono = jwtPluginUnderTest.doExecute(webExchange, chain, selectorData, ruleData);
+        PluginData pluginData = new PluginData("pluginId", "pluginName", "{\"secretKey\":\"\"}", "0", false);
+        JwtPluginDataHandler jwtPluginDataHandler = new JwtPluginDataHandler();
+        jwtPluginDataHandler.handlerPlugin(pluginData);
+        Mono<Void> mono = jwtPluginUnderTest.doExecute(exchange, chain, selectorData, ruleData);
         StepVerifier.create(mono).expectSubscription().verifyComplete();
     }
 
     @Test
     public void testConverter() {
-        final ServerHttpRequest newRequest = exchange.getRequest().mutate().header("token", this.token).build();
+        List<JwtRuleHandle.Convert> converts = new ArrayList<>();
+        JwtRuleHandle.Convert convert = new JwtRuleHandle.Convert();
         convert.setJwtVal("userId");
         convert.setHeaderVal("userId");
         converts.add(convert);
         jwtRuleHandle.setConverter(converts);
         ruleData.setHandle(GsonUtils.getGson().toJson(jwtRuleHandle));
-        ServerWebExchange webExchange = exchange.mutate().request(newRequest).build();
-        Mono<Void> mono = jwtPluginUnderTest.doExecute(webExchange, chain, selectorData, ruleData);
+        when(this.chain.execute(any())).thenReturn(Mono.empty());
+        Mono<Void> mono = jwtPluginUnderTest.doExecute(exchange, chain, selectorData, ruleData);
+        StepVerifier.create(mono).expectSubscription().verifyComplete();
+    }
+
+    @Test
+    public void testMulti() {
+        final List<JwtRuleHandle.Convert> converts = new ArrayList<>();
+        JwtRuleHandle.Convert webConvert = new JwtRuleHandle.Convert();
+        webConvert.setJwtVal("web.shenyu");
+        webConvert.setHeaderVal("shenyu");
+        JwtRuleHandle.Convert userConvert = new JwtRuleHandle.Convert();
+        userConvert.setJwtVal("userId");
+        userConvert.setHeaderVal("userId");
+        converts.add(userConvert);
+        converts.add(webConvert);
+        jwtRuleHandle.setConverter(converts);
+        ruleData.setHandle(GsonUtils.getGson().toJson(jwtRuleHandle));
+        when(this.chain.execute(any())).thenReturn(Mono.empty());
+        Mono<Void> mono = jwtPluginUnderTest.doExecute(exchange, chain, selectorData, ruleData);
         StepVerifier.create(mono).expectSubscription().verifyComplete();
     }
 
