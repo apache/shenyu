1a73905c95bd926fcbaaec0917ef17ded97b36d8
hailang <hailang@webuy.ai>
2022-02-26 10:58:37 +0800

    [ISSUE #2627] support failover RetryStrategy

--- shenyu-plugin/shenyu-plugin-httpclient/src/main/java/org/apache/shenyu/plugin/httpclient/WebClientPlugin.java
+++ shenyu-plugin/shenyu-plugin-httpclient/src/main/java/org/apache/shenyu/plugin/httpclient/WebClientPlugin.java
@@ -20,14 +20,23 @@ package org.apache.shenyu.plugin.httpclient;
 import io.netty.channel.ConnectTimeoutException;
 import io.netty.handler.timeout.ReadTimeoutException;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shenyu.common.constant.Constants;
+import org.apache.shenyu.common.enums.LoadBalanceEnum;
 import org.apache.shenyu.common.enums.PluginEnum;
 import org.apache.shenyu.common.enums.ResultEnum;
+import org.apache.shenyu.common.enums.RetryEnum;
+import org.apache.shenyu.common.exception.ShenyuException;
+import org.apache.shenyu.common.utils.GsonUtils;
+import org.apache.shenyu.loadbalancer.cache.UpstreamCacheManager;
+import org.apache.shenyu.loadbalancer.entity.Upstream;
+import org.apache.shenyu.loadbalancer.factory.LoadBalancerFactory;
 import org.apache.shenyu.plugin.api.ShenyuPlugin;
 import org.apache.shenyu.plugin.api.ShenyuPluginChain;
 import org.apache.shenyu.plugin.api.context.ShenyuContext;
 import org.apache.shenyu.plugin.api.result.ShenyuResultEnum;
 import org.apache.shenyu.plugin.api.result.ShenyuResultWrap;
+import org.apache.shenyu.plugin.api.utils.RequestQueryCodecUtil;
 import org.apache.shenyu.plugin.api.utils.WebFluxResultUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -79,10 +88,19 @@ public class WebClientPlugin implements ShenyuPlugin {
         }
         long timeout = (long) Optional.ofNullable(exchange.getAttribute(Constants.HTTP_TIME_OUT)).orElse(3000L);
         int retryTimes = (int) Optional.ofNullable(exchange.getAttribute(Constants.HTTP_RETRY)).orElse(0);
-        LOG.info("The request urlPath is {}, retryTimes is {}", uri.toASCIIString(), retryTimes);
-        HttpMethod method = HttpMethod.valueOf(exchange.getRequest().getMethodValue());
-        WebClient.RequestBodySpec requestBodySpec = webClient.method(method).uri(uri);
-        return handleRequestBody(requestBodySpec, exchange, timeout, retryTimes, chain);
+        final String retryStrategy = (String) Optional.ofNullable(exchange.getAttribute(Constants.RETRY_STRATEGY)).orElseGet(RetryEnum.CURRENT::getName);
+        LOG.info("The request urlPath is {}, retryStrategy is{}, retryTimes is {}", uri.toASCIIString(), retryStrategy, retryTimes);
+        final HttpMethod method = HttpMethod.valueOf(exchange.getRequest().getMethodValue());
+        final Mono<ClientResponse> clientResponse = handleRequest(method, uri, exchange, timeout);
+        if (RetryEnum.CURRENT.getName().equals(retryStrategy)) {
+            //old version of DividePlugin and SpringCloudPlugin will run on this
+            return clientResponse.retryWhen(Retry.anyOf(TimeoutException.class, ConnectTimeoutException.class, ReadTimeoutException.class)
+                    .retryMax(retryTimes)
+                    .backoff(Backoff.exponential(Duration.ofMillis(200), Duration.ofSeconds(20), 2, true)))
+                    .flatMap(e -> doNext(e, exchange, chain));
+        }
+        return resend(clientResponse, method, uri, exchange, timeout, shenyuContext, retryTimes)
+                .flatMap(e -> doNext(e, exchange, chain));
     }
 
     @Override
@@ -100,12 +118,75 @@ public class WebClientPlugin implements ShenyuPlugin {
         return skipExceptHttpLike(exchange);
     }
 
-    private Mono<Void> handleRequestBody(final WebClient.RequestBodySpec requestBodySpec,
-                                         final ServerWebExchange exchange,
-                                         final long timeout,
-                                         final int retryTimes,
-                                         final ShenyuPluginChain chain) {
-        return requestBodySpec.headers(httpHeaders -> {
+    private Mono<ClientResponse> resend(Mono<ClientResponse> clientResponse,
+                                        final HttpMethod method,
+                                        final URI uri,
+                                        final ServerWebExchange exchange,
+                                        final long timeout,
+                                        final ShenyuContext shenyuContext,
+                                        final int retryTimes) {
+        for (int i = 0; i < retryTimes; i++) {
+            clientResponse = resend(clientResponse, method, uri, exchange, timeout, shenyuContext);
+        }
+        return clientResponse;
+    }
+
+    private Mono<ClientResponse> resend(final Mono<ClientResponse> clientResponse,
+                                        final HttpMethod method,
+                                        final URI uri,
+                                        final ServerWebExchange exchange,
+                                        final long timeout,
+                                        final ShenyuContext shenyuContext) {
+        //todo How to add backoff ?
+        return clientResponse.onErrorResume(e -> {
+            final String hostAndPort = uri.getHost() + ":" + uri.getPort();
+            final String selectorId = exchange.getAttribute(Constants.DIVIDE_SELECTOR_ID);
+            List<Upstream> upstreamList = UpstreamCacheManager.getInstance().findUpstreamListBySelectorId(selectorId)
+                    .stream().filter(data -> !hostAndPort.equals(data.getUrl().trim())).collect(Collectors.toList());
+            if (CollectionUtils.isEmpty(upstreamList)) {
+                LOG.error("upstream configuration error： {}", GsonUtils.getInstance().toJson(upstreamList));
+                return Mono.error(new ShenyuException(ShenyuResultEnum.CANNOT_FIND_HEALTHY_UPSTREAM_URL.getMsg()));
+            }
+            final String loadBalance = (String) Optional.ofNullable(exchange.getAttribute(Constants.LOAD_BALANCE)).orElseGet(LoadBalanceEnum.RANDOM::getName);
+            String ip = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress();
+            Upstream upstream = LoadBalancerFactory.selector(upstreamList, loadBalance, ip);
+            if (Objects.isNull(upstream)) {
+                LOG.error("has no upstream");
+                return Mono.error(new ShenyuException(ShenyuResultEnum.CANNOT_FIND_HEALTHY_UPSTREAM_URL.getMsg()));
+            }
+            final URI newUri = buildUri(upstream, exchange, shenyuContext);
+            return handleRequest(method, newUri, exchange, timeout);
+        });
+    }
+
+    private URI buildUri(final Upstream upstream,
+                         final ServerWebExchange exchange,
+                         final ShenyuContext shenyuContext) {
+        String protocol = upstream.getProtocol();
+        if (StringUtils.isBlank(protocol)) {
+            protocol = "http://";
+        }
+        String path = protocol + upstream.getUrl().trim();
+        String rewriteUri = (String) exchange.getAttributes().get(Constants.REWRITE_URI);
+        if (StringUtils.isNoneBlank(rewriteUri)) {
+            path = path + rewriteUri;
+        } else {
+            String realUrl = shenyuContext.getRealUrl();
+            if (StringUtils.isNoneBlank(realUrl)) {
+                path = path + realUrl;
+            }
+        }
+        if (StringUtils.isNoneBlank(exchange.getRequest().getURI().getQuery())) {
+            path = String.join("?", path, RequestQueryCodecUtil.getCodecQuery(exchange));
+        }
+        return URI.create(path);
+    }
+
+    private Mono<ClientResponse> handleRequest(final HttpMethod method,
+                                               final URI uri,
+                                               final ServerWebExchange exchange,
+                                               final long timeout) {
+        return webClient.method(method).uri(uri).headers(httpHeaders -> {
             httpHeaders.addAll(exchange.getRequest().getHeaders());
             // remove gzip
             List<String> acceptEncoding = httpHeaders.get(HttpHeaders.ACCEPT_ENCODING);
@@ -119,11 +200,7 @@ public class WebClientPlugin implements ShenyuPlugin {
                 .body(BodyInserters.fromDataBuffers(exchange.getRequest().getBody()))
                 .exchange()
                 .doOnError(e -> LOG.error(e.getMessage(), e))
-                .timeout(Duration.ofMillis(timeout))
-                .retryWhen(Retry.anyOf(TimeoutException.class, ConnectTimeoutException.class, ReadTimeoutException.class)
-                        .retryMax(retryTimes)
-                        .backoff(Backoff.exponential(Duration.ofMillis(200), Duration.ofSeconds(20), 2, true)))
-                .flatMap(e -> doNext(e, exchange, chain));
+                .timeout(Duration.ofMillis(timeout));
     }
 
     private Mono<Void> doNext(final ClientResponse res, final ServerWebExchange exchange, final ShenyuPluginChain chain) {
