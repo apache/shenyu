721a92c3d595e2f3c2df65c573f8c4c2f5dbf387
YuI <13451528+HessTina-YuI@users.noreply.github.com>
2022-03-03 10:40:38 +0800

    [type: feature]: add shenyu_dubbo_request_total metrics. (#2944)
    Co-authored-by: 艺铭 <yi.liu@perfma.com>

--- shenyu-agent/shenyu-agent-plugins/shenyu-agent-plugin-metrics/shenyu-agent-plugin-metrics-api/src/main/java/org/apache/shenyu/agent/plugin/metrics/api/constant/MetricsConstant.java
+++ shenyu-agent/shenyu-agent-plugins/shenyu-agent-plugin-metrics/shenyu-agent-plugin-metrics-api/src/main/java/org/apache/shenyu/agent/plugin/metrics/api/constant/MetricsConstant.java
@@ -43,13 +43,43 @@ public final class MetricsConstant {
     public static final String HTTP_REQUEST_TOTAL = "shenyu_http_request_total";
 
     /**
-     * The constant SHENYU_REQUEST_UNDONE.
+     * The constant DUBBO_REQUEST_TOTAL.
      */
-    public static final String SHENYU_REQUEST_UNDONE = "shenyu_request_undone";
+    public static final String DUBBO_REQUEST_TOTAL = "shenyu_dubbo_request_total";
 
     /**
-     * The constant SHENYU_EXECUTE_LATENCY_MILLIS.
+     * The constant GRPC_REQUEST_TOTAL.
      */
-    public static final String SHENYU_EXECUTE_LATENCY_MILLIS = "shenyu_execute_latency_millis";
+    public static final String GRPC_REQUEST_TOTAL = "shenyu_grpc_request_total";
+
+    /**
+     * The constant MOTAN_REQUEST_TOTAL.
+     */
+    public static final String MOTAN_REQUEST_TOTAL = "shenyu_motan_request_total";
+
+    /**
+     * The constant SOFA_REQUEST_TOTAL.
+     */
+    public static final String SOFA_REQUEST_TOTAL = "shenyu_sofa_request_total";
+
+    /**
+     * The constant TARS_REQUEST_TOTAL.
+     */
+    public static final String TARS_REQUEST_TOTAL = "shenyu_tars_request_total";
+
+    /**
+     * The constant SPRING_CLOUD_REQUEST_TOTAL.
+     */
+    public static final String SPRING_CLOUD_REQUEST_TOTAL = "shenyu_spring_cloud_request_total";
+
+    /**
+     * The constant REQUEST_UNDONE.
+     */
+    public static final String REQUEST_UNDONE = "shenyu_request_undone";
+
+    /**
+     * The constant EXECUTE_LATENCY_MILLIS.
+     */
+    public static final String EXECUTE_LATENCY_MILLIS = "shenyu_execute_latency_millis";
 
 }
