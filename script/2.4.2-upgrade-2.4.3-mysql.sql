e24a8d86b4244fc8fbbe21cfde3d3bd9714d0855
erdengk <15596570256@163.com>
2022-03-04 14:29:35 +0800

    fix shaUtils

--- script/2.4.2-upgrade-2.4.3-mysql.sql
+++ script/2.4.2-upgrade-2.4.3-mysql.sql
@@ -17,4 +17,4 @@
 -- this file works for MySQL.
 
 -- update admin password
-UPDATE dashboard_user SET password='ba3253876aed6bc22d4a6ff53d846c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413' WHERE user_name = 'admin';
+UPDATE dashboard_user SET password='ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413' WHERE user_name = 'admin';
