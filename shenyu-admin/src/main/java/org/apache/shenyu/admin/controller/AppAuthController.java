5a721685dd4e7278661bf87b04478003eab1a7d9
admin <an88411980@gmail.com>
2022-03-27 13:50:22 +0800

    [fix admin #3088] : Click on the EditResourceDetails button error

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/AppAuthController.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/AppAuthController.java
@@ -35,7 +35,12 @@ import org.apache.shenyu.admin.utils.ShenyuResultMessage;
 import org.apache.shenyu.admin.validation.annotation.Existed;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
