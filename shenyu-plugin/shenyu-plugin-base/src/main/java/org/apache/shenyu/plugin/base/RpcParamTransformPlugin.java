79e3f1a637cb24d51497a4a0e05e4fa6d3afeb3b
moremind <hefengen@hotmail.com>
2022-04-03 23:19:47 +0800

    [ISSUE #3158] support big request body and formdata

--- shenyu-plugin/shenyu-plugin-base/src/main/java/org/apache/shenyu/plugin/base/RpcParamTransformPlugin.java
+++ shenyu-plugin/shenyu-plugin-base/src/main/java/org/apache/shenyu/plugin/base/RpcParamTransformPlugin.java
@@ -81,14 +81,14 @@ public class RpcParamTransformPlugin implements ShenyuPlugin {
     }
 
     private Mono<Void> formData(final ServerWebExchange exchange, final ServerHttpRequest serverHttpRequest, final ShenyuPluginChain chain) {
-        return Mono.from(serverHttpRequest.getBody()
+        return Mono.from(DataBufferUtils.join(serverHttpRequest.getBody())
                 .flatMap(map -> {
                     String param = resolveBodyFromRequest(map);
                     LinkedMultiValueMap<String, String> linkedMultiValueMap;
                     try {
                         linkedMultiValueMap = BodyParamUtils.buildBodyParams(URLDecoder.decode(param, StandardCharsets.UTF_8.name()));
                     } catch (UnsupportedEncodingException e) {
-                        return Flux.error(e);
+                        return Mono.error(e);
                     }
                     exchange.getAttributes().put(Constants.PARAM_TRANSFORM, HttpParamConverter.toMap(() -> linkedMultiValueMap));
                     return chain.execute(exchange);
