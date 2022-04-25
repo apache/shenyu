6284f7bc655ccc33bd6d7e13e5adc66fe50f04b4
moremind <hefengen@hotmail.com>
2022-04-25 12:26:08 +0800

    [ISSUE #3299] commit LocalDispatcherFilter test case

--- shenyu-web/src/test/java/org/apache/shenyu/web/filter/LocalDispatcherFilterTest.java
+++ shenyu-web/src/test/java/org/apache/shenyu/web/filter/LocalDispatcherFilterTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shenyu.web.filter;
 
 import org.apache.shenyu.common.utils.ShaUtils;
@@ -20,7 +37,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
- * test case for LocalDispatcherFilter
+ * test case for LocalDispatcherFilter.
  * @see LocalDispatcherFilter
  */
 @ExtendWith(MockitoExtension.class)
@@ -41,8 +58,8 @@ public class LocalDispatcherFilterTest {
     }
 
     /**
-     * test method for {@linkplain LocalDispatcherFilter#doFilter(ServerWebExchange, WebFilterChain)}
-     * execute {@linkplain LocalDispatcherFilter#doDenyResponse(ServerWebExchange)}
+     * test method for {@linkplain LocalDispatcherFilter#doFilter(ServerWebExchange, WebFilterChain)}.
+     * execute {@linkplain LocalDispatcherFilter#doDenyResponse(ServerWebExchange)}.
      */
     @Test
     public void testFilter() {
@@ -55,7 +72,7 @@ public class LocalDispatcherFilterTest {
     }
 
     /**
-     * test method for {@linkplain LocalDispatcherFilter#doFilter(ServerWebExchange, WebFilterChain)}
+     * test method for {@linkplain LocalDispatcherFilter#doFilter(ServerWebExchange, WebFilterChain)}.
      */
     @Test
     public void testFilterNotMatch() {
