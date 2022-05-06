0b69199e7ec070c0da47edb6e9951a4efddcb0d8
拉姆徐 <lahmxj@gmail.com>
2022-05-06 14:12:53 +0800

    [ISSUE #3118]  feature protocol-mqtt: enhanced mqtt plugin (#3377)
    * feature protocol-mqtt: handle pingReq message
    
    * feature protocol-mqtt: handle unSubscribe message

--- shenyu-protocol/shenyu-protocol-mqtt/src/main/java/org/apache/shenyu/protocol/mqtt/Connect.java
+++ shenyu-protocol/shenyu-protocol-mqtt/src/main/java/org/apache/shenyu/protocol/mqtt/Connect.java
@@ -22,29 +22,46 @@ import io.netty.handler.codec.mqtt.MqttConnAckMessage;
 import io.netty.handler.codec.mqtt.MqttConnectMessage;
 import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
 import io.netty.handler.codec.mqtt.MqttMessageBuilders;
+import io.netty.handler.codec.mqtt.MqttVersion;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shenyu.common.utils.Singleton;
 import org.apache.shenyu.protocol.mqtt.repositories.ChannelRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.netty.channel.ChannelFutureListener.CLOSE_ON_FAILURE;
+import static io.netty.handler.codec.mqtt.MqttConnectReturnCode.CONNECTION_REFUSED_BAD_USER_NAME_OR_PASSWORD;
+import static io.netty.handler.codec.mqtt.MqttConnectReturnCode.CONNECTION_REFUSED_IDENTIFIER_REJECTED;
+import static io.netty.handler.codec.mqtt.MqttConnectReturnCode.CONNECTION_REFUSED_UNACCEPTABLE_PROTOCOL_VERSION;
 
 /**
  * Client requests a connection to a server.
  */
 public class Connect extends MessageType {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Connect.class);
+
     @Override
     public void connect(final ChannelHandlerContext ctx, final MqttConnectMessage msg) {
 
         String clientId = msg.payload().clientIdentifier();
         if (StringUtils.isEmpty(clientId)) {
-            ctx.writeAndFlush(wrong(MqttConnectReturnCode.CONNECTION_REFUSED_IDENTIFIER_REJECTED));
+            LOG.info("MQTT clientId can not be empty.");
+            close(ctx, CONNECTION_REFUSED_IDENTIFIER_REJECTED);
             return;
+        }
+
+        if (!allowedProtocolVersion(msg)) {
+            LOG.info("MQTT protocol version is not supported. clientId: {}", clientId);
+            close(ctx, CONNECTION_REFUSED_UNACCEPTABLE_PROTOCOL_VERSION);
         }
 
         String userName = msg.payload().userName();
         byte[] passwordInBytes = msg.payload().passwordInBytes();
 
         if (!MqttContext.isValid(userName, passwordInBytes)) {
-            ctx.writeAndFlush(wrong(MqttConnectReturnCode.CONNECTION_REFUSED_BAD_USER_NAME_OR_PASSWORD));
+            LOG.info("MQTT client verification failed, please check the username and password.");
+            close(ctx, CONNECTION_REFUSED_BAD_USER_NAME_OR_PASSWORD);
             return;
         }
 
@@ -58,10 +75,19 @@ public class Connect extends MessageType {
         setConnected(true);
     }
 
+    private void close(final ChannelHandlerContext ctx, final MqttConnectReturnCode returnCode) {
+        ctx.writeAndFlush(wrong(returnCode));
+        ctx.close().addListener(CLOSE_ON_FAILURE);
+    }
+
     private MqttConnAckMessage wrong(final MqttConnectReturnCode returnCode) {
         return MqttMessageBuilders.connAck()
                 .returnCode(returnCode)
                 .sessionPresent(false)
                 .build();
+    }
+
+    private boolean allowedProtocolVersion(final MqttConnectMessage msg) {
+        return msg.variableHeader().version() == MqttVersion.MQTT_3_1.protocolLevel();
     }
 }
