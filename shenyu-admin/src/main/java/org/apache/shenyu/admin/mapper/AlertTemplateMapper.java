5b6fbd4373272e02bc1f363a49a231fa9f9c157c
sinsy <550569627@qq.com>
2022-05-16 21:16:21 +0800

    fix ci error

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/mapper/AlertTemplateMapper.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/mapper/AlertTemplateMapper.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.shenyu.admin.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
--- shenyu-admin/src/main/java/org/apache/shenyu/admin/model/entity/AlertTemplateDO.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/model/entity/AlertTemplateDO.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.shenyu.admin.model.entity;
 
 import java.util.Date;
