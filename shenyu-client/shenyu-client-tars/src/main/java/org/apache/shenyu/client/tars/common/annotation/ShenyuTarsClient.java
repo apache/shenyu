a5088443dfe3258e6d153f1e62230087354ce27b
weihu <weihubeats@163.com>
2022-06-11 10:05:55 +0800

    simplify ShenyuTarsClient

--- shenyu-client/shenyu-client-tars/src/main/java/org/apache/shenyu/client/tars/common/annotation/ShenyuTarsClient.java
+++ shenyu-client/shenyu-client-tars/src/main/java/org/apache/shenyu/client/tars/common/annotation/ShenyuTarsClient.java
@@ -17,6 +17,8 @@
 
 package org.apache.shenyu.client.tars.common.annotation;
 
+import org.springframework.core.annotation.AliasFor;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -30,12 +32,22 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Inherited
 public @interface ShenyuTarsClient {
+
     /**
      * Path string.
      *
      * @return the string
      */
-    String path();
+    @AliasFor(attribute = "path")
+    String value() default "";
+    
+    /**
+     * Path string.
+     *
+     * @return the string
+     */
+    @AliasFor(attribute = "value")
+    String path() default "";
 
     /**
      * Rule name string.
