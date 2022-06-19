f1f22406165eed3e03c765be7434a4bc4baa340e
hailang <hailang@webuy.ai>
2022-06-19 11:09:32 +0800

    [ISSUE #3523] remove duplicate aop code in websocket

--- shenyu-examples/shenyu-examples-websocket/shenyu-example-spring-reactive-websocket/src/main/java/org/apache/shenyu/examples/websocket/handler/EchoHandler.java
+++ shenyu-examples/shenyu-examples-websocket/shenyu-example-spring-reactive-websocket/src/main/java/org/apache/shenyu/examples/websocket/handler/EchoHandler.java
@@ -17,7 +17,7 @@
 
 package org.apache.shenyu.examples.websocket.handler;
 
-import org.apache.shenyu.examples.websocket.aop.Log;
+import org.apache.shenyu.examples.common.aop.Log;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
