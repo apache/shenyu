a5ee50fb75e989734c8c649defdcf342170b4403
lianjunwei <13819823+lianjunwei@users.noreply.github.com>
2022-06-25 09:55:00 +0800

    [Task] admin: Fix API document failed to build because of NPE. (#3604)
    * apidoc sql
    
    * refact
    
    * commit
    
    * [Task] Shenyu-admin: Fix API document failed to build because of NPE.
    
    * [Task] Shenyu-admin: Fix API document failed to build because of NPE.
    
    * delete useless code.
    
    * delete useless code.
    
    Co-authored-by: lianjunwei <lianjunwei@didiglobal.com>

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/ApiDocController.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/ApiDocController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * ApiDoc Controller.
+ * Api Documet Controller.
  */
 @RestController
 @RequestMapping("/apidoc")
@@ -64,8 +64,6 @@ public class ApiDocController {
         docVO.setGatewayUrl(apiDocProperties.getGatewayUrl());
         docVO.setMenuProjects(menuProjects);
         docVO.setEnvProps(apiDocProperties.getEnvProps());
-        docVO.setCookie("Fill in the real cookie value.(signature authentication and login free API ignore this item)");
-        docVO.setAppKey("");
         return ShenyuAdminResult.success(docVO);
     }
 
