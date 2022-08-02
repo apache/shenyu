176d434dd4be5d0727f155c63d3c99acd3f90716
Bowen Li <libw19@fudan.edu.cn>
2022-08-02 16:04:44 +0800

    shenyu logging add pulsar test

---
+++ shenyu-plugin/shenyu-plugin-logging/shenyu-plugin-logging-pulsar/src/test/java/org/apache/shenyu/plugin/logging/pulsar/client/PulsarLogCollectClientTest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.plugin.logging.pulsar.client;
+
+import org.apache.shenyu.common.dto.PluginData;
+import org.apache.shenyu.common.utils.GsonUtils;
+import org.apache.shenyu.plugin.logging.common.constant.GenericLoggingConstant;
+import org.apache.shenyu.plugin.logging.common.entity.ShenyuRequestLog;
+import org.apache.shenyu.plugin.logging.pulsar.config.PulsarLogCollectConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PulsarLogCollectClientTest {
+
+    private final PluginData pluginData = new PluginData();
+
+    private final Properties properties = new Properties();
+
+    private final List<ShenyuRequestLog> logs = new ArrayList<>();
+
+    private final ShenyuRequestLog shenyuRequestLog = new ShenyuRequestLog();
+
+    private PulsarLogCollectConfig.PulsarLogConfig pulsarLogConfig;
+
+    private PulsarLogCollectClient pulsarLogCollectClient;
+
+    @BeforeEach
+    public void setup() {
+        this.pulsarLogCollectClient = new PulsarLogCollectClient();
+        pluginData.setEnabled(true);
+        pluginData.setConfig("{\"topic\":\"test\", \"serviceUrl\":\"test\"}");
+        pulsarLogConfig = GsonUtils.getInstance().fromJson(pluginData.getConfig(), PulsarLogCollectConfig.PulsarLogConfig.class);
+        properties.setProperty(GenericLoggingConstant.TOPIC, pulsarLogConfig.getTopic());
+        properties.setProperty(GenericLoggingConstant.SERVICE_URL, pulsarLogConfig.getServiceUrl());
+        shenyuRequestLog.setClientIp("0.0.0.0");
+        shenyuRequestLog.setPath("org/apache/shenyu/plugin/logging");
+        logs.add(shenyuRequestLog);
+    }
+
+    @Test
+    public void testConsume() {
+        String msg = "";
+        PulsarLogCollectConfig.INSTANCE.setPulsarLogConfig(pulsarLogConfig);
+        pulsarLogCollectClient.initProducer(properties);
+        try {
+            pulsarLogCollectClient.consume(logs);
+        } catch (Exception e) {
+            msg = "false";
+        }
+        Assertions.assertEquals(msg, "");
+        pulsarLogCollectClient.close();
+    }
+}
+
