998a6e174cc9dca24dbeca912f3a24561020adf8
mahaitao617 <mahaitao617@mahaitao617deMacBook-Pro.local>
2022-08-03 13:20:08 +0800

    feat:fix

--- shenyu-integrated-test/shenyu-integrated-test-websocket/src/test/java/org/apache/shenyu/integrated/test/websocket/UploadControllerTest.java
+++ shenyu-integrated-test/shenyu-integrated-test-websocket/src/test/java/org/apache/shenyu/integrated/test/websocket/UploadControllerTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shenyu.integrated.test.websocket;
 
 import org.apache.shenyu.integratedtest.common.AbstractPluginDataInit;
@@ -8,44 +25,49 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
-public class UploadControllerTest  extends AbstractPluginDataInit {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class UploadControllerTest extends AbstractPluginDataInit {
 
     private static final Logger LOG = LoggerFactory.getLogger(WebsocketPluginTest.class);
 
     private static final String WEBSOCKET_URI = "ws://localhost:9195/ws-annotation/upload?token=Jack";
 
     @Test
-    public void testWebsocketUpLoad() {
-        try {
-            WebSocketClient webSocketClient = new WebSocketClient(new URI(WEBSOCKET_URI)) {
-                @Override
-                public void onOpen(ServerHandshake serverHandshake) {
+    public void testWebsocketUpLoad() throws InterruptedException, URISyntaxException {
 
-                }
+        ArrayBlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(1);
+        WebSocketClient webSocketClient = new WebSocketClient(new URI(WEBSOCKET_URI)) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
 
-                @Override
-                public void onMessage(String s) {
+            }
 
-                }
+            @Override
+            public void onMessage(String s) {
 
-                @Override
-                public void onClose(int i, String s, boolean b) {
+            }
 
-                }
+            @Override
+            public void onClose(int i, String s, boolean b) {
 
-                @Override
-                public void onError(Exception e) {
+            }
 
-                }
-            };
-            byte [] msg = new byte[16];
-            msg[0] = 1;
-            webSocketClient.connectBlocking();
-            webSocketClient.send(msg);
-            System.out.println("testeteteteteettete");
-        } catch (Exception e) {
+            @Override
+            public void onError(Exception e) {
 
-        }
+            }
+        };
+        byte[] msg = new byte[16];
+        msg[0] = 1;
+        webSocketClient.connectBlocking();
+        webSocketClient.send(msg);
+        String receivedMessage = blockingQueue.poll(10, TimeUnit.SECONDS);
+        assertThat(receivedMessage, is("apache shenyu server send to Jack message : -> " + msg));
     }
 }
