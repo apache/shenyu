10d466cfc5f1bd0e37d3d11e3e5b070528a775e1
Haitao Ma(jimmy) <15828010639@163.com>
2022-08-05 00:23:12 +0800

    [ISSUE #3740] add websocket upload file example and integrated test (#3798)
    * feat:add websocket upload file example and integrated test
    
    * feat:fix
    
    * feat:fix file with checkstyle problem
    
    * feat:fix checkstyle problem
    
    * feat:fix websocket-integrated-test
    
    * formate
    
    * feat:format
    
    Co-authored-by: mahaitao617 <mahaitao617@mahaitao617deMacBook-Pro.local>

--- shenyu-examples/shenyu-examples-websocket/shenyu-example-spring-native-websocket/src/main/java/org/apache/shenyu/examples/websocket/controller/TestHttpController.java
+++ shenyu-examples/shenyu-examples-websocket/shenyu-example-spring-native-websocket/src/main/java/org/apache/shenyu/examples/websocket/controller/TestHttpController.java
@@ -21,8 +21,12 @@ import org.apache.shenyu.client.springmvc.annotation.ShenyuSpringMvcClient;
 import org.apache.shenyu.examples.common.aop.Log;
 import org.apache.shenyu.examples.websocket.config.WsSessionManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -46,5 +50,23 @@ public class TestHttpController {
         }
         webSocketSession.sendMessage(new TextMessage(msg));
         return "Message sent successfully";
+    }
+
+    @PostMapping(value = "/upload")
+    @Log
+    public @ResponseBody
+    Object file(String token, @RequestParam("file") final MultipartFile file) {
+        try {
+            WebSocketSession webSocketSession = WsSessionManager.get(token);
+            if (webSocketSession == null) {
+                return "User login has expired";
+            }
+            if (file.getOriginalFilename().endsWith(".bin")) {
+                webSocketSession.sendMessage(new BinaryMessage(file.getBytes()));
+            }
+        } catch (Exception e) {
+
+        }
+        return "ok";
     }
 }
