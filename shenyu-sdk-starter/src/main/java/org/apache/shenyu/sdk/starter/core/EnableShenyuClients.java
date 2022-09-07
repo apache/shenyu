2c812ff3a25c6ef94c73ae96959cb0dc7a062260
yunlongn <yunlongn@outlook.com>
2022-09-07 22:02:15 +0800

    [type:feat] java sdk module init.

--- shenyu-sdk-starter/src/main/java/org/apache/shenyu/sdk/starter/core/EnableShenyuClients.java
+++ shenyu-sdk-starter/src/main/java/org/apache/shenyu/sdk/starter/core/EnableShenyuClients.java
@@ -27,7 +27,7 @@ import java.lang.annotation.Target;
 
 /**
  * Scans for interfaces that declare they are feign clients (via
- * {@link org.springframework.cloud.openfeign.FeignClient} <code>@FeignClient</code>).
+ * {@link ShenyuClient} <code>@ShenyuClient</code>).
  * Configures component scanning directives for use with
  * {@link org.springframework.context.annotation.Configuration}
  * <code>@Configuration</code> classes.
@@ -38,40 +38,40 @@ import java.lang.annotation.Target;
 @Import(ShenyuClientsRegistrar.class)
 public @interface EnableShenyuClients {
 
-	/**
-	 * Alias for the {@link #basePackages()} attribute. Allows for more concise annotation
-	 * declarations e.g.: {@code @ComponentScan("org.my.pkg")} instead of
-	 * {@code @ComponentScan(basePackages="org.my.pkg")}.
-	 * @return the array of 'basePackages'.
-	 */
-	String[] value() default {};
+    /**
+     * Alias for the {@link #basePackages()} attribute. Allows for more concise annotation
+     * declarations e.g.: {@code @ComponentScan("org.my.pkg")} instead of
+     * {@code @ComponentScan(basePackages="org.my.pkg")}.
+     * @return the array of 'basePackages'.
+     */
+    String[] value() default {};
 
-	/**
-	 * Base packages to scan for annotated components.
-	 * <p>
-	 * {@link #value()} is an alias for (and mutually exclusive with) this attribute.
-	 * <p>
-	 * Use {@link #basePackageClasses()} for a type-safe alternative to String-based
-	 * package names.
-	 * @return the array of 'basePackages'.
-	 */
-	String[] basePackages() default {};
+    /**
+     * Base packages to scan for annotated components.
+     * <p>
+     * {@link #value()} is an alias for (and mutually exclusive with) this attribute.
+     * <p>
+     * Use {@link #basePackageClasses()} for a type-safe alternative to String-based
+     * package names.
+     * @return the array of 'basePackages'.
+     */
+    String[] basePackages() default {};
 
-	/**
-	 * Type-safe alternative to {@link #basePackages()} for specifying the packages to
-	 * scan for annotated components. The package of each class specified will be scanned.
-	 * <p>
-	 * Consider creating a special no-op marker class or interface in each package that
-	 * serves no purpose other than being referenced by this attribute.
-	 * @return the array of 'basePackageClasses'.
-	 */
-	Class<?>[] basePackageClasses() default {};
+    /**
+     * Type-safe alternative to {@link #basePackages()} for specifying the packages to
+     * scan for annotated components. The package of each class specified will be scanned.
+     * <p>
+     * Consider creating a special no-op marker class or interface in each package that
+     * serves no purpose other than being referenced by this attribute.
+     * @return the array of 'basePackageClasses'.
+     */
+    Class<?>[] basePackageClasses() default {};
 
-	/**
-	 * List of classes annotated with @FeignClient. If not empty, disables classpath
-	 * scanning.
-	 * @return list of FeignClient classes
-	 */
-	Class<?>[] clients() default {};
+    /**
+     * List of classes annotated with @ShenyuClient. If not empty, disables classpath
+     * scanning.
+     * @return list of ShenyuClient classes
+     */
+    Class<?>[] clients() default {};
 
 }
