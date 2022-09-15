8269b198acc4061f82efb9ccb236104df3fa6717
totalo <totalo@apache.org>
2022-09-15 13:00:55 +0800

    [type:refactor] update

--- shenyu-client/shenyu-client-grpc/src/main/java/org/apache/shenyu/client/grpc/GrpcClientEventListener.java
+++ shenyu-client/shenyu-client-grpc/src/main/java/org/apache/shenyu/client/grpc/GrpcClientEventListener.java
@@ -23,6 +23,7 @@ import io.grpc.MethodDescriptor;
 import io.grpc.ServerServiceDefinition;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shenyu.client.core.client.AbstractContextRefreshedEventListener;
+import org.apache.shenyu.client.core.constant.ShenyuClientConstants;
 import org.apache.shenyu.client.core.exception.ShenyuClientIllegalArgumentException;
 import org.apache.shenyu.client.grpc.common.annotation.ShenyuGrpcClient;
 import org.apache.shenyu.client.grpc.common.dto.GrpcExt;
@@ -34,17 +35,21 @@ import org.apache.shenyu.register.client.api.ShenyuClientRegisterRepository;
 import org.apache.shenyu.register.common.config.PropertiesConfig;
 import org.apache.shenyu.register.common.dto.MetaDataRegisterDTO;
 import org.apache.shenyu.register.common.dto.URIRegisterDTO;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.lang.NonNull;
 import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -73,8 +78,82 @@ public class GrpcClientEventListener extends AbstractContextRefreshedEventListen
         Map<String, BindableService> beans = getBeans(contextRefreshedEvent.getApplicationContext());
         for (Entry<String, BindableService> entry : beans.entrySet()) {
             exportJsonGenericService(entry.getValue());
-            handle(entry.getKey(), entry.getValue());
+            handler(entry.getValue());
         }
+    }
+    
+    private void handler(final Object serviceBean) {
+        Class<?> clazz;
+        try {
+            clazz = serviceBean.getClass();
+        } catch (Exception e) {
+            LOG.error("failed to get grpc target class", e);
+            return;
+        }
+        if (AopUtils.isAopProxy(serviceBean)) {
+            clazz = AopUtils.getTargetClass(serviceBean);
+        }
+        Class<?> parent = clazz.getSuperclass();
+        Class<?> classes = parent.getDeclaringClass();
+        String packageName;
+        try {
+            String serviceName = ShenyuClientConstants.SERVICE_NAME;
+            Field field = classes.getField(serviceName);
+            field.setAccessible(true);
+            packageName = field.get(null).toString();
+        } catch (Exception e) {
+            LOG.error(String.format("SERVICE_NAME field not found: %s", classes), e);
+            return;
+        }
+        if (StringUtils.isEmpty(packageName)) {
+            LOG.error(String.format("grpc SERVICE_NAME can not found: %s", classes));
+            return;
+        }
+        ShenyuGrpcClient beanShenyuClient = AnnotatedElementUtils.findMergedAnnotation(clazz, ShenyuGrpcClient.class);
+        String basePath = Optional.ofNullable(beanShenyuClient).map(annotation -> StringUtils.defaultIfBlank(beanShenyuClient.path(), "")).orElse("");
+        if (basePath.contains("*")) {
+            Method[] methods = ReflectionUtils.getDeclaredMethods(clazz);
+            for (Method method : methods) {
+                getPublisher().publishEvent(buildMetaDataDTO(packageName, beanShenyuClient, method, basePath));
+            }
+            return;
+        }
+        final Method[] methods = ReflectionUtils.getUniqueDeclaredMethods(clazz);
+        for (Method method : methods) {
+            ShenyuGrpcClient methodShenyuClient = AnnotatedElementUtils.findMergedAnnotation(method, ShenyuGrpcClient.class);
+            if (Objects.nonNull(methodShenyuClient)) {
+                getPublisher().publishEvent(buildMetaDataDTO(packageName, methodShenyuClient, method, basePath));
+            }
+        }
+    }
+    
+    private MetaDataRegisterDTO buildMetaDataDTO(final String packageName, final ShenyuGrpcClient shenyuGrpcClient,
+                                                 final Method method, final String basePath) {
+        String path = buildApiPath(method, basePath, shenyuGrpcClient);
+        String desc = shenyuGrpcClient.desc();
+        String host = IpUtils.isCompleteHost(getHost()) ? getHost() : IpUtils.getHost(getHost());
+        String configRuleName = shenyuGrpcClient.ruleName();
+        String ruleName = StringUtils.defaultIfBlank(configRuleName, path);
+        String methodName = method.getName();
+        Class<?>[] parameterTypesClazz = method.getParameterTypes();
+        String parameterTypes = Arrays.stream(parameterTypesClazz).map(Class::getName)
+                .collect(Collectors.joining(","));
+        MethodDescriptor.MethodType methodType = JsonServerServiceInterceptor.getMethodTypeMap().get(packageName + "/" + methodName);
+        return MetaDataRegisterDTO.builder()
+                .appName(getIpAndPort())
+                .serviceName(packageName)
+                .methodName(methodName)
+                .contextPath(getContextPath())
+                .host(host)
+                .port(Integer.parseInt(getPort()))
+                .path(path)
+                .ruleName(ruleName)
+                .pathDesc(desc)
+                .parameterTypes(parameterTypes)
+                .rpcType(RpcTypeEnum.GRPC.getName())
+                .rpcExt(buildRpcExt(shenyuGrpcClient, methodType))
+                .enabled(shenyuGrpcClient.enabled())
+                .build();
     }
     
     @Override
