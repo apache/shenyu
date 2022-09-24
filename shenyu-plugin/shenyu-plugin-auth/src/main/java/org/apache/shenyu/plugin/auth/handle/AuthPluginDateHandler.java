84f1b72edf702e4952216295b271e99dc7983d24
jakiuncle <2509913103@qq.com>
2022-09-24 16:18:29 +0800

    [type:feat] Make an Apache Shenyu SSO authentication plugin based on casdoor

--- shenyu-plugin/shenyu-plugin-auth/src/main/java/org/apache/shenyu/plugin/auth/handle/AuthPluginDateHandler.java
+++ shenyu-plugin/shenyu-plugin-auth/src/main/java/org/apache/shenyu/plugin/auth/handle/AuthPluginDateHandler.java
@@ -39,13 +39,7 @@ public class AuthPluginDateHandler implements PluginDataHandler {
         certificate = certificate.replace("\\n", "\n");
         String organization = Optional.ofNullable(configMap.get("organization-name")).orElse("");
         String application = Optional.ofNullable(configMap.get("application-name")).orElse("");
-        CasdoorConfig casdoorConfig = new CasdoorConfig();
-        casdoorConfig.setApplicationName(application);
-        casdoorConfig.setCertificate(certificate);
-        casdoorConfig.setClientId(clientId);
-        casdoorConfig.setEndpoint(endpoint);
-        casdoorConfig.setOrganizationName(organization);
-        casdoorConfig.setClientSecret(clientSecrect);
+        CasdoorConfig casdoorConfig = new CasdoorConfig(endpoint,clientId,clientSecrect,certificate,organization,application);
         CasdoorAuthService casdoorAuthService = new CasdoorAuthService(casdoorConfig);
         Singleton.INST.single(CasdoorAuthService.class, casdoorAuthService);
     }
