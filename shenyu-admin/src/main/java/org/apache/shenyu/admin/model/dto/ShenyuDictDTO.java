1d9b6dad2e9e823c3a7de2da720c80652ecd521d
pandaapo <1052156701@qq.com>
2022-09-26 21:06:07 +0800

    fix issue: an error occurs when saving modified Dictionary. Because there is no enable item.

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/model/dto/ShenyuDictDTO.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/model/dto/ShenyuDictDTO.java
@@ -76,8 +76,7 @@ public class ShenyuDictDTO implements Serializable {
     /**
      * whether enabled.
      */
-    @NotNull
-    private Boolean enabled;
+    private Boolean enabled = true;
     
     public ShenyuDictDTO() {
     }
