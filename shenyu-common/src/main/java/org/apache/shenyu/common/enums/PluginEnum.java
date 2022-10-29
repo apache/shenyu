d1d614f17e3e7a47ee5f918ad43a9a1f631f7a47
MichaelDeSteven <1270728683@qq.com>
2022-10-30 01:52:54 +0800

    add: brpc plugin enum

--- shenyu-common/src/main/java/org/apache/shenyu/common/enums/PluginEnum.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/enums/PluginEnum.java
@@ -238,6 +238,11 @@ public enum PluginEnum {
     MOTAN(310, 0, "motan"),
 
     /**
+     * Motan plugin enum.
+     */
+    BRPC(310, 0, "brpc"),
+
+    /**
      * Cryptor response plugin enum.
      */
     CRYPTOR_RESPONSE(410, 0, "cryptorResponse"),
