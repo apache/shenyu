7b88fec5852134514f7692f9da57115d0af74814
ywj1352 <18856309550@163.com>
2022-10-31 11:40:58 +0800

    shenyu-examples-sdk-http

--- shenyu-examples/shenyu-examples-sdk/shenyu-examples-sdk-http/src/main/java/org/apache/shenyu/examples/sdk/http/service/ShenyuSdkService.java
+++ shenyu-examples/shenyu-examples-sdk/shenyu-examples-sdk-http/src/main/java/org/apache/shenyu/examples/sdk/http/service/ShenyuSdkService.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shenyu.examples.sdk.http.controller;
+package org.apache.shenyu.examples.sdk.http.service;
 
 import org.apache.shenyu.common.dto.ConditionData;
 import org.apache.shenyu.examples.sdk.http.api.ShenyuSdkApi;
