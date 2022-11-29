5bfd429418c5018f91ba25c051ccd2ceb85e698c
‘xcsnx’ <1192709687@qq.com>
2022-11-29 18:45:56 +0800

    test common: Improve the test coverage of 'UriUtils'

--- shenyu-common/src/test/java/org/apache/shenyu/common/utils/UriUtilsTest.java
+++ shenyu-common/src/test/java/org/apache/shenyu/common/utils/UriUtilsTest.java
@@ -21,9 +21,7 @@ import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test cases for UriUtils.
@@ -81,5 +79,22 @@ public final class UriUtilsTest {
         assertNotNull(uri);
         ret = UriUtils.getPathWithParams(uri);
         assertEquals("/path?key=val", ret);
+    }
+
+    @Test
+    void appendScheme() {
+        String uri = UriUtils.appendScheme("example.com", "http");
+        assertEquals("http://example.com", uri);
+
+        uri = UriUtils.appendScheme("example.com", "https");
+        assertEquals("https://example.com", uri);
+
+        uri = UriUtils.appendScheme("https://example.com", "http");
+        assertEquals("https://example.com", uri);
+        assertNotEquals("http://example.com", uri);
+
+        uri = UriUtils.appendScheme("http://example.com", "https");
+        assertEquals("http://example.com", uri);
+        assertNotEquals("https://example.com", uri);
     }
 }
