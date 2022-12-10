fbf8da7eda774759567ce060a2638b7cc284893c
愿凌飞 <tian-pengfei@foxmail.com>
2022-12-10 10:42:05 +0800

    [ISSUE #4235]supports generation of mock data on request (#4236)
    * [ISSUE 4228] fixed generate mock data in multithreading
    
    * [ISSUE #4235]supports generation of mock data on request
    
    * fixed unsafe
    
    * fix
    
    Co-authored-by: xiaoyu <xiaoyu@apache.org>

--- shenyu-plugin/shenyu-plugin-mock/src/main/java/org/apache/shenyu/plugin/mock/MockPlugin.java
+++ shenyu-plugin/shenyu-plugin-mock/src/main/java/org/apache/shenyu/plugin/mock/MockPlugin.java
@@ -24,10 +24,15 @@ import org.apache.shenyu.common.enums.PluginEnum;
 import org.apache.shenyu.plugin.api.ShenyuPluginChain;
 import org.apache.shenyu.plugin.base.AbstractShenyuPlugin;
 import org.apache.shenyu.plugin.base.utils.CacheKeyUtils;
+import org.apache.shenyu.plugin.mock.api.MockRequest;
 import org.apache.shenyu.plugin.mock.generator.GeneratorFactory;
 import org.apache.shenyu.plugin.mock.handler.MockPluginHandler;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -37,28 +42,49 @@ import java.nio.charset.StandardCharsets;
  * MockPlugin.
  */
 public class MockPlugin extends AbstractShenyuPlugin {
-    
+
     @Override
     protected Mono<Void> doExecute(final ServerWebExchange exchange, final ShenyuPluginChain chain,
                                    final SelectorData selector, final RuleData rule) {
+
         MockHandle mockHandle = MockPluginHandler.CACHED_HANDLE.get().obtainHandle(CacheKeyUtils.INST.getKey(rule));
-        String replaceContent = GeneratorFactory.dealRule(mockHandle.getResponseContent());
         exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
         exchange.getResponse().setStatusCode(HttpStatus.valueOf(mockHandle.getHttpStatusCode()));
-        final byte[] bytes = replaceContent.getBytes(StandardCharsets.UTF_8);
-        return exchange.getResponse().writeWith(Mono.just(exchange.getResponse()
-            .bufferFactory().wrap(bytes))
-            .doOnNext(data -> exchange.getResponse().getHeaders()
-                .setContentLength(data.readableByteCount())));
+
+        return DataBufferUtils.join(exchange.getRequest().getBody())
+                .switchIfEmpty(Mono.just(DefaultDataBufferFactory.sharedInstance.allocateBuffer(0)))
+                .map(dataBuffer -> dealRule(dataBuffer, mockHandle.getResponseContent(), exchange.getRequest()))
+                .flatMap(bytes -> exchange.getResponse().writeWith(Mono.just(exchange.getResponse()
+                        .bufferFactory().wrap(bytes))
+                        .doOnNext(data -> exchange.getResponse().getHeaders()
+                                .setContentLength(data.readableByteCount()))));
     }
-    
+
     @Override
     public int getOrder() {
         return PluginEnum.MOCK.getCode();
     }
-    
+
     @Override
     public String named() {
         return PluginEnum.MOCK.getName();
+    }
+
+    private byte[] dealRule(final DataBuffer requestBodyBuffer, final String response, final ServerHttpRequest serverHttpRequest) {
+        byte[] originalBody = new byte[requestBodyBuffer.readableByteCount()];
+        requestBodyBuffer.read(originalBody);
+        DataBufferUtils.release(requestBodyBuffer);
+        MockRequest mockRequest = buildMockRequest(originalBody, serverHttpRequest);
+        return GeneratorFactory.dealRule(response, mockRequest).getBytes(StandardCharsets.UTF_8);
+    }
+
+    private MockRequest buildMockRequest(final byte[] originalBody, final ServerHttpRequest serverHttpRequest) {
+
+        return MockRequest.Builder.builder()
+                .headers(serverHttpRequest.getHeaders().toSingleValueMap())
+                .method(serverHttpRequest.getMethodValue())
+                .queries(serverHttpRequest.getQueryParams().toSingleValueMap())
+                .uri(serverHttpRequest.getURI().toString())
+                .body(originalBody).build();
     }
 }
