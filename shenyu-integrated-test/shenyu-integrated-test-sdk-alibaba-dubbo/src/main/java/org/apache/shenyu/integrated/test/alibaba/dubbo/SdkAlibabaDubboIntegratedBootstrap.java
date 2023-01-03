617f86f1aaaf1048c167ef078331af11768a680f
huanccwang <WangHuan0606@outlook.com>
2023-01-03 09:50:00 +0800

    shenyu-sdk module add fallback (#4285)
    * fallback以及fallback测试
    
    * format
    
    * format
    
    * .
    
    * .
    
    * 修改zk地址为localhost
    
    * fallback integrated test
    
    * 修改抛出rte
    
    * 增加spring cloud fallback以及fallback factory集成测试
    
    * .
    
    * spring cloud集成测试
    
    * .
    
    * 修改springcloud集成测试
    
    * .
    
    * 修改集成测试
    
    Co-authored-by: yunlongn <yunlongn@outlook.com>
    Co-authored-by: xiaoyu <xiaoyu@apache.org>

--- shenyu-integrated-test/shenyu-integrated-test-sdk-alibaba-dubbo/src/main/java/org/apache/shenyu/integrated/test/alibaba/dubbo/SdkAlibabaDubboIntegratedBootstrap.java
+++ shenyu-integrated-test/shenyu-integrated-test-sdk-alibaba-dubbo/src/main/java/org/apache/shenyu/integrated/test/alibaba/dubbo/SdkAlibabaDubboIntegratedBootstrap.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shenyu.integrated.test.apache.dubbo;
+package org.apache.shenyu.integrated.test.alibaba.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
