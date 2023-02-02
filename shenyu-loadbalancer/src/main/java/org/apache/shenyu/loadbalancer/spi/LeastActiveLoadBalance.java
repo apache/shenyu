46d3b727c0e064ea202fce6c6eeb88a30ee35d68
DamonXue(Fibonacci) <DamonXue2@gmail.com>
2023-02-02 09:17:00 +0800

    change count type integer 2 long.

--- shenyu-loadbalancer/src/main/java/org/apache/shenyu/loadbalancer/spi/LeastActiveLoadBalance.java
+++ shenyu-loadbalancer/src/main/java/org/apache/shenyu/loadbalancer/spi/LeastActiveLoadBalance.java
@@ -33,7 +33,7 @@ import org.apache.shenyu.spi.Join;
 @Join
 public class LeastActiveLoadBalance extends AbstractLoadBalancer {
 
-    private final Map<String, Integer> countMap = new ConcurrentHashMap<>();
+    private final Map<String, Long> countMap = new ConcurrentHashMap<>();
 
     @Override
     protected Upstream doSelect(final List<Upstream> upstreamList, final String ip) {
@@ -42,16 +42,16 @@ public class LeastActiveLoadBalance extends AbstractLoadBalancer {
 
         domainMap.keySet().stream()
                 .filter(key -> !countMap.containsKey(key))
-                .forEach(domain -> countMap.put(domain, 0));
+                .forEach(domain -> countMap.put(domain, Long.MIN_VALUE));
 
         final String domain = countMap.entrySet().stream()
                 // Ensure that the filtered domain is included in the domainMap.
                 .filter(entry -> domainMap.containsKey(entry.getKey()))
-                .min(Comparator.comparingInt(Map.Entry::getValue))
+                .min(Comparator.comparingLong(Map.Entry::getValue))
                 .map(Map.Entry::getKey)
                 .orElse(upstreamList.get(0).buildDomain());
 
-        countMap.computeIfPresent(domain, (key, actived) -> Optional.of(actived).orElse(0) + 1);
+        countMap.computeIfPresent(domain, (key, actived) -> Optional.of(actived).orElse(Long.MIN_VALUE) + 1);
         return domainMap.get(domain);
     }
     
