2942f3168bdc4dc9cd8315d59f536b3dba65ddf6
mahaitao617 <mahaitao617@mahaitao617deMacBook-Pro.local>
2023-02-23 21:31:32 +0800

    feat:fix

--- shenyu-plugin/shenyu-plugin-brpc/src/main/java/org/apache/shenyu/plugin/brpc/spi/SharedThreadPoolFactory.java
+++ shenyu-plugin/shenyu-plugin-brpc/src/main/java/org/apache/shenyu/plugin/brpc/spi/SharedThreadPoolFactory.java
@@ -34,7 +34,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * shared threadpool factory.
+ *  shared threadpool factory.
  */
 public class SharedThreadPoolFactory implements ThreadPoolFactory {
     private static final Logger LOGGER = LoggerFactory.getLogger(SharedThreadPoolFactory.class);
