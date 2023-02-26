5c6fb5a1fafdf85404b5cdf8360f49f8faa757cc
fabian4 <fabian4@163.com>
2023-02-26 18:27:53 +0800

    Add support for polaris.

---
+++ shenyu-spring-boot-starter/shenyu-spring-boot-starter-sync-data-center/shenyu-spring-boot-starter-sync-data-polaris/src/main/java/org/apache/shenyu/springboot/starter/sync/data/polaris/PolarisSyncDataConfiguration.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.springboot.starter.sync.data.polaris;
+
+import com.tencent.polaris.configuration.api.core.ConfigFileService;
+import com.tencent.polaris.configuration.factory.ConfigFileServiceFactory;
+import org.apache.shenyu.sync.data.api.AuthDataSubscriber;
+import org.apache.shenyu.sync.data.api.MetaDataSubscriber;
+import org.apache.shenyu.sync.data.api.PluginDataSubscriber;
+import org.apache.shenyu.sync.data.api.SyncDataService;
+import org.apache.shenyu.sync.data.polaris.PolarisSyncDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Polaris sync data configuration for spring boot.
+ */
+@Configuration
+@ConditionalOnClass(PolarisSyncDataService.class)
+@ConditionalOnProperty(prefix = "shenyu.sync.polaris", name = "address")
+public class PolarisSyncDataConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PolarisSyncDataConfiguration.class);
+
+    /**
+     * Polaris sync data service
+     *
+     * @param configFileServices the config service
+     * @param pluginSubscriber  the plugin subscriber
+     * @param metaSubscribers  the meta subscribers
+     * @param authSubscribers  the auth subscribers
+     * @return
+     */
+    @Bean
+    public SyncDataService polarisSyncDataService(final ObjectProvider<ConfigFileService> configFileServices, final ObjectProvider<PluginDataSubscriber> pluginSubscriber,
+                                                  final ObjectProvider<List<MetaDataSubscriber>> metaSubscribers, final ObjectProvider<List<AuthDataSubscriber>> authSubscribers) {
+        LOGGER.info("you use polaris sync shenyu data.......");
+        return new PolarisSyncDataService(configFileServices.getIfAvailable(), pluginSubscriber.getIfAvailable(),
+                metaSubscribers.getIfAvailable(Collections::emptyList), authSubscribers.getIfAvailable(Collections::emptyList));
+    }
+
+    /**
+     * Polaris config service.
+     *
+     * @return the config service
+     */
+    @Bean
+    public ConfigFileService polarisConfigServices() {
+        return ConfigFileServiceFactory.createConfigFileService();
+    }
+
+}
---
+++ shenyu-sync-data-center/shenyu-sync-data-polaris/src/main/java/org/apache/shenyu/sync/data/polaris/PolarisSyncDataService.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.sync.data.polaris;
+
+import com.tencent.polaris.configuration.api.core.ConfigFileService;
+import org.apache.shenyu.common.constant.NacosPathConstants;
+import org.apache.shenyu.common.constant.PolarisPathConstants;
+import org.apache.shenyu.sync.data.api.AuthDataSubscriber;
+import org.apache.shenyu.sync.data.api.MetaDataSubscriber;
+import org.apache.shenyu.sync.data.api.PluginDataSubscriber;
+import org.apache.shenyu.sync.data.api.SyncDataService;
+import org.apache.shenyu.sync.data.polaris.handler.PolarisCacheHandler;
+
+import java.util.List;
+
+/**
+ * The type Polaris sync data service.
+ */
+public class PolarisSyncDataService extends PolarisCacheHandler implements SyncDataService {
+
+    public PolarisSyncDataService(final ConfigFileService configFileService, final PluginDataSubscriber pluginDataSubscriber,
+                                  final List<MetaDataSubscriber> metaDataSubscribers, final List<AuthDataSubscriber> authDataSubscribers) {
+
+        super(configFileService, pluginDataSubscriber, metaDataSubscribers, authDataSubscribers);
+        start();
+    }
+
+    /**
+     * Start.
+     */
+    public void start() {
+        watcherData(NacosPathConstants.PLUGIN_DATA_ID, this::updatePluginMap);
+        watcherData(NacosPathConstants.SELECTOR_DATA_ID, this::updateSelectorMap);
+        watcherData(NacosPathConstants.RULE_DATA_ID, this::updateRuleMap);
+        watcherData(NacosPathConstants.META_DATA_ID, this::updateMetaDataMap);
+        watcherData(NacosPathConstants.AUTH_DATA_ID, this::updateAuthMap);
+    }
+
+    @Override
+    public void close() {
+        LISTENERS.forEach((dataId, lss) -> {
+            lss.forEach(listener -> getConfigFileService().
+                    getConfigFile(PolarisPathConstants.NAMESPACE, PolarisPathConstants.FILE_GROUP, dataId)
+                    .removeChangeListener(listener));
+            lss.clear();
+        });
+        LISTENERS.clear();
+    }
+}
