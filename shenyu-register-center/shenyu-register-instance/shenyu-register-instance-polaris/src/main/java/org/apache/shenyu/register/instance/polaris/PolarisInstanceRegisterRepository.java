00aa4a11e5cf694c82f616256151360ea9b99355
DamonXue(Fibonacci) <DamonXue2@gmail.com>
2023-03-02 11:14:37 +0800

    Polaris instance register repository.

--- shenyu-register-center/shenyu-register-instance/shenyu-register-instance-polaris/src/main/java/org/apache/shenyu/register/instance/polaris/PolarisInstanceRegisterRepository.java
+++ shenyu-register-center/shenyu-register-instance/shenyu-register-instance-polaris/src/main/java/org/apache/shenyu/register/instance/polaris/PolarisInstanceRegisterRepository.java
@@ -17,6 +17,21 @@
 
 package org.apache.shenyu.register.instance.polaris;
 
+import com.tencent.polaris.api.config.Configuration;
+import com.tencent.polaris.api.core.ConsumerAPI;
+import com.tencent.polaris.api.core.ProviderAPI;
+import com.tencent.polaris.api.exception.PolarisException;
+import com.tencent.polaris.api.pojo.Instance;
+import com.tencent.polaris.api.rpc.GetHealthyInstancesRequest;
+import com.tencent.polaris.api.rpc.InstanceRegisterRequest;
+import com.tencent.polaris.api.rpc.InstancesResponse;
+import com.tencent.polaris.api.rpc.WatchServiceRequest;
+import com.tencent.polaris.api.rpc.WatchServiceResponse;
+import com.tencent.polaris.client.api.SDKContext;
+import com.tencent.polaris.factory.ConfigAPIFactory;
+import com.tencent.polaris.factory.api.DiscoveryAPIFactory;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.shenyu.common.exception.ShenyuException;
 import org.apache.shenyu.register.instance.api.ShenyuInstanceRegisterRepository;
 import org.apache.shenyu.register.instance.api.config.RegisterConfig;
 import org.apache.shenyu.register.instance.api.entity.InstanceEntity;
@@ -24,42 +39,171 @@ import org.apache.shenyu.register.instance.api.watcher.WatcherListener;
 import org.apache.shenyu.spi.Join;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import shade.polaris.io.netty.handler.codec.http.HttpScheme;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
- * The type Nacos instance register repository.
+ * The type Polaris instance register repository.
  */
 @Join
 public class PolarisInstanceRegisterRepository implements ShenyuInstanceRegisterRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PolarisInstanceRegisterRepository.class);
 
+    /**
+     * Polaris namespace.
+     */
     private static final String NAMESPACE = "polarisNameSpace";
 
-    private String groupName;
+    /**
+     * Polaris ttl.
+     */
+    private static final String DEFAULT_TTL = "5";
 
+    private Properties props;
+
+    private String namespace;
+
+    private ProviderAPI providerAPI;
+
+    private ConsumerAPI consumerAPI;
+
+    /**
+     * init repository.
+     *
+     * @param config the config
+     */
     @Override
     public void init(final RegisterConfig config) {
-        
+        Configuration configuration = buildConfiguration(config);
+        SDKContext sdkContext = SDKContext.initContextByConfig(configuration);
+        this.providerAPI = DiscoveryAPIFactory.createProviderAPIByContext(sdkContext);
+        this.consumerAPI = DiscoveryAPIFactory.createConsumerAPIByContext(sdkContext);
+        this.props = config.getProps();
+        this.namespace = props.getProperty("namespace", NAMESPACE);
     }
 
+    /**
+     * build polaris configuration.
+     *
+     * @param config config
+     * @return Configuration
+     */
+    private Configuration buildConfiguration(final RegisterConfig config) {
+        String serverLists = config.getServerLists();
+        return ConfigAPIFactory.createConfigurationByAddress(serverLists);
+    }
+
+    /**
+     * persist instance.
+     *
+     * @param instance instance
+     */
     @Override
     public void persistInstance(final InstanceEntity instance) {
 
+        final String token = props.getProperty("token", null);
+        final String version = props.getProperty("version", null);
+        final String weight = props.getProperty("weight", "0");
+        final String ttl = props.getProperty("ttl", DEFAULT_TTL);
+
+        // build register info
+        InstanceRegisterRequest registerRequest = new InstanceRegisterRequest();
+        registerRequest.setNamespace(namespace);
+        registerRequest.setService(instance.getAppName());
+        registerRequest.setHost(instance.getHost());
+        registerRequest.setPort(instance.getPort());
+
+        registerRequest.setProtocol(HttpScheme.HTTPS.toString());
+        registerRequest.setTtl(Integer.valueOf(ttl));
+        registerRequest.setToken(token);
+        registerRequest.setVersion(version);
+        registerRequest.setWeight(Integer.valueOf(weight));
+
+        // register service
+        try {
+            providerAPI.registerInstance(registerRequest);
+        } catch (Exception e) {
+            throw new ShenyuException(e);
+        }
+        LOGGER.info("polaris client register instance success: {}", instance);
     }
 
+    /**
+     * select and watch instances.
+     *
+     * @param selectKey       selectKey
+     * @param watcherListener watcherListener
+     * @return List
+     */
     @Override
     public List<InstanceEntity> selectInstancesAndWatcher(final String selectKey, final WatcherListener watcherListener) {
-        return null;
+
+        final WatchServiceRequest watcherRequest = WatchServiceRequest.builder()
+                .service(selectKey)
+                .namespace(namespace)
+                .listeners(Collections.singletonList(event -> watcherListener.listener(transformInstances(event.getAllInstances()))))
+                .build();
+
+        Instance[] instances;
+        try {
+            final WatchServiceResponse watchServiceResponse = consumerAPI.watchService(watcherRequest);
+            watchServiceResponse.getResponse().getInstances();
+            instances = watchServiceResponse.getResponse().getInstances();
+        } catch (PolarisException e) {
+            throw new ShenyuException(e);
+        }
+        if (Objects.isNull(instances)) {
+            try {
+                GetHealthyInstancesRequest req = new GetHealthyInstancesRequest();
+                req.setService(selectKey);
+                req.setNamespace(namespace);
+                final InstancesResponse response = consumerAPI.getHealthyInstances(req);
+                instances = response.getInstances();
+            } catch (PolarisException e) {
+                // ignore
+                LOGGER.error("polaris client select instances failed: {}", e.getMessage());
+            }
+        }
+        if (Objects.isNull(instances)) {
+            return Collections.emptyList();
+        }
+        return transformInstances(Arrays.asList(instances));
     }
 
-    private String buildInstanceNodeName(final InstanceEntity instance) {
-        return null;
+    /**
+     * transform to instance entities.
+     *
+     * @param instances instances
+     * @return List
+     */
+    private List<InstanceEntity> transformInstances(final List<Instance> instances) {
+        if (CollectionUtils.isEmpty(instances)) {
+            return Collections.emptyList();
+        }
+        return instances.stream()
+                .map(instance -> InstanceEntity.builder()
+                        .appName(instance.getService())
+                        .host(instance.getHost())
+                        .port(instance.getPort())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     @Override
     public void close() {
-       
+        try {
+            providerAPI.close();
+            consumerAPI.close();
+        } catch (PolarisException e) {
+            throw new ShenyuException(e);
+        }
     }
+    
 }
