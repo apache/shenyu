d20f407c4816853233f6ec0ca78f49edf87adbc5
moremind <hefengen@apache.org>
2023-04-09 18:24:41 +0800

    [type:refactor] rename shenyu trie

--- shenyu-common/src/main/java/org/apache/shenyu/common/config/ShenyuConfig.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/config/ShenyuConfig.java
@@ -19,7 +19,7 @@ package org.apache.shenyu.common.config;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shenyu.common.concurrent.MemoryLimitCalculator;
-import org.apache.shenyu.common.enums.TrieMatchModeEvent;
+import org.apache.shenyu.common.enums.TrieMatchModeEnum;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -1838,9 +1838,9 @@ public class ShenyuConfig {
 
         /**
          * match mode.
-         * @see TrieMatchModeEvent
+         * @see TrieMatchModeEnum
          */
-        private String matchMode = TrieMatchModeEvent.ANT_PATH_MATCH.getMatchMode();
+        private String matchMode = TrieMatchModeEnum.ANT_PATH_MATCH.getMatchMode();
     
         /**
          * get match enabled.
