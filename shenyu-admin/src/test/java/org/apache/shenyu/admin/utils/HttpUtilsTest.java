a96ea17702605b6da4eb5618318769d522a9e135
hudongdong <hudongdong9406@zto.com>
2023-04-12 11:59:07 +0800

    [test]update CI/CD problem

--- shenyu-admin/src/test/java/org/apache/shenyu/admin/utils/HttpUtilsTest.java
+++ shenyu-admin/src/test/java/org/apache/shenyu/admin/utils/HttpUtilsTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shenyu.admin.utils;
 
 import okhttp3.FormBody;
@@ -6,18 +23,15 @@ import okhttp3.Request;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /**
- * HTTP request tool test {@link HttpUtils}
+ * HTTP request tool test {@link HttpUtils}.
  */
 public class HttpUtilsTest {
-
 
     private static final String TEST_URL = "http://127.0.0.1/";
 
