86a4c1c14e3464318e115aaa9d6e3fea14747752
杨文杰 <31105009+ywj1352@users.noreply.github.com>
2023-05-04 12:20:51 +0800

    Tcp plugin (#4607)
    * init tcp plugin
    
    * init tcp plugin
    
    * tigger ci
    
    * init tcp
    
    * init tcp
    
    * demo
    
    * add tcp
    proxy demo
    
    * change code
    
    * add tcp
    proxy demo
    
    * change code
    
    * change code
    
    * change code
    
    * change codes
    
    * tcp proxy
    
    * tcp proxy
    
    * add tcp
    proxy
    
    * add tcp
    proxy
    
    * change tcp proxy code
    
    * add tcp
    proxy
    
    * support hot update
    
    * support hot update
    
    * add tcp
    proxy
    
    * add tcp
    proxy
    
    * add tcp
    proxy
    
    * add tcp
    proxy

---
+++ shenyu-common/src/main/java/org/apache/shenyu/common/dto/ProxySelectorData.java
@@ -0,0 +1,146 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.common.dto;
+
+import java.util.Properties;
+
+/**
+ * ProxySelectorData.
+ */
+public class ProxySelectorData {
+
+    private String id;
+
+    private String name;
+
+    private String pluginName;
+
+    private String type;
+
+    private Integer forwardPort;
+
+    private Properties props = new Properties();
+
+    /**
+     * getId.
+     *
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * setId.
+     *
+     * @param id id
+     */
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    /**
+     * getName.
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * setName.
+     *
+     * @param name name
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * getPluginName.
+     *
+     * @return pluginName
+     */
+    public String getPluginName() {
+        return pluginName;
+    }
+
+    /**
+     * setPluginName.
+     *
+     * @param pluginName pluginName
+     */
+    public void setPluginName(final String pluginName) {
+        this.pluginName = pluginName;
+    }
+
+    /**
+     * getType.
+     *
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * setType.
+     *
+     * @param type type
+     */
+    public void setType(final String type) {
+        this.type = type;
+    }
+
+    /**
+     * getForwardPort.
+     *
+     * @return forwardPort
+     */
+    public Integer getForwardPort() {
+        return forwardPort;
+    }
+
+    /**
+     * setForwardPort.
+     *
+     * @param forwardPort forwardPort
+     */
+    public void setForwardPort(final Integer forwardPort) {
+        this.forwardPort = forwardPort;
+    }
+
+    /**
+     * getProps.
+     *
+     * @return props
+     */
+    public Properties getProps() {
+        return props;
+    }
+
+    /**
+     * setProps.
+     *
+     * @param props props
+     */
+    public void setProps(final Properties props) {
+        this.props = props;
+    }
+}
