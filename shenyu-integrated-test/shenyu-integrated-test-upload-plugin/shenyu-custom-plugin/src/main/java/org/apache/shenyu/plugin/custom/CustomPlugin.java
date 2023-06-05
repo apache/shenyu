c6b6dbc1abe9454ed686a559a56c9230c0c5d520
ywj1352 <18856309550@163.com>
2023-06-05 22:16:59 +0800

    intergated upload test

--- shenyu-integrated-test/shenyu-integrated-test-upload-plugin/shenyu-custom-plugin/src/main/java/org/apache/shenyu/plugin/custom/CustomPlugin.java
+++ shenyu-integrated-test/shenyu-integrated-test-upload-plugin/shenyu-custom-plugin/src/main/java/org/apache/shenyu/plugin/custom/CustomPlugin.java
@@ -32,7 +32,7 @@ public class CustomPlugin extends AbstractShenyuPlugin {
 
     private static final Logger LOG = LoggerFactory.getLogger(CustomPlugin.class);
 
-    protected Mono<Void> doExecute(ServerWebExchange exchange, ShenyuPluginChain chain, SelectorData selectorData, RuleData rule) {
+    protected Mono<Void> doExecute(final ServerWebExchange exchange, final ShenyuPluginChain chain, final SelectorData selectorData, final RuleData rule) {
         LOG.info(".......... function plugin start CustomPlugin............");
         byte[] bytes = "CustomPlugin jar version : 1.0".getBytes(StandardCharsets.UTF_8);
         return exchange.getResponse().writeWith(Mono.just(exchange.getResponse()
