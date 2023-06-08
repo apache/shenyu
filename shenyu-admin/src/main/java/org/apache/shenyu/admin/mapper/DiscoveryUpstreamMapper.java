d2598bea0b222bc3357e7ea6f6db73690ed7f4bb
ywj1352 <18856309550@163.com>
2023-06-08 17:35:52 +0800

    trigger ci

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/mapper/DiscoveryUpstreamMapper.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/mapper/DiscoveryUpstreamMapper.java
@@ -46,7 +46,7 @@ public interface DiscoveryUpstreamMapper extends ExistProvider {
     List<DiscoveryUpstreamDO> selectByIds(@Param("ids") List<String> ids);
 
     /**
-     *insert.
+     * insert.
      *
      * @param discoveryUpstreamDO discoveryUpstreamDO
      * @return rows int
