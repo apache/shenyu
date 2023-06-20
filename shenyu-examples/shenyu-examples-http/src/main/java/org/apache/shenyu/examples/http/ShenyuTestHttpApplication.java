e1fce7e8e50f1b0487f9c6d567e237254f00603c
tianpengfei <tian-pengfei@foxmail.com>
2023-06-20 20:19:42 +0800

    remove sth

--- shenyu-examples/shenyu-examples-http/src/main/java/org/apache/shenyu/examples/http/ShenyuTestHttpApplication.java
+++ shenyu-examples/shenyu-examples-http/src/main/java/org/apache/shenyu/examples/http/ShenyuTestHttpApplication.java
@@ -17,7 +17,6 @@
 
 package org.apache.shenyu.examples.http;
 
-import org.apache.shenyu.client.auto.config.EnableClientRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- shenyu-examples/shenyu-examples-springcloud/src/main/java/org/apache/shenyu/examples/springcloud/ShenyuTestSpringCloudApplication.java
+++ shenyu-examples/shenyu-examples-springcloud/src/main/java/org/apache/shenyu/examples/springcloud/ShenyuTestSpringCloudApplication.java
@@ -17,7 +17,6 @@
 
 package org.apache.shenyu.examples.springcloud;
 
-import org.apache.shenyu.client.auto.config.EnableClientRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
