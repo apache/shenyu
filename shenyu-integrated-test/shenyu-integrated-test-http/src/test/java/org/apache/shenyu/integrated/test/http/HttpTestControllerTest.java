f50b25da40da3d493c56bd9b711b2987b1d376af
moremind <hefengen@apache.org>
2023-06-21 13:57:39 +0800

    [type:fix] fix memory limit

--- shenyu-integrated-test/shenyu-integrated-test-http/src/test/java/org/apache/shenyu/integrated/test/http/HttpTestControllerTest.java
+++ shenyu-integrated-test/shenyu-integrated-test-http/src/test/java/org/apache/shenyu/integrated/test/http/HttpTestControllerTest.java
@@ -24,9 +24,11 @@ import okhttp3.Response;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.shenyu.common.utils.GsonUtils;
 import org.apache.shenyu.integratedtest.common.AbstractTest;
+import org.apache.shenyu.integratedtest.common.dto.BigObject;
 import org.apache.shenyu.integratedtest.common.dto.UserDTO;
 import org.apache.shenyu.integratedtest.common.helper.HttpHelper;
 import org.apache.shenyu.integratedtest.common.result.ResultBean;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
@@ -236,8 +238,7 @@ public final class HttpTestControllerTest extends AbstractTest {
 
     @Test
     public void testBigObject() throws IOException {
-        // TODO fix ci error
-        //BigObject response = HttpHelper.INSTANCE.postGateway("/http/test/bigObject", BigObject.class);
-        //assertNotNull(response);
+        BigObject response = HttpHelper.INSTANCE.postGateway("/http/test/bigObject", BigObject.class);
+        assertNotNull(response);
     }
 }
