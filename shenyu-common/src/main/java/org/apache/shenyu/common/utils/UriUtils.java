2b12786b56296905e18b8139e4e01d7c5c02e069
lianjunwei <lianjunwei@didiglobal.com>
2023-06-28 00:08:53 +0800

    [ISSUE #4774] The access layer interface for the apidoc function only allows requests for gateway addresses within the whitelist.

--- shenyu-common/src/main/java/org/apache/shenyu/common/utils/UriUtils.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/utils/UriUtils.java
@@ -17,6 +17,7 @@
 
 package org.apache.shenyu.common.utils;
 
+import javax.annotation.Nullable;
 import org.apache.commons.lang3.StringUtils;
 
 import java.net.URI;
@@ -26,9 +27,9 @@ import java.util.Objects;
  * uri util.
  */
 public class UriUtils {
-    
+
     private static final String PRE_FIX = "/";
-    
+
     /**
      * create URI {@link URI}.
      *
@@ -41,7 +42,7 @@ public class UriUtils {
         }
         return null;
     }
-    
+
     /**
      * Repair data string.
      *
@@ -51,7 +52,7 @@ public class UriUtils {
     public static String repairData(final String name) {
         return name.startsWith(PRE_FIX) ? name : PRE_FIX + name;
     }
-    
+
     /**
      * Remove prefix string.
      *
@@ -80,7 +81,7 @@ public class UriUtils {
      * appendScheme.
      *
      * @param scheme scheme
-     * @param url url
+     * @param url    url
      * @return {@link String}
      */
     public static String appendScheme(final String url, final String scheme) {
@@ -89,5 +90,26 @@ public class UriUtils {
             schemeUrl = scheme + "://" + schemeUrl;
         }
         return schemeUrl;
+    }
+
+    /**
+     * get actual port.
+     *
+     * @param scheme scheme eg:http
+     * @param port   port
+     * @return {@link int}
+     */
+    public static int getActualPort(@Nullable final String scheme, final Integer port) {
+        Integer actualPort = port;
+        if (Objects.isNull(port) || port.intValue() < 0) {
+            if (!"http".equals(scheme) && !"ws".equals(scheme)) {
+                if ("https".equals(scheme) || "wss".equals(scheme)) {
+                    actualPort = 443;
+                }
+            } else {
+                actualPort = 80;
+            }
+        }
+        return actualPort;
     }
 }
