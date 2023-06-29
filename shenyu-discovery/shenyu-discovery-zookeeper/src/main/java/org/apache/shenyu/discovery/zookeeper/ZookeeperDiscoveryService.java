80cf05d9329c5b0fa16b36500fbc6a329904582d
ywj1352 <18856309550@163.com>
2023-06-29 18:00:43 +0800

    Integration module

--- shenyu-discovery/shenyu-discovery-zookeeper/src/main/java/org/apache/shenyu/discovery/zookeeper/ZookeeperDiscoveryService.java
+++ shenyu-discovery/shenyu-discovery-zookeeper/src/main/java/org/apache/shenyu/discovery/zookeeper/ZookeeperDiscoveryService.java
@@ -40,6 +40,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The type Zookeeper for shenyu discovery service.
@@ -90,7 +91,9 @@ public class ZookeeperDiscoveryService implements ShenyuDiscoveryService {
         });
         this.client.start();
         try {
-            this.client.blockUntilConnected();
+            if (!this.client.blockUntilConnected(30, TimeUnit.SECONDS)) {
+                throw new ShenyuException("shenyu start ZookeeperDiscoveryService failure 30 seconds timeout");
+            }
         } catch (InterruptedException e) {
             throw new ShenyuException(e);
         }
