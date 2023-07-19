380a44bcf5391c7b14a5ca79fe647070f68168d5
fabian4 <fabian4@163.com>
2023-07-19 14:29:57 +0800

    add unit test.

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/listener/polaris/PolarisDataChangedListener.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/listener/polaris/PolarisDataChangedListener.java
@@ -24,6 +24,7 @@ import com.tencent.polaris.configuration.api.core.ConfigFileService;
 import com.tencent.polaris.configuration.client.internal.DefaultConfigFileMetadata;
 import org.apache.shenyu.admin.config.properties.PolarisProperties;
 import org.apache.shenyu.admin.listener.AbstractListDataChangedListener;
+import org.apache.shenyu.common.constant.NacosPathConstants;
 import org.apache.shenyu.common.constant.PolarisPathConstants;
 import org.apache.shenyu.common.exception.ShenyuException;
 import org.apache.shenyu.common.utils.GsonUtils;
@@ -45,7 +46,8 @@ public class PolarisDataChangedListener extends AbstractListDataChangedListener 
 
     public PolarisDataChangedListener(final PolarisProperties polarisProperties, final ConfigFileService configFileService, final ConfigFilePublishService configFilePublishService) {
         super(new ChangeData(PolarisPathConstants.PLUGIN_DATA_FILE_NAME, PolarisPathConstants.SELECTOR_DATA_FILE_NAME,
-                PolarisPathConstants.RULE_DATA_FILE_NAME, PolarisPathConstants.AUTH_DATA_ID_FILE_NAME, PolarisPathConstants.META_DATA_FILE_NAME, PolarisPathConstants.PROXY_SELECTOR_FILE_NAME));
+                PolarisPathConstants.RULE_DATA_FILE_NAME, PolarisPathConstants.AUTH_DATA_ID_FILE_NAME,
+                PolarisPathConstants.META_DATA_FILE_NAME, PolarisPathConstants.PROXY_SELECTOR_FILE_NAME, NacosPathConstants.DISCOVERY_DATA_ID));
         this.polarisProperties = polarisProperties;
         this.configFileService = configFileService;
         this.configFilePublishService = configFilePublishService;
