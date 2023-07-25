0a14874d29b34b2a8d4afc02600bdc02cc2a7186
misaya295 <864730420@qq.com>
2023-07-25 15:54:55 +0800

    fix style

--- shenyu-admin/src/test/java/org/apache/shenyu/admin/listener/apollo/ApolloClientTest.java
+++ shenyu-admin/src/test/java/org/apache/shenyu/admin/listener/apollo/ApolloClientTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shenyu.admin.listener.apollo;
 
 import static org.junit.Assert.assertEquals;
@@ -9,7 +26,6 @@ import org.apache.shenyu.admin.config.properties.ApolloProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,11 +37,8 @@ public class ApolloClientTest {
     @MockBean
     private ApolloClient apolloClient;
 
-    @Autowired
-    private ApolloProperties apolloProperties;
-
     /**
-     * Method under test: {@link ApolloClient#ApolloClient(ApolloProperties)}
+     * Method under test: {@link ApolloClient#ApolloClient(ApolloProperties)}.
      */
     @Test
     public void testConstructor() {
@@ -48,7 +61,7 @@ public class ApolloClientTest {
     }
 
     /**
-     * Method under test: {@link ApolloClient#getItemValue(String)}
+     * Method under test: {@link ApolloClient#getItemValue(String)}.
      */
     @Test
     public void testGetItemValue() {
@@ -58,7 +71,7 @@ public class ApolloClientTest {
     }
 
     /**
-     * Method under test: {@link ApolloClient#createOrUpdateItem(String, Object, String)}
+     * Method under test: {@link ApolloClient#createOrUpdateItem(String, Object, String)}.
      */
     @Test
     public void testCreateOrUpdateItem() {
@@ -69,7 +82,7 @@ public class ApolloClientTest {
     }
 
     /**
-     * Method under test: {@link ApolloClient#createOrUpdateItem(String, String, String)}
+     * Method under test: {@link ApolloClient#createOrUpdateItem(String, String, String)}.
      */
     @Test
     public void testCreateOrUpdateItem2() {
@@ -80,7 +93,7 @@ public class ApolloClientTest {
     }
 
     /**
-     * Method under test: {@link ApolloClient#publishNamespace(String, String)}
+     * Method under test: {@link ApolloClient#publishNamespace(String, String)}.
      */
     @Test
     public void testPublishNamespace() {
--- shenyu-admin/src/test/java/org/apache/shenyu/admin/listener/apollo/ApolloDataChangedInitTest.java
+++ shenyu-admin/src/test/java/org/apache/shenyu/admin/listener/apollo/ApolloDataChangedInitTest.java
@@ -17,8 +17,8 @@
 
 package org.apache.shenyu.admin.listener.apollo;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doNothing;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,18 +31,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration(classes = {ApolloDataChangedListener.class})
+@ContextConfiguration(classes = {ApolloDataChangedInit.class})
 @RunWith(SpringJUnit4ClassRunner.class)
-public class ApolloDataChangedListenerTest {
+public class ApolloDataChangedInitTest {
 
     @MockBean
     private ApolloClient apolloClient;
 
     @Autowired
-    private ApolloDataChangedListener apolloDataChangedListener;
+    private ApolloDataChangedInit apolloDataChangedInit;
 
     /**
-     * Method under test: {@link ApolloDataChangedListener#ApolloDataChangedListener(ApolloClient)}.
+     * Method under test: {@link ApolloDataChangedInit#ApolloDataChangedInit(ApolloClient)}.
      */
     @Test
     public void testConstructor() {
@@ -54,39 +54,18 @@ public class ApolloDataChangedListenerTest {
         apolloConfig.setNamespace("Namespace");
         apolloConfig.setPortalUrl("http://localhost:8080");
         apolloConfig.setToken("ABC123");
-        new ApolloDataChangedListener(new ApolloClient(apolloConfig));
+        ApolloClient apolloClient = new ApolloClient(apolloConfig);
+        ApolloDataChangedInit actualApolloDataChangedInit = new ApolloDataChangedInit(apolloClient);
+        assertNotNull(actualApolloDataChangedInit);
     }
 
     /**
-     * Method under test: {@link ApolloDataChangedListener#publishConfig(String, Object)}.
+     * Method under test: {@link ApolloDataChangedInit#notExist()}.
      */
     @Test
-    public void testPublishConfig() {
-        doNothing().when(apolloClient)
-                .createOrUpdateItem(Mockito.<String>any(), Mockito.<Object>any(), Mockito.<String>any());
-        doNothing().when(apolloClient).publishNamespace(Mockito.<String>any(), Mockito.<String>any());
-        apolloDataChangedListener.publishConfig("42", "Data");
-        verify(apolloClient).createOrUpdateItem(Mockito.<String>any(), Mockito.<Object>any(), Mockito.<String>any());
-        verify(apolloClient).publishNamespace(Mockito.<String>any(), Mockito.<String>any());
-    }
-
-    /**
-     * Method under test: {@link ApolloDataChangedListener#getConfig(String)}.
-     */
-    @Test
-    public void testGetConfig() {
+    public void testNotExist() {
         when(apolloClient.getItemValue(Mockito.<String>any())).thenReturn("42");
-        assertEquals("42", apolloDataChangedListener.getConfig("42"));
-        verify(apolloClient).getItemValue(Mockito.<String>any());
-    }
-
-    /**
-     * Method under test: {@link ApolloDataChangedListener#getConfig(String)}.
-     */
-    @Test
-    public void testGetConfig2() {
-        when(apolloClient.getItemValue(Mockito.<String>any())).thenReturn("");
-        assertEquals("{}", apolloDataChangedListener.getConfig("42"));
+        assertFalse(apolloDataChangedInit.notExist());
         verify(apolloClient).getItemValue(Mockito.<String>any());
     }
 }
