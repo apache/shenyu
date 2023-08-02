5702263d9b8ef513e20e4932252c52e6765e3b2a
gongzhongqiang <gongzhongqiang@gigacloudtech.com>
2023-08-02 13:15:27 +0800

    [type:fix] Polish Code.

--- shenyu-common/src/test/java/org/apache/shenyu/common/cache/WindowTinyLFUMapTest.java
+++ shenyu-common/src/test/java/org/apache/shenyu/common/cache/WindowTinyLFUMapTest.java
@@ -28,10 +28,11 @@ import java.util.Map;
 public class WindowTinyLFUMapTest {
     
     @Test
+    @SuppressWarnings("all")
     public void weakKeyCache() {
         Map<String, String> map = new WindowTinyLFUMap<>(100, 100, Boolean.TRUE);
-        String key1 = "abc";
-        String key2 = "abc";
+        String key1 = new String("abc");
+        String key2 = new String("abc");
         map.put(key1, "1");
         map.put(key2, "1");
         Assert.assertEquals(2, map.size());
@@ -40,10 +41,11 @@ public class WindowTinyLFUMapTest {
     }
     
     @Test
+    @SuppressWarnings("all")
     public void strongKeyCache() {
         Map<String, String> map = new WindowTinyLFUMap<>(100, 100, Boolean.FALSE);
-        String key1 = "abc";
-        String key2 = "abc";
+        String key1 = new String("abc");
+        String key2 = new String("abc");
         map.put(key1, "1");
         map.put(key2, "1");
         Assert.assertEquals(map.get(key1), map.get(key2));
