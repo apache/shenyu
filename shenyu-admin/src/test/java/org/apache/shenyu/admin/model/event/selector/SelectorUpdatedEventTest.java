654a4ff4a4bed102669684ed36fe4ca3a4ae6d25
hanchao53 <hanchao53@jd.com>
2023-08-08 16:18:33 +0800

    [type: test]: Add selector event test

---
+++ shenyu-admin/src/test/java/org/apache/shenyu/admin/model/event/selector/SelectorUpdatedEventTest.java
@@ -0,0 +1,56 @@
+package org.apache.shenyu.admin.model.event.selector;
+
+import org.apache.shenyu.admin.model.dto.SelectorConditionDTO;
+import org.apache.shenyu.admin.model.dto.SelectorDTO;
+import org.apache.shenyu.admin.model.entity.SelectorDO;
+import org.apache.shenyu.common.enums.SelectorTypeEnum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SelectorUpdatedEventTest {
+
+    private SelectorDO selectorDO;
+
+    private SelectorUpdatedEvent selectorUpdatedEvent;
+
+    @BeforeEach
+    void init() {
+        selectorDO = buildSelectorDO();
+
+        selectorUpdatedEvent = new SelectorUpdatedEvent(selectorDO, selectorDO, "test-operator");
+    }
+
+    @Test
+    void getSelector() {
+        SelectorDO selector = selectorUpdatedEvent.getSelector();
+
+        assertEquals(selectorDO, selector);
+    }
+
+    private SelectorDO buildSelectorDO() {
+        SelectorDTO selectorDTO = new SelectorDTO();
+        selectorDTO.setId("456");
+        selectorDTO.setPluginId("789");
+        selectorDTO.setName("kuan");
+        selectorDTO.setType(SelectorTypeEnum.FULL_FLOW.getCode());
+        selectorDTO.setHandle("[{\"upstreamHost\": \"127.0.0.1\", \"protocol\": \"http://\", \"upstreamUrl\": \"anotherUrl\"}]");
+        SelectorConditionDTO selectorConditionDTO1 = new SelectorConditionDTO();
+        selectorConditionDTO1.setId("111");
+        selectorConditionDTO1.setSelectorId("456");
+        SelectorConditionDTO selectorConditionDTO2 = new SelectorConditionDTO();
+        selectorConditionDTO2.setId("222");
+        selectorConditionDTO2.setSelectorId("456");
+        selectorDTO.setSelectorConditions(Arrays.asList(selectorConditionDTO1, selectorConditionDTO2));
+        SelectorDO selectorDO = SelectorDO.buildSelectorDO(selectorDTO);
+        Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+        selectorDO.setDateCreated(now);
+        selectorDO.setDateUpdated(now);
+        return selectorDO;
+    }
+}
