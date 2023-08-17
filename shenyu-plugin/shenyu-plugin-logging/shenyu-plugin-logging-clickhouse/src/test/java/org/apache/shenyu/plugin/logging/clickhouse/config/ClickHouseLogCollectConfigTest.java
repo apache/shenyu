4ee34f8a6cb19d39920b2811a9f6025ef71ed898
misaya295 <864730420@qq.com>
2023-08-17 12:19:49 +0800

    Revert "fix style"
    This reverts commit 68ec8a4e5167c25c3e4ef2a5ba8b23e1ba36d1bf.

--- shenyu-plugin/shenyu-plugin-logging/shenyu-plugin-logging-clickhouse/src/test/java/org/apache/shenyu/plugin/logging/clickhouse/config/ClickHouseLogCollectConfigTest.java
+++ shenyu-plugin/shenyu-plugin-logging/shenyu-plugin-logging-clickhouse/src/test/java/org/apache/shenyu/plugin/logging/clickhouse/config/ClickHouseLogCollectConfigTest.java
@@ -38,7 +38,6 @@ public class ClickHouseLogCollectConfigTest {
         clickHouseLogConfig.setUsername("foo");
         clickHouseLogConfig.setPassword("bar");
         clickHouseLogConfig.setTtl("30");
-        clickHouseLogConfig.setClusterName("clusterName");
         clickHouseLogConfig.setDatabase(GenericLoggingConstant.DEFAULT_SOURCE);
     }
 
@@ -124,17 +123,5 @@ public class ClickHouseLogCollectConfigTest {
     public void testGetTTL() {
         final String ttl = clickHouseLogConfig.getTtl();
         Assertions.assertEquals("30", ttl);
-    }
-
-    @Test
-    public void testSetClusterName() {
-        clickHouseLogConfig.setClusterName("clusterName");
-        Assertions.assertNull(null);
-    }
-
-    @Test
-    public void testGetClusterName() {
-        final String clusterName = clickHouseLogConfig.getClusterName();
-        Assertions.assertEquals("clusterName", clusterName);
     }
 }
