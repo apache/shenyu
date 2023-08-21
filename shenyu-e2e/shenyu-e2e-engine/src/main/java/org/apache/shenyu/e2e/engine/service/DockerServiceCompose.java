517bf5c6442938a8505d6de9550e221bb1a7fab1
haiqi <2683064908@qq.com>
2023-08-21 23:35:25 +0800

    test etcd syn method08

--- shenyu-e2e/shenyu-e2e-engine/src/main/java/org/apache/shenyu/e2e/engine/service/DockerServiceCompose.java
+++ shenyu-e2e/shenyu-e2e-engine/src/main/java/org/apache/shenyu/e2e/engine/service/DockerServiceCompose.java
@@ -281,16 +281,12 @@ public class DockerServiceCompose implements ServiceCompose {
             Map<String, Object> shenyuParameter = (Map<String, Object>) yamlData.get("shenyu");
             Map<String, Object> parameter = (Map<String, Object>) shenyuParameter.get("sync");
             Map<String, Object> subParameters = DataSyncHandler.getDataSynMap(dockerServiceConfigure.getProperties().getProperty("dataSyn"));
-            log.info("----" + parameter.toString() + "----");
-            log.info("----" + subParameters.toString() + "----");
-            log.info("----" + dockerServiceConfigure.getProperties().getProperty("dataSyn") + "----");
             String synMethod = "";
             if (dockerServiceConfigure.getProperties().getProperty("dataSyn").contains("_")) {
                 synMethod = dockerServiceConfigure.getProperties().getProperty("dataSyn").split("_")[1];
             } else {
                 synMethod = dockerServiceConfigure.getProperties().getProperty("dataSyn");
             }
-            log.info("----" + synMethod + "----");
             parameter.put(synMethod, subParameters);
             String finalSynMethod = synMethod;
             parameter.keySet().removeIf(key -> !key.equals(finalSynMethod));
@@ -311,6 +307,10 @@ public class DockerServiceCompose implements ServiceCompose {
                     log.info(line);
                 }
                 reader.close();
+                log.info("---- " + parameter.toString() + " ----");
+                log.info("---- " + subParameters.toString() + " ----");
+                log.info("---- " + dockerServiceConfigure.getProperties().getProperty("dataSyn") + " ----");
+                log.info("---- " + synMethod + " ----");
             } catch (IOException e) {
                 log.error(e.getMessage(), e);
             }
