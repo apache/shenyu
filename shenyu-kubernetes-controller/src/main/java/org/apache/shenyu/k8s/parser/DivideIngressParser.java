41f60fb2cfb7b627e4710085edc1a3fd734af6ec
runqi-zhao <529978097@qq.com>
2023-08-25 10:44:24 +0800

    support config protocol in annotation

--- shenyu-kubernetes-controller/src/main/java/org/apache/shenyu/k8s/parser/DivideIngressParser.java
+++ shenyu-kubernetes-controller/src/main/java/org/apache/shenyu/k8s/parser/DivideIngressParser.java
@@ -234,7 +234,7 @@ public class DivideIngressParser implements K8sResourceParser<V1Ingress> {
                             .logged(false)
                             .continued(true)
                             .conditionList(conditionList).build();
-                    List<DivideUpstream> upstreamList = parseUpstream(path.getBackend(), namespace);
+                    List<DivideUpstream> upstreamList = parseUpstream(path.getBackend(), namespace, annotations);
                     if (upstreamList.isEmpty()) {
                         upstreamList = defaultUpstream;
                     }
@@ -275,14 +275,13 @@ public class DivideIngressParser implements K8sResourceParser<V1Ingress> {
         return null;
     }
 
-    private List<DivideUpstream> parseUpstream(final V1IngressBackend backend, final String namespace) {
+    private List<DivideUpstream> parseUpstream(final V1IngressBackend backend, final String namespace, final Map<String, String> annotations) {
         List<DivideUpstream> upstreamList = new ArrayList<>();
         if (Objects.nonNull(backend) && Objects.nonNull(backend.getService()) && Objects.nonNull(backend.getService().getName())) {
             String serviceName = backend.getService().getName();
             // shenyu routes directly to the container
             V1Endpoints v1Endpoints = endpointsLister.namespace(namespace).get(serviceName);
             List<V1EndpointSubset> subsets = v1Endpoints.getSubsets();
-            Map<String, String> annotations = Objects.isNull(v1Endpoints.getMetadata().getAnnotations()) ? null : v1Endpoints.getMetadata().getAnnotations();
             String protocol = Objects.isNull(annotations) ? null : annotations.get(IngressConstants.UPSTREAMS_PROTOCOL_ANNOTATION_KEY);
             if (Objects.isNull(subsets) || CollectionUtils.isEmpty(subsets)) {
                 LOG.info("Endpoints {} do not have subsets", serviceName);
