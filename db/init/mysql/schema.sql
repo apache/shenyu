5eeb26b6587cfc3c67dc6800fa6304d4d733e7d4
bert82503 <bert825_work@163.com>
2023-08-27 01:10:29 +0800

    Add TABLE `tag_relation` index

--- db/init/mysql/schema.sql
+++ db/init/mysql/schema.sql
@@ -2016,7 +2016,9 @@ CREATE TABLE `tag_relation`
     `tag_id`       varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'parent tag id',
     `date_created` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'create time',
     `date_updated` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT 'update time',
-    PRIMARY KEY (`id`) USING BTREE
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `idx_api_id` (`api_id`),
+    INDEX `idx_tag_id` (`tag_id`)
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 
