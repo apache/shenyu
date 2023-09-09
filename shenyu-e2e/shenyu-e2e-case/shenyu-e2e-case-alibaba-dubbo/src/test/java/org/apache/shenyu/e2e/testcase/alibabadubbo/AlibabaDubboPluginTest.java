7d379e5abce21f931f614dc87accca0d0685a659
haiqi <2683064908@qq.com>
2023-09-09 14:14:34 +0800

    modify e2e data sync 05

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-alibaba-dubbo/src/test/java/org/apache/shenyu/e2e/testcase/alibabadubbo/AlibabaDubboPluginTest.java
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-alibaba-dubbo/src/test/java/org/apache/shenyu/e2e/testcase/alibabadubbo/AlibabaDubboPluginTest.java
@@ -78,8 +78,8 @@ public class AlibabaDubboPluginTest {
     @BeforeAll
     static void setup(final AdminClient adminClient, final GatewayClient gatewayClient) throws InterruptedException, JsonProcessingException {
         adminClient.login();
+        modify e2e data sync again
         Thread.sleep(10000);
-        adminClient.syncPluginAll();
         List<SelectorDTO> selectorDTOList = adminClient.listAllSelectors();
         List<SelectorCacheData> selectorCacheList = gatewayClient.getSelectorCache();
         Assertions.assertEquals(selectorDTOList.size(), selectorCacheList.size());
