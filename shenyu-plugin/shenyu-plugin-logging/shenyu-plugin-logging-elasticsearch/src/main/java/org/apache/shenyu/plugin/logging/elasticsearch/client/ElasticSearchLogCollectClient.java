9fb72f5e01708381aa66caf9d8493d370112290c
misaya295 <864730420@qq.com>
2023-09-14 23:51:10 +0800

    fix indexName not working for es-logging

--- shenyu-plugin/shenyu-plugin-logging/shenyu-plugin-logging-elasticsearch/src/main/java/org/apache/shenyu/plugin/logging/elasticsearch/client/ElasticSearchLogCollectClient.java
+++ shenyu-plugin/shenyu-plugin-logging/shenyu-plugin-logging-elasticsearch/src/main/java/org/apache/shenyu/plugin/logging/elasticsearch/client/ElasticSearchLogCollectClient.java
@@ -58,6 +58,8 @@ public class ElasticSearchLogCollectClient extends AbstractLogConsumeClient<Elas
 
     private ElasticsearchClient client;
 
+    private String indexName = GenericLoggingConstant.INDEX;
+
     /**
      * init elasticsearch client.
      *
@@ -84,10 +86,11 @@ public class ElasticSearchLogCollectClient extends AbstractLogConsumeClient<Elas
         restClient = builder.build();
         transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         client = new ElasticsearchClient(transport);
+        this.indexName = config.getIndexName();
         LogUtils.info(LOG, "init ElasticSearchLogCollectClient success");
         // Determine whether the index exists, and create it if it does not exist
-        if (!existsIndex(config.getIndexName())) {
-            createIndex(config.getIndexName());
+        if (!existsIndex(indexName)) {
+            createIndex(indexName);
             LogUtils.info(LOG, "create index success");
         }
     }
@@ -97,14 +100,14 @@ public class ElasticSearchLogCollectClient extends AbstractLogConsumeClient<Elas
         List<BulkOperation> bulkOperations = new ArrayList<>();
         logs.forEach(log -> {
             try {
-                bulkOperations.add(new BulkOperation.Builder().create(d -> d.document(log).index(GenericLoggingConstant.INDEX)).build());
+                bulkOperations.add(new BulkOperation.Builder().create(d -> d.document(log).index(indexName)).build());
             } catch (Exception e) {
                 LogUtils.error(LOG, "add logs error: ", e);
             }
         });
         // Bulk storage
         try {
-            client.bulk(e -> e.index(GenericLoggingConstant.INDEX).operations(bulkOperations));
+            client.bulk(e -> e.index(indexName).operations(bulkOperations));
         } catch (Exception e) {
             LogUtils.error(LOG, "elasticsearch store logs error:", e);
         }
