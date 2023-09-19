0c8292b7a023aa47d160b00f33c0e778d0840bf5
moremind <hefengen@apache.org>
2023-09-19 14:57:39 +0800

    [type:feat] support e2e contain in k8s

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-http/k8s/script/e2e-test-mysql.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-http/k8s/script/e2e-test-mysql.sh
@@ -16,21 +16,11 @@
 # limitations under the License.
 #
 
-## init kubernetes for mysql
+echo -e `nc -z -v -w30 shenyu-mysql 30306`
+
 curPath=$(readlink -f "$(dirname "$0")")
 PRGDIR=`dirname "$curPath"`
 echo $PRGDIR
-kubectl apply -f ${PRGDIR}/shenyu-mysql.yml
-sleep 10s
-
-kubectl get pod -o wide
-
-sleep 60s
-
-kubectl get pod -o wide
-
-echo -e `nc -z -v -w30 shenyu-mysql 30306`
-
 kubectl apply -f ${PRGDIR}/shenyu-deployment-mysql.yml
 kubectl apply -f ${PRGDIR}/shenyu-app-service-mysql.yml
 
@@ -38,36 +28,8 @@ kubectl get pod -o wide
 
 sleep 60s
 
-kubectl run -it --rm --image=mysql:8.0 --restart=Never mysql-client -- mysql -h shenyu-mysql -uroot -pshenyue2e -e "show databases;"
 kubectl get pod -o wide
 
 chmod +x ${curPath}/healthcheck.sh
 sh ${curPath}/healthcheck.sh mysql
-#
-#kubectl logs -l app=shenyu-admin-mysql
-#
-#sleep 10s
-#
-#kubectl -n kube-system  get pods | grep Evicted |awk '{print$1}'|xargs kubectl -n kube-system delete pods
-#kubectl get pod -o wide
-#
-## execute healthcheck.sh
-#chmod +x ${curPath}/healthcheck.sh
-#sh ${curPath}/healthcheck.sh mysql
-#
-### run e2e-test
-#curl http://localhost:31196/actuator/shenyu/pluginData
 
-### wait shenyu-admin and shenyu-bootstrap start
-
-# get shenyu-admin pod name
-#
-#shenyu_admin_pod_name=`kubectl get pod -n kube-system | grep shenyu-admin | awk '{print $1}'`
-#
-## get shenyu-bootstrap pod name
-#
-#shenyu_bootstrap_pod_name=`kubectl get pod -n kube-system | grep shenyu-bootstrap | awk '{print $1}'`
-#
-## get shenyu-admin pod status
-#
-#shenyu_admin_pod_status=`kubectl get pod -n kube-system | grep shenyu-admin | awk '{print $3}'`
