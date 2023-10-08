03d079a7427024888b10694786dead1a549f7125
moremind <hefengen@apache.org>
2023-10-08 13:36:39 +0800

    [type:fix] e2e k8s

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-spring-cloud/k8s/script/healthcheck.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-spring-cloud/k8s/script/healthcheck.sh
@@ -18,31 +18,28 @@
 
 PRGDIR=$(dirname "$0")
 
-for service in $(grep -v -E "^$|^#" "${PRGDIR}"/services-"${1}".list)
-do
-    for loop in $(seq 1 30)
-    do
-        status=$(curl -o /dev/null -s -w %{http_code} "$service")
-        echo -e "curl $service response $status"
+for service in $(grep -v -E "^$|^#" "${PRGDIR}"/services-"${1}".list); do
+  for loop in $(seq 1 30); do
+    status=$(curl -o /dev/null -s -w %{http_code} "$service")
+    echo -e "curl $service response $status"
 
-        if [ "$status" -eq 200 ]; then
-            break
-        fi
+    if [ "$status" -eq 200 ]; then
+      break
+    fi
 
-        sleep 2
-    done
+    sleep 2
+  done
 done
-
 
 admin_status=$(curl -s -o /dev/null -w "%{http_code}" -X GET "${2}" -H "accept: */*")
 bootstrap_status=$(curl -s -o /dev/null -w "%{http_code}" -X GET "${3}" -H "accept: */*")
 
 if [ "$admin_status" -eq 200 -a "$bootstrap_status" -eq 200 ]; then
-    echo -e "\n-------------------"
-    echo -e "Success to send request: $admin_status"
-    echo -e "Success to send request: $bootstrap_status"
-    echo -e "\n-------------------"
-    exit 0
+  echo -e "\n-------------------"
+  echo -e "Success to send request: $admin_status"
+  echo -e "Success to send request: $bootstrap_status"
+  echo -e "\n-------------------"
+  exit 0
 fi
 echo -e "\n-------------------"
 echo -e "Failed to send request from shenyu-admin : $admin_status"
