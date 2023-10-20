717d0f9ce1b47ba947e70a0c4c98ae7abe2265c8
yunlongn <yunlongn@outlook.com>
2023-10-20 16:03:07 +0800

    [type:fix] add log.

--- shenyu-plugin/shenyu-plugin-proxy/shenyu-plugin-rpc/shenyu-plugin-sofa/src/main/java/org/apache/shenyu/plugin/sofa/cache/ApplicationConfigCache.java
+++ shenyu-plugin/shenyu-plugin-proxy/shenyu-plugin-rpc/shenyu-plugin-sofa/src/main/java/org/apache/shenyu/plugin/sofa/cache/ApplicationConfigCache.java
@@ -204,6 +204,7 @@ public final class ApplicationConfigCache {
         reference.setRegistry(registryConfig);
         reference.setInterfaceId(metaData.getServiceName());
         reference.setProtocol(RpcConstants.PROTOCOL_TYPE_BOLT);
+        reference.setSerialization(RpcConstants.SERIALIZE_PROTOBUF);
         reference.setInvokeType(RpcConstants.INVOKER_TYPE_CALLBACK);
         reference.setRepeatedReferLimit(-1);
         String rpcExt = metaData.getRpcExt();
