8f26ce2413a2b39e88e636327532e8744076d8e1
yunlongn <yunlongn@outlook.com>
2023-10-20 11:42:01 +0800

    [type:fix] add log.

--- shenyu-bootstrap/src/main/java/org/apache/shenyu/bootstrap/ShenyuBootstrapApplication.java
+++ shenyu-bootstrap/src/main/java/org/apache/shenyu/bootstrap/ShenyuBootstrapApplication.java
@@ -32,6 +32,7 @@ public class ShenyuBootstrapApplication {
      * @param args startup arguments
      */
     public static void main(final String[] args) {
+        System.setProperty("logger.impl", "com.alipay.sofa.rpc.log.SLF4JLoggerImpl");
         SpringApplication.run(ShenyuBootstrapApplication.class, args);
     }
 }
