4e264b185c7f3dee13ae1365b574a3d2496f7654
whenelse <41423774+whenelse@users.noreply.github.com>
2023-11-03 15:53:05 +0800

    [type:test][ISSUE #4540] add all event test cases for admin (#5263)
    * [type:test][ISSUE #4540] add plugin test cases for admin
    
    * [type:test][ISSUE #4540] add Apache License for plugin test cases for admin
    
    * [type:test][ISSUE #4540] add CheckStyle for plugin test cases for admin
    
    * [type:test][ISSUE #4540] add batch plugin test cases for admin
    
    * [type:test][ISSUE #4540] add plugin handle test cases for admin
    
    * [type:test]add metadata test case for admin
    
    * [type:refactor]refactor metadata comment and format
    
    * [type:test]add dict test case for admin
    
    * [type:test]add resource and user test case for admin
    
    * [type:fix]resource test case checkstyle
    
    ---------
    
    Co-authored-by: Shibo Xia <xiashibo@rd.netease.com>
    Co-authored-by: moremind <hefengen@apache.org>

---
+++ shenyu-admin/src/test/java/org/apache/shenyu/admin/model/event/metadata/BatchMetaDataChangedEventTest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.admin.model.event.metadata;
+
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shenyu.admin.model.entity.MetaDataDO;
+import org.apache.shenyu.admin.model.enums.EventTypeEnum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * test case for {@link BatchMetaDataChangedEvent}.
+ */
+public class BatchMetaDataChangedEventTest {
+
+    private MetaDataDO one;
+
+    private MetaDataDO two;
+
+    @BeforeEach
+    public void setUp() {
+        one = MetaDataDO.builder()
+                .id("1")
+                .appName("testAppNameOne")
+                .path("/testPathOne")
+                .pathDesc("testPathDescOne")
+                .rpcType("http")
+                .serviceName("org.apache.shenyu.examples.http.controller.TestOneController")
+                .methodName("post")
+                .parameterTypes("java.lang.String")
+                .enabled(true)
+                .build();
+
+        two = MetaDataDO.builder()
+                .id("2")
+                .appName("testAppNameTwo")
+                .path("/testPathTwo")
+                .pathDesc("testPathDescTwo")
+                .rpcType("http")
+                .serviceName("org.apache.shenyu.examples.http.controller.TestTwoController")
+                .methodName("post")
+                .parameterTypes("java.lang.String")
+                .enabled(true)
+                .build();
+    }
+
+    @Test
+    public void batchMetaDataChangedContextTest() {
+        BatchMetaDataChangedEvent batchMetaDataChangedEvent =
+                new BatchMetaDataChangedEvent(Arrays.asList(one, two), null, EventTypeEnum.META_DATA_UPDATE, "test-operator");
+
+        String context = String.format("the meta data [%s] is %s",
+                "testAppNameOne,testAppNameTwo", StringUtils.lowerCase(EventTypeEnum.META_DATA_UPDATE.getType().toString()));
+
+        assertEquals(context, batchMetaDataChangedEvent.buildContext());
+    }
+}
