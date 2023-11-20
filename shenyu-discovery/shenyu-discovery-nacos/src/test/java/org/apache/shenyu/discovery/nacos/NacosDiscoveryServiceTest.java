e4da5b1f54f9735e28888b8a98b2cec231d1f1e9
lulu <1534972955@qq.com>
2023-11-20 18:11:41 +0800

    [type:test] Add the statement at the beginning

--- shenyu-discovery/shenyu-discovery-nacos/src/test/java/org/apache/shenyu/discovery/nacos/NacosDiscoveryServiceTest.java
+++ shenyu-discovery/shenyu-discovery-nacos/src/test/java/org/apache/shenyu/discovery/nacos/NacosDiscoveryServiceTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shenyu.discovery.nacos;
 
 import com.alibaba.nacos.api.exception.NacosException;
@@ -124,7 +141,8 @@ class NacosDiscoveryServiceTest {
 
         // Mock the behavior of namingService.subscribe
         when(namingService.selectInstances(key, "SHENYU_GROUP", true))
-                .thenReturn(initialInstances, updatedInstances);
+                .thenReturn(initialInstances)
+                .thenReturn(updatedInstances);
         doAnswer(invocation -> {
             EventListener nacosListener = invocation.getArgument(2);
             NamingEvent event = new NamingEvent(key, updatedInstances);
@@ -206,7 +224,9 @@ class NacosDiscoveryServiceTest {
     }
 
     /**
-     * same as in NacosDiscoveryService
+     * Same as in NacosDiscoveryService.
+     *
+     * @param instance Convert Nacos instance to Json string
      */
     private String buildInstanceInfoJson(final Instance instance) {
         JsonObject instanceJson = new JsonObject();
