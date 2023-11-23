d518c8d73fbcdfa5cccb49c55045eda739e8c77a
杨文杰 <18856309550@163.com>
2023-11-23 21:09:03 +0800

    trigger ci

--- shenyu-integrated-test/shenyu-integrated-test-http/src/test/java/org/apache/shenyu/integrated/test/http/SpringMvcMappingPathControllerTest.java
+++ shenyu-integrated-test/shenyu-integrated-test-http/src/test/java/org/apache/shenyu/integrated/test/http/SpringMvcMappingPathControllerTest.java
@@ -19,8 +19,8 @@ package org.apache.shenyu.integrated.test.http;
 
 import org.junit.jupiter.api.Test;
 import org.apache.shenyu.integratedtest.common.AbstractTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.apache.shenyu.integratedtest.common.helper.HttpHelper;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 
