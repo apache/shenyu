7df0b316feb3396823cd7d64d57ffaaccb164c79
zzzk1 <madzhou1@gmail.com>
2023-11-23 23:24:54 +0800

    try e2e storage test

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-storage/k8s/script/e2e-oracle.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-storage/k8s/script/e2e-oracle.sh
@@ -34,7 +34,7 @@ sleep 30s
 kubectl get pod -o wide
 
 chmod +x "${curPath}"/healthcheck.sh
-sh "${curPath}"/healthcheck.sh mysql http://localhost:31095/actuator/health http://localhost:31195/actuator/health
+sh "${curPath}"/healthcheck.sh oracle http://localhost:31095/actuator/health http://localhost:31195/actuator/health
 
 kubectl logs "$(kubectl get pod -o wide | grep shenyu-admin | awk '{print $1}')"
 
