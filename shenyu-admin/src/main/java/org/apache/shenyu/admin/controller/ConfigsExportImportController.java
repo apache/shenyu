233f4ead9041781844496e9e1b3e902bf01b1cc6
liuhy <rokkki@163.com>
2024-03-16 15:47:05 +0800

    Export Selector Data should be combined with Discovery Data #5492

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/ConfigsExportImportController.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/ConfigsExportImportController.java
@@ -105,6 +105,7 @@ public class ConfigsExportImportController {
         try {
             ShenyuAdminResult importResult = configsService.configsImport(file.getBytes());
             if (importResult.getCode().equals(CommonErrorCode.SUCCESSFUL)) {
+                // sync data
                 syncDataService.syncAll(DataEventTypeEnum.REFRESH);
             }
             return importResult;
