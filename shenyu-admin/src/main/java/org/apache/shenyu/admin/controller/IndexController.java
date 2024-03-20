36c82f4f57c1a9f2e131e9e964bfae30f40a07ce
VampireAchao <achao@apache.org>
2024-03-20 21:16:51 +0800

    [Fix] Resolve dashboard routing mismatch post context-path update

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/IndexController.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/controller/IndexController.java
@@ -34,7 +34,7 @@ public class IndexController {
      * @param model the model
      * @return the string
      */
-    @RequestMapping("/index")
+    @RequestMapping("/")
     public String index(final Model model) {
         model.addAttribute("domain", ShenyuDomain.getInstance().getHttpPath());
         return "index";
