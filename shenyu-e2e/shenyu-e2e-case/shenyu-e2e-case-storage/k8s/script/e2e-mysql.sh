d92d76524acf1be697aca9cf92c651330ad498f4
liuhy <rokkki@163.com>
2024-04-18 15:38:27 +0800

    e2e storage fail debug

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-storage/k8s/script/e2e-mysql.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-storage/k8s/script/e2e-mysql.sh
@@ -36,6 +36,10 @@ kubectl get pod -o wide
 chmod +x "${curPath}"/healthcheck.sh
 sh "${curPath}"/healthcheck.sh mysql http://localhost:31095/actuator/health http://localhost:31195/actuator/health
 
+
+kubectl logs "$(kubectl get pod -o wide | grep shenyu-mysql | awk '{print $1}')"
+
+kubectl logs "$(kubectl get pod -o wide | grep shenyu-admin | awk '{print $1}')"
 ## run e2e-test
 
 curl -S "http://localhost:31195/actuator/pluginData"
