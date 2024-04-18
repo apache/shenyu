a0f4e074d13eb0b5976e6f8d0d08a65e3471ed23
liuhy <rokkki@163.com>
2024-04-18 09:07:07 +0800

    jdk17 support: gson modify

--- shenyu-common/src/main/java/org/apache/shenyu/common/utils/GsonUtils.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/utils/GsonUtils.java
@@ -73,7 +73,7 @@ public class GsonUtils {
             .registerTypeAdapter(String.class, new StringTypeAdapter())
             .registerTypeHierarchyAdapter(Pair.class, new PairTypeAdapter())
             .registerTypeHierarchyAdapter(Duration.class, new DurationTypeAdapter())
-            .registerTypeHierarchyAdapter(Timestamp.class, new TimestampTypeAdapter())
+//            .registerTypeHierarchyAdapter(Timestamp.class, new TimestampTypeAdapter())
             .create();
 
     private static final Gson GSON_MAP = new GsonBuilder().serializeNulls().registerTypeHierarchyAdapter(new TypeToken<Map<String, Object>>() {
@@ -499,32 +499,32 @@ public class GsonUtils {
         }
     }
 
-    private static class TimestampTypeAdapter extends TypeAdapter<Timestamp> {
-        @Override
-        public void write(final JsonWriter out, final Timestamp value) {
-            try {
-                if (value == null) {
-                    out.nullValue();
-                    return;
-                }
-                out.value(value.getTime());
-            } catch (IOException e) {
-                LOG.error("failed to write", e);
-            }
-        }
-
-        @Override
-        public Timestamp read(final JsonReader reader) {
-            try {
-                if (reader.peek() == JsonToken.NULL) {
-                    reader.nextNull();
-                    return null;
-                }
-                long timestamp = reader.nextLong();
-                return new Timestamp(timestamp);
-            } catch (IOException e) {
-                throw new ShenyuException(e);
-            }
-        }
-    }
+//    private static class TimestampTypeAdapter extends TypeAdapter<Timestamp> {
+//        @Override
+//        public void write(final JsonWriter out, final Timestamp value) {
+//            try {
+//                if (value == null) {
+//                    out.nullValue();
+//                    return;
+//                }
+//                out.value(value.getTime());
+//            } catch (IOException e) {
+//                LOG.error("failed to write", e);
+//            }
+//        }
+//
+//        @Override
+//        public Timestamp read(final JsonReader reader) {
+//            try {
+//                if (reader.peek() == JsonToken.NULL) {
+//                    reader.nextNull();
+//                    return null;
+//                }
+//                long timestamp = reader.nextLong();
+//                return new Timestamp(timestamp);
+//            } catch (IOException e) {
+//                throw new ShenyuException(e);
+//            }
+//        }
+//    }
 }
