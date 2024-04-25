c3cf66236a418c406e8cf06ee9634bdd34d91849
liuhy <rokkki@163.com>
2024-04-25 16:32:30 +0800

    ci debug

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/utils/HttpUtils.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/utils/HttpUtils.java
@@ -682,7 +682,7 @@ public class HttpUtils {
                         input.close();
                     }
                 } catch (IOException ioe) {
-                    LOG.error(e);
+                    LOG.error(ioe);
                 }
             }
         }
