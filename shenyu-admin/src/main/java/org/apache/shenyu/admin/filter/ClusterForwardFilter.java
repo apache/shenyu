c89deb398fb210bf30dd73c10e80954eb74e5688
liuhy <rokkki@163.com>
2024-05-02 18:44:15 +0800

    ShenYu Admin Cluster #5448

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/filter/ClusterForwardFilter.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/filter/ClusterForwardFilter.java
@@ -17,14 +17,15 @@
 
 package org.apache.shenyu.admin.filter;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shenyu.admin.config.properties.ClusterProperties;
 import org.apache.shenyu.admin.model.dto.ClusterMasterDTO;
 import org.apache.shenyu.admin.service.ClusterMasterService;
 import org.jetbrains.annotations.NotNull;
-import org.owasp.encoder.Encode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -35,6 +36,7 @@ import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Resource;
@@ -42,10 +44,10 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Writer;
 import java.util.Collections;
 import java.util.Objects;
 
@@ -67,6 +69,10 @@ public class ClusterForwardFilter extends OncePerRequestFilter {
     
     @Resource
     private ClusterProperties clusterProperties;
+    
+    @Autowired
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+    
     
     @Override
     protected void doFilterInternal(@NotNull final HttpServletRequest request,
@@ -120,7 +126,15 @@ public class ClusterForwardFilter extends OncePerRequestFilter {
         if (StringUtils.isNotEmpty(request.getQueryString())) {
             builder.query(request.getQueryString());
         }
-
+//        HandlerExecutionChain handlerExecutionChain = null;
+//            handlerExecutionChain = requestMappingHandlerMapping.getHandler(request);
+//            if (handlerExecutionChain == null) {
+//                // 没有匹配的处理程序
+//                return filterChain.doFilter(request, response);
+//            }
+//            // 获取目标方法
+//            HandlerMethod handlerMethod = (HandlerMethod) handlerExecutionChain.getHandler();
+//            Method method = handlerMethod.getMethod();
         LOG.debug("forwarding request to url: {}", builder.toUriString());
         // Create request entity
         HttpHeaders headers = new HttpHeaders();
@@ -128,7 +142,7 @@ public class ClusterForwardFilter extends OncePerRequestFilter {
         copyHeaders(request, headers);
         HttpEntity<byte[]> requestEntity = new HttpEntity<>(getBody(request), headers);
         // Send request
-        ResponseEntity<String> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.valueOf(request.getMethod()), requestEntity, String.class);
+        ResponseEntity<byte[]> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.valueOf(request.getMethod()), requestEntity, byte[].class);
 
         // Set response status and headers
         response.setStatus(responseEntity.getStatusCodeValue());
@@ -137,11 +151,13 @@ public class ClusterForwardFilter extends OncePerRequestFilter {
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 
-        try (Writer writer = response.getWriter()) {
+//        try (Writer writer = response.getWriter()) {
 //            writer.write(URLEncoder.encode(Objects.requireNonNull(responseEntity.getBody()), "UTF-8"));
-            writer.write(Encode.forJava(Objects.requireNonNull(responseEntity.getBody())));
-            writer.flush();
-        }
+//            writer.write();
+        IOUtils.copy(new ByteArrayInputStream(Objects.requireNonNull(responseEntity.getBody())), response.getOutputStream());
+        response.getOutputStream().flush();
+//            writer.flush();
+//        }
         
     }
     
@@ -152,10 +168,10 @@ public class ClusterForwardFilter extends OncePerRequestFilter {
     
     private void copyHeaders(final HttpHeaders sourceHeaders, final HttpServletResponse response) {
         sourceHeaders.forEach((headerName, headerValues) -> {
-//            String name = headerName.replaceAll("[^a-zA-Z ]", "");
-            if (!response.containsHeader(headerName)) {
+            String name = headerName.replaceAll("\r", "").replaceAll("\n", "");
+            if (!response.containsHeader(name)) {
                 headerValues.forEach(headerValue -> {
-                    response.addHeader(headerName, headerValue.replaceAll("\r", "").replaceAll("\n", ""));
+                    response.addHeader(name, headerValue.replaceAll("\r", "").replaceAll("\n", ""));
                 });
             }
         });
