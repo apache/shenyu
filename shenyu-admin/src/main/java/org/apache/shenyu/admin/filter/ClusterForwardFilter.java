6141f9377d8a19dc88e3df1a84ddd76e0f745275
liuhy <rokkki@163.com>
2024-05-02 14:45:27 +0800

    ShenYu Admin Cluster: url component builder #5448

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/filter/ClusterForwardFilter.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/filter/ClusterForwardFilter.java
@@ -24,8 +24,11 @@ import org.apache.shenyu.admin.service.ClusterMasterService;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
@@ -37,12 +40,13 @@ import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Writer;
 import java.util.Collections;
+import java.util.Objects;
 
 /**
  * Cluster forward filter.
@@ -97,79 +101,59 @@ public class ClusterForwardFilter extends OncePerRequestFilter {
         ClusterMasterDTO master = clusterMasterService.getMaster();
         String host = master.getMasterHost();
         String port = master.getMasterPort();
-//        String contextPath = master.getContextPath();
+        //        String contextPath = master.getContextPath();
         
         // new URL
-//        String uri = request.getRequestURI();
-//        String requestContextPath = request.getContextPath();
-//        uri = uri.replaceAll(requestContextPath, "");
-//        if (StringUtils.isNotEmpty(contextPath)) {
-//            uri = contextPath + "/" + uri;
-//        }
-//        UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
-//                .host(host)
-//                .port(port)
-//                .path(uri);
-        String originalUrl = request.getRequestURL().toString();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(originalUrl).host(host)
+        //        String uri = request.getRequestURI();
+        //        String requestContextPath = request.getContextPath();
+        //        uri = uri.replaceAll(requestContextPath, "");
+        //        if (StringUtils.isNotEmpty(contextPath)) {
+        //            uri = contextPath + "/" + uri;
+        //        }
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromHttpRequest(new ServletServerHttpRequest(request))
+                .host(host)
                 .port(port);
         
         // 添加查询参数
-//        if (StringUtils.isNotEmpty(request.getQueryString())) {
-//            builder.query(request.getQueryString());
-//        }
-//
-//        LOG.debug("forwarding request to url: {}", builder.toUriString());
-//        // Create request entity
-//        HttpHeaders headers = new HttpHeaders();
-//        copyHeaders(request, headers);
-//        HttpEntity<byte[]> requestEntity = new HttpEntity<>(getBody(request), headers);
-//        //        String urlWithParams = url;
-//        //        if (StringUtils.isNotEmpty(request.getQueryString())) {
-//        //            urlWithParams += "?" + request.getQueryString();
-//        //        }
-//        //        if (!urlWithParams.startsWith(clusterMasterService.getMasterUrl() + uri)) {
-//        //            throw new IllegalArgumentException("Invalid URL");
-//        //        }
-//        // Send request
-//        ResponseEntity<String> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.valueOf(request.getMethod()), requestEntity, String.class);
-//
-//        // Set response status and headers
-//        response.setStatus(responseEntity.getStatusCodeValue());
-//        copyHeaders(responseEntity.getHeaders(), response);
-//        // fix cors error
-//        response.addHeader("Access-Control-Allow-Origin", host);
-//        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//
-//        try (Writer writer = response.getWriter()) {
-//            writer.write(URLEncoder.encode(Objects.requireNonNull(responseEntity.getBody()), "UTF-8"));
-//            writer.flush();
-//        }
-        HttpServletRequest newRequest = new UrlRewriteRequestWrapper(request, builder.toUriString());
-        filterChain.doFilter(newRequest, response);
-        
-    }
-    
-    private String checkValidUrl(final String url) {
-        if (!url.startsWith(clusterMasterService.getMasterUrl())) {
-            throw new IllegalArgumentException("Invalid URL");
+        if (StringUtils.isNotEmpty(request.getQueryString())) {
+            builder.query(request.getQueryString());
         }
-        return url;
+
+        LOG.debug("forwarding request to url: {}", builder.toUriString());
+        // Create request entity
+        HttpHeaders headers = new HttpHeaders();
+        copyHeaders(request, headers);
+        HttpEntity<byte[]> requestEntity = new HttpEntity<>(getBody(request), headers);
+        // Send request
+        ResponseEntity<String> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.valueOf(request.getMethod()), requestEntity, String.class);
+
+        // Set response status and headers
+        response.setStatus(responseEntity.getStatusCodeValue());
+        copyHeaders(responseEntity.getHeaders(), response);
+        // fix cors error
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+        try (Writer writer = response.getWriter()) {
+//            writer.write(URLEncoder.encode(Objects.requireNonNull(responseEntity.getBody()), "UTF-8"));
+            writer.write(Objects.requireNonNull(responseEntity.getBody()));
+            writer.flush();
+        }
+        
     }
     
     private void copyHeaders(final HttpServletRequest request, final HttpHeaders headers) {
         Collections.list(request.getHeaderNames())
-                .forEach(headerName -> {
-                    headers.add(headerName, request.getHeader(headerName).replaceAll("[^a-zA-Z ]", ""));
-                });
+                .forEach(headerName -> headers.add(headerName, request.getHeader(headerName)));
     }
     
     private void copyHeaders(final HttpHeaders sourceHeaders, final HttpServletResponse response) {
         sourceHeaders.forEach((headerName, headerValues) -> {
-            String name = headerName.replaceAll("[^a-zA-Z ]", "");
-            if (!response.containsHeader(name)) {
+//            String name = headerName.replaceAll("[^a-zA-Z ]", "");
+            if (!response.containsHeader(headerName)) {
                 headerValues.forEach(headerValue -> {
-                    response.addHeader(name, headerValue.replaceAll("[^a-zA-Z ]", ""));
+                    response.addHeader(headerName, headerValue);
                 });
             }
         });
@@ -186,17 +170,4 @@ public class ClusterForwardFilter extends OncePerRequestFilter {
         return baos.toByteArray();
     }
     
-    static final class UrlRewriteRequestWrapper extends HttpServletRequestWrapper {
-        private final String url;
-        
-        private UrlRewriteRequestWrapper(final HttpServletRequest request, final String url) {
-            super(request);
-            this.url = url;
-        }
-        
-        @Override
-        public String getRequestURI() {
-            return this.url;
-        }
-    }
 }
