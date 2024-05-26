35539f9baa9ff5b60b6973cf17753eb4c5ef2b10
liuhy <rokkki@163.com>
2024-05-26 10:17:46 +0800

    ShenYu Admin Cluster, websocket #5448

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-storage/k8s/script/e2e-mysql.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-storage/k8s/script/e2e-mysql.sh
@@ -41,7 +41,7 @@ kubectl logs "$(kubectl get pod -o wide | grep shenyu-admin | awk '{print $1}')"
 kubectl logs "$(kubectl get pod -o wide | grep shenyu-bootstrap | awk '{print $1}')"
 
 ## run e2e-test
-
+sleep 60s
 curl -S "http://localhost:31195/actuator/pluginData"
 
 ./mvnw -B -f ./shenyu-e2e/pom.xml -pl shenyu-e2e-case/shenyu-e2e-case-storage -am test
