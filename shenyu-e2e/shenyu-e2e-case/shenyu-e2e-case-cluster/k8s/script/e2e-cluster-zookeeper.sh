1d1b15a3a906d161f633e75bcd44c1eb54bda434
liuhy <rokkki@163.com>
2024-07-04 19:12:52 +0800

    ShenYu Admin Cluster, cluster e2e zookeeper #5448

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-cluster/k8s/script/e2e-cluster-zookeeper.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-cluster/k8s/script/e2e-cluster-zookeeper.sh
@@ -24,7 +24,7 @@ bash "$shenyuTestCaseDir"/k8s/script/init/mysql_container_init.sh
 curPath=$(readlink -f "$(dirname "$0")")
 PRGDIR=$(dirname "$curPath")
 echo "$PRGDIR"
-kubectl apply -f "${SHENYU_TESTCASE_DIR}"/k8s/shenyu-zookeeper.yml
+kubectl apply -f "${shenyuTestCaseDir}"/k8s/shenyu-zookeeper.yml
 kubectl apply -f "${PRGDIR}"/shenyu-cluster-zookeeper.yml
 
 kubectl get pod -o wide
