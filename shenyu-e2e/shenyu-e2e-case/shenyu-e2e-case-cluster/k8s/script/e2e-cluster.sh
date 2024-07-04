ab912c5cb605e5bc089162b6ddde2895f3f3eeb1
liuhy <rokkki@163.com>
2024-07-04 11:24:38 +0800

    ShenYu Admin Cluster, e2e #5448

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-cluster/k8s/script/e2e-cluster.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-cluster/k8s/script/e2e-cluster.sh
@@ -32,9 +32,9 @@ kubectl get pod -o wide
 sleep 30s
 
 chmod +x "${curPath}"/healthcheck.sh
-sh "${curPath}"/healthcheck.sh cluster http://localhost:31095/actuator/health  http://localhost:31096/actuator/health http://localhost:31195/actuator/health
+sh "${curPath}"/healthcheck.sh cluster http://localhost:31095/actuator/health http://localhost:31096/actuator/health http://localhost:31195/actuator/health
 
-kubectl logs "$(kubectl get pod -o wide | grep shenyu-mysql | awk '{print $1}')"
+#kubectl logs "$(kubectl get pod -o wide | grep shenyu-mysql | awk '{print $1}')"
 
 kubectl logs "$(kubectl get pod -o wide | grep shenyu-admin | awk '{print $1}')"
 
