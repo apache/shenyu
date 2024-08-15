3871f5f102731677b2098bd8a148872c87dd0772
liuhy <rokkki@163.com>
2024-08-15 15:39:22 +0800

    [type:feature] upgrade checkstyle plugin version and fix checkstyle errors

--- shenyu-common/src/test/java/org/apache/shenyu/common/concurrent/MemoryLimiterTest.java
+++ shenyu-common/src/test/java/org/apache/shenyu/common/concurrent/MemoryLimiterTest.java
@@ -32,6 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 /**
  * Test cases for MemoryLimiter.
  */
--- shenyu-common/src/test/java/org/apache/shenyu/common/dto/ConfigDataTest.java
+++ shenyu-common/src/test/java/org/apache/shenyu/common/dto/ConfigDataTest.java
@@ -24,6 +24,7 @@ import java.util.Collections;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
 /**
  * Test case for ConfigData.
  */
