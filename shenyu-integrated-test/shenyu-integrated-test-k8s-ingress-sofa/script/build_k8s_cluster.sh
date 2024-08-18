6c8510f19ba25753848cd3d3b6421c6abf0ba26e
liuhy <rokkki@163.com>
2024-08-18 08:11:27 +0800

    [type:bugfix] fix ingress sofa ci error

--- shenyu-integrated-test/shenyu-integrated-test-k8s-ingress-sofa/script/build_k8s_cluster.sh
+++ shenyu-integrated-test/shenyu-integrated-test-k8s-ingress-sofa/script/build_k8s_cluster.sh
@@ -25,3 +25,7 @@ kubectl wait --for=condition=Ready pod -l app=shenyu-examples-sofa -n shenyu-ing
 kubectl apply -f ./shenyu-integrated-test/shenyu-integrated-test-k8s-ingress-sofa/deploy/deploy-shenyu.yaml
 kubectl wait --for=condition=Ready pod -l app=shenyu-ingress-controller -n shenyu-ingress
 kubectl apply -f ./shenyu-examples/shenyu-examples-sofa/shenyu-examples-sofa-service/k8s/ingress.yml
+
+kubectl get pod -o wide
+
+kubectl logs "$(kubectl get pod -o wide | grep shenyu-examples-sofa | awk '{print $1}')"
