663c5a7656a91ef520ab64f5d6942aa9d7901c5d
moremind <hefengen@apache.org>
2024-09-03 14:16:14 +0800

    refactor namespace id

--- shenyu-client/shenyu-client-core/src/main/java/org/apache/shenyu/client/core/client/AbstractContextRefreshedEventListener.java
+++ shenyu-client/shenyu-client-core/src/main/java/org/apache/shenyu/client/core/client/AbstractContextRefreshedEventListener.java
@@ -83,11 +83,6 @@ public abstract class AbstractContextRefreshedEventListener<T, A extends Annotat
     protected static final String PATH_SEPARATOR = "/";
 
     protected static final String EVERY_PATH = "**";
-    
-    /**
-     * multiple namespace support.
-     */
-    protected final List<String> namespace;
 
     private final ShenyuClientRegisterEventPublisher publisher = ShenyuClientRegisterEventPublisher.getInstance();
 
@@ -108,6 +103,11 @@ public abstract class AbstractContextRefreshedEventListener<T, A extends Annotat
     private ApplicationContext context;
 
     private final Boolean isDiscoveryLocalMode;
+    
+    /**
+     * multiple namespace support.
+     */
+    private final List<String> namespace;
 
     /**
      * Instantiates a new context refreshed event listener.
