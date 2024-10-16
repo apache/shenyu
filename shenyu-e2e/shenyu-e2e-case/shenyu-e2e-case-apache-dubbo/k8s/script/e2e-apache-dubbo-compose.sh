7b02fddb125575dc4bc24a9a0ce6c55f0ec4d070
moremind <hefengen@hotmail.com>
2024-10-16 10:23:13 +0800

    fix mysql script

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-apache-dubbo/k8s/script/e2e-apache-dubbo-compose.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-apache-dubbo/k8s/script/e2e-apache-dubbo-compose.sh
@@ -88,7 +88,7 @@ for sync in "${SYNC_ARRAY[@]}"; do
 #  docker compose -f "$SHENYU_TESTCASE_DIR"/compose/shenyu-mysql.yml rm -f
 #  kubectl delete -f "${SHENYU_TESTCASE_DIR}"/k8s/sync/shenyu-admin-"${sync}".yml
 docker compose -f "$SHENYU_TESTCASE_DIR"/compose/sync/shenyu-sync-"${sync}".yml stop
-  docker compose -f "$SHENYU_TESTCASE_DIR"/compose/sync/shenyu-sync"${sync}".yml rm -f
+  docker compose -f "$SHENYU_TESTCASE_DIR"/compose/sync/shenyu-sync-"${sync}".yml rm -f
 #  kubectl delete -f "${SHENYU_TESTCASE_DIR}"/k8s/sync/shenyu-bootstrap-"${sync}".yml
 #  docker compose -f "$SHENYU_TESTCASE_DIR"/compose/sync/shenyu-bootstrap-"${sync}".yml rm -f
 #  kubectl delete -f "${PRGDIR}"/shenyu-examples-dubbo.yml
