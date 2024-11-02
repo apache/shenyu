521eb8c2766007a7f5d378ec746009218e417b14
liuhy <liuhongyu@apache.org>
2024-11-02 18:45:13 +0800

    [type:feature] namespace user rel

---
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/service/impl/NamespaceUserServiceImpl.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shenyu.admin.service.impl;
+
+import org.apache.shenyu.admin.exception.ShenyuAdminException;
+import org.apache.shenyu.admin.mapper.NamespaceUserRelMapper;
+import org.apache.shenyu.admin.model.entity.DashboardUserDO;
+import org.apache.shenyu.admin.model.entity.NamespaceUserRelDO;
+import org.apache.shenyu.admin.model.event.user.UserCreatedEvent;
+import org.apache.shenyu.admin.model.vo.NamespaceUserRelVO;
+import org.apache.shenyu.admin.service.NamespaceUserService;
+import org.apache.shenyu.common.constant.AdminConstants;
+import org.apache.shenyu.common.constant.Constants;
+import org.apache.shenyu.common.utils.UUIDUtils;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class NamespaceUserServiceImpl implements NamespaceUserService {
+    
+    private final NamespaceUserRelMapper namespaceUserRelMapper;
+    
+    public NamespaceUserServiceImpl(final NamespaceUserRelMapper namespaceUserRelMapper) {
+        this.namespaceUserRelMapper = namespaceUserRelMapper;
+    }
+    
+    @Override
+    public NamespaceUserRelVO create(final String namespaceId, final String userId) {
+        NamespaceUserRelDO existNamespaceUserRelDO = namespaceUserRelMapper.selectByNamespaceIdAndUserId(namespaceId, userId);
+        if (!Objects.isNull(existNamespaceUserRelDO)) {
+            throw new ShenyuAdminException(AdminConstants.NAMESPACE_USER_EXIST);
+        }
+        String uuid = UUIDUtils.getInstance().generateShortUuid();
+        NamespaceUserRelDO namespaceUserRelDO = NamespaceUserRelDO.builder()
+                .id(uuid)
+                .namespaceId(namespaceId)
+                .userId(userId)
+                .build();
+        namespaceUserRelMapper.insertSelective(namespaceUserRelDO);
+        
+        return NamespaceUserRelVO.builder()
+                .id(uuid)
+                .namespaceId(namespaceId)
+                .userId(userId)
+                .build();
+    }
+    
+    @EventListener(value = UserCreatedEvent.class)
+    public void onUserCreated(final UserCreatedEvent event) {
+        DashboardUserDO dashboardUserDO = event.getChangedUser();
+        if (Objects.isNull(dashboardUserDO)) {
+            return;
+        }
+        create(Constants.SYS_DEFAULT_NAMESPACE_ID, dashboardUserDO.getId());
+    }
+}
