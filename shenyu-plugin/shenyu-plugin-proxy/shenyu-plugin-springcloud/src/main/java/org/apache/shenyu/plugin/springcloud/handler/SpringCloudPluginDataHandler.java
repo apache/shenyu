a72ee71200ff8f4cb153dda1cf2ead012fa3fd69
yunlongn <yunlongn@outlook.com>
2024-11-05 14:24:30 +0800

    [type:refactor] refactor discovery plugin.

--- shenyu-plugin/shenyu-plugin-proxy/shenyu-plugin-springcloud/src/main/java/org/apache/shenyu/plugin/springcloud/handler/SpringCloudPluginDataHandler.java
+++ shenyu-plugin/shenyu-plugin-proxy/shenyu-plugin-springcloud/src/main/java/org/apache/shenyu/plugin/springcloud/handler/SpringCloudPluginDataHandler.java
@@ -18,28 +18,22 @@
 package org.apache.shenyu.plugin.springcloud.handler;
 
 
-import com.alibaba.nacos.client.naming.utils.CollectionUtils;
 import org.apache.shenyu.common.config.ShenyuConfig;
 import org.apache.shenyu.common.constant.Constants;
 import org.apache.shenyu.common.dto.RuleData;
 import org.apache.shenyu.common.dto.SelectorData;
 import org.apache.shenyu.common.dto.convert.rule.impl.SpringCloudRuleHandle;
-import org.apache.shenyu.common.dto.convert.selector.DivideUpstream;
 import org.apache.shenyu.common.dto.convert.selector.SpringCloudSelectorHandle;
 import org.apache.shenyu.common.enums.PluginEnum;
 import org.apache.shenyu.common.utils.GsonUtils;
 import org.apache.shenyu.loadbalancer.cache.UpstreamCacheManager;
-import org.apache.shenyu.loadbalancer.entity.Upstream;
 import org.apache.shenyu.plugin.base.cache.CommonHandleCache;
 import org.apache.shenyu.plugin.base.handler.PluginDataHandler;
 import org.apache.shenyu.plugin.base.utils.BeanHolder;
 import org.apache.shenyu.plugin.base.utils.CacheKeyUtils;
-import org.apache.shenyu.plugin.springcloud.cache.ServiceInstanceCache;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 /**
  * The type spring cloud plugin data handler.
@@ -58,13 +52,6 @@ public class SpringCloudPluginDataHandler implements PluginDataHandler {
 
     @Override
     public void handlerSelector(final SelectorData selectorData) {
-        SpringCloudSelectorHandle springCloudSelectorHandle = GsonUtils.getInstance().fromJson(selectorData.getHandle(), SpringCloudSelectorHandle.class);
-        SELECTOR_CACHED.get().cachedHandle(selectorData.getId(), springCloudSelectorHandle);
-        if (CollectionUtils.isEmpty(springCloudSelectorHandle.getDivideUpstreams())) {
-            UpstreamCacheManager.getInstance().removeByKey(selectorData.getId());
-            return;
-        }
-        UpstreamCacheManager.getInstance().submit(selectorData.getId(), convertUpstreamList(springCloudSelectorHandle.getDivideUpstreams()));
         if (!selectorData.getContinued()) {
             RULE_CACHED.get().cachedHandle(CacheKeyUtils.INST.getKey(selectorData.getId(), Constants.DEFAULT_RULE), SpringCloudRuleHandle.newDefaultInstance());
         }
@@ -72,11 +59,6 @@ public class SpringCloudPluginDataHandler implements PluginDataHandler {
 
     @Override
     public void removeSelector(final SelectorData selectorData) {
-        if (springCloudCacheConfig.getEnabled()) {
-            SpringCloudSelectorHandle selectorHandle = SELECTOR_CACHED.get().obtainHandle(selectorData.getId());
-            ServiceInstanceCache.removeServiceInstance(selectorHandle.getServiceId());
-        }
-        SELECTOR_CACHED.get().removeHandle(selectorData.getId());
         UpstreamCacheManager.getInstance().removeByKey(selectorData.getId());
         RULE_CACHED.get().removeHandle(CacheKeyUtils.INST.getKey(selectorData.getId(), Constants.DEFAULT_RULE));
     }
@@ -97,17 +79,6 @@ public class SpringCloudPluginDataHandler implements PluginDataHandler {
     @Override
     public String pluginNamed() {
         return PluginEnum.SPRING_CLOUD.getName();
-    }
-
-    private List<Upstream> convertUpstreamList(final List<DivideUpstream> upstreamList) {
-        return upstreamList.stream().map(u -> Upstream.builder()
-                .protocol(u.getProtocol())
-                .url(u.getUpstreamUrl())
-                .weight(u.getWeight())
-                .status(u.isStatus())
-                .timestamp(u.getTimestamp())
-                .warmup(u.getWarmup())
-                .build()).collect(Collectors.toList());
     }
 
 }
