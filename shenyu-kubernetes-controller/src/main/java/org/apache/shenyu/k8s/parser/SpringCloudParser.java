cf8113be58fb048b1320bda45e9f97d7facd269d
zhengpeng <847850277@qq.com>
2024-11-11 11:07:32 +0800

    [type:fix] cli.

--- shenyu-kubernetes-controller/src/main/java/org/apache/shenyu/k8s/parser/SpringCloudParser.java
+++ shenyu-kubernetes-controller/src/main/java/org/apache/shenyu/k8s/parser/SpringCloudParser.java
@@ -59,7 +59,6 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
