4271789992a00e42dd26063bd2061e8a0dc1ba3a
Misaya295 <chenwenkang@apache.org>
2024-11-15 09:11:58 +0800

    fix: fix cr

--- shenyu-admin/src/main/java/org/apache/shenyu/admin/scale/config/ScaleProperties.java
+++ shenyu-admin/src/main/java/org/apache/shenyu/admin/scale/config/ScaleProperties.java
@@ -24,7 +24,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "shenyu.k8s.scale")
 public class ScaleProperties {
 
-    private boolean enabled = false;
+    private boolean enabled = true;
 
     private long monitorInterval;
 
