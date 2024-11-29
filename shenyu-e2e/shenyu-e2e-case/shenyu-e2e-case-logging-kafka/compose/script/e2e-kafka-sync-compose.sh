e080ed73b46542df33f700f209c198f4c515d962
huk <2509913103@qq.com>
2024-11-29 09:55:26 +0800

    [type:feat]add kafka logging e2e test

--- shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-logging-kafka/compose/script/e2e-kafka-sync-compose.sh
+++ shenyu-e2e/shenyu-e2e-case/shenyu-e2e-case-logging-kafka/compose/script/e2e-kafka-sync-compose.sh
@@ -46,7 +46,7 @@ for sync in "${SYNC_ARRAY[@]}"; do
   sleep 10s
   docker ps -a
   ## run e2e-test
-  ./mvnw -B -f ./shenyu-e2e/pom.xml -pl shenyu-e2e-case/shenyu-e2e-case-http -am test
+  ./mvnw -B -f ./shenyu-e2e/pom.xml -pl shenyu-e2e-case/shenyu-e2e-case-logging-kafka -am test
   # shellcheck disable=SC2181
   if (($?)); then
     echo "${sync}-sync-e2e-test failed"
