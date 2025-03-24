8d64a4a6ab9661c117a307aa253849862a221cda
dengtianjiao <jiao_snow@126.com>
2025-03-25 01:02:48 +0800

    修复格式

--- shenyu-common/src/main/java/org/apache/shenyu/common/enums/HttpRetryBackoffSpecEnum.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/enums/HttpRetryBackoffSpecEnum.java
@@ -1,32 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shenyu.common.enums;
 
 import java.util.Arrays;
 
+/**
+ * The http scheme enum.
+ */
 public enum HttpRetryBackoffSpecEnum {
+
     /**
-     * 默认重试
+     * 默认重试.
      */
     DEFAULT_BACKOFF("default"),
 
     /**
-     * 固定重试
+     * 固定重试.
      */
     FIXED_BACKOFF("fixed"),
 
     /**
-     * 指数重试
+     * 指数重试.
      */
     EXPONENTIAL_BACKOFF("exponential"),
 
     /**
-     * 自定义重试
+     * 自定义重试.
      */
     CUSTOM_BACKOFF("custom");
 
     private final String name;
 
 
-    HttpRetryBackoffSpecEnum(String name) {
+    HttpRetryBackoffSpecEnum(final String name) {
         this.name = name;
     }
 
