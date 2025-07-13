242acf5b356f65b8b6675493bfea9b3f31320e3e
Yusiheng <2874602095@qq.com>
2025-07-13 19:56:41 +0800

    [feat] add aiRequestTransformer config based on rule sql

--- shenyu-plugin/shenyu-plugin-ai/shenyu-plugin-ai-request-transformer/src/main/java/org/apache/shenyu/plugin/ai/transformer/request/AiRequestTransformerPlugin.java
+++ shenyu-plugin/shenyu-plugin-ai/shenyu-plugin-ai-request-transformer/src/main/java/org/apache/shenyu/plugin/ai/transformer/request/AiRequestTransformerPlugin.java
@@ -84,6 +84,7 @@ public class AiRequestTransformerPlugin extends AbstractShenyuPlugin {
         ChatClient client = ChatClientCache.getInstance().getClient("default");
         // Create final config with rule handle taking precedence
         if (Objects.nonNull(aiRequestTransformerHandle)) {
+            aiRequestTransformerConfig = new AiRequestTransformerConfig();
             Optional.ofNullable(aiRequestTransformerHandle.getProvider()).ifPresent(aiRequestTransformerConfig::setProvider);
             Optional.ofNullable(aiRequestTransformerHandle.getBaseUrl()).ifPresent(aiRequestTransformerConfig::setBaseUrl);
             Optional.ofNullable(aiRequestTransformerHandle.getApiKey()).ifPresent(aiRequestTransformerConfig::setApiKey);
@@ -105,11 +106,10 @@ public class AiRequestTransformerPlugin extends AbstractShenyuPlugin {
             return chain.execute(exchange);
         }
 
-        ChatModel aiModel = aiModelFactoryRegistry
-                .getFactory(AiModelProviderEnum.getByName(provider))
-                .createAiModel(AiRequestTransformerPluginHandler.convertConfig(aiRequestTransformerConfig));
-
         if (Objects.isNull(client)) {
+            ChatModel aiModel = aiModelFactoryRegistry
+                    .getFactory(AiModelProviderEnum.getByName(provider))
+                    .createAiModel(AiRequestTransformerPluginHandler.convertConfig(aiRequestTransformerConfig));
             client = ChatClientCache.getInstance().init(rule.getId(), aiModel);
         }
 
