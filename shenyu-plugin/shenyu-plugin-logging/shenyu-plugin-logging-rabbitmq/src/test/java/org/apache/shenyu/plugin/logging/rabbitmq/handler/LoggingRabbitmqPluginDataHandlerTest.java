751dba177d12b4f4c09eb428c162b5759115241b
Yusiheng <2874602095@qq.com>
2025-07-28 22:04:13 +0800

    fix

--- shenyu-plugin/shenyu-plugin-logging/shenyu-plugin-logging-rabbitmq/src/test/java/org/apache/shenyu/plugin/logging/rabbitmq/handler/LoggingRabbitmqPluginDataHandlerTest.java
+++ shenyu-plugin/shenyu-plugin-logging/shenyu-plugin-logging-rabbitmq/src/test/java/org/apache/shenyu/plugin/logging/rabbitmq/handler/LoggingRabbitmqPluginDataHandlerTest.java
@@ -23,18 +23,11 @@ import org.apache.shenyu.common.dto.SelectorData;
 import org.apache.shenyu.common.enums.ParamTypeEnum;
 import org.apache.shenyu.common.enums.PluginEnum;
 import org.apache.shenyu.common.enums.SelectorTypeEnum;
-import org.apache.shenyu.common.utils.GsonUtils;
-import org.apache.shenyu.common.utils.Singleton;
 import org.apache.shenyu.plugin.logging.rabbitmq.client.RabbitmqLogCollectClient;
-import org.apache.shenyu.plugin.logging.rabbitmq.config.RabbitmqLogCollectConfig;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
+import org.apache.shenyu.plugin.logging.rabbitmq.handler.LoggingRabbitmqPluginDataHandler;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -42,18 +35,10 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 /**
  * The Test Case For LoggingRabbitmqPluginDataHandler.
  */
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
-public final class LoggingRabbitmqPluginDataHandlerTest {
+public class LoggingRabbitmqPluginDataHandlerTest {
 
     private final SelectorData selectorData = new SelectorData();
 
@@ -66,9 +51,9 @@ public final class LoggingRabbitmqPluginDataHandlerTest {
 
     private LoggingRabbitmqPluginDataHandler loggingRabbitmqPluginDataHandler;
 
-    @BeforeEach
+    @Before
     public void setUp() {
-        loggingRabbitmqPluginDataHandler = Mockito.spy(new LoggingRabbitmqPluginDataHandler());
+        this.loggingRabbitmqPluginDataHandler = new LoggingRabbitmqPluginDataHandler();
         selectorData.setId("1");
         selectorData.setType(SelectorTypeEnum.CUSTOM_FLOW.getCode());
         selectorData.setHandle("{\"routingKey\":\"routingKey.logging.rabbitmq\", \"sampleRate\":\"1\"}");
@@ -91,80 +76,24 @@ public final class LoggingRabbitmqPluginDataHandlerTest {
         loggingRabbitmqPluginDataHandler.handlerPlugin(pluginData);
         Field field = loggingRabbitmqPluginDataHandler.getClass().getDeclaredField("RABBITMQ_LOG_COLLECT_CLIENT");
         field.setAccessible(Boolean.TRUE);
-        Assertions.assertEquals(field.get(loggingRabbitmqPluginDataHandler).getClass(), RabbitmqLogCollectClient.class);
+        Assert.assertEquals(field.get(loggingRabbitmqPluginDataHandler).getClass(), RabbitmqLogCollectClient.class);
         pluginData.setEnabled(Boolean.FALSE);
         loggingRabbitmqPluginDataHandler.handlerPlugin(pluginData);
     }
 
     @Test
     public void testPluginNamed() {
-        Assertions.assertEquals(loggingRabbitmqPluginDataHandler.pluginNamed(), PluginEnum.LOGGING_RABBITMQ.getName());
+        Assert.assertEquals(loggingRabbitmqPluginDataHandler.pluginNamed(), PluginEnum.LOGGING_RABBITMQ.getName());
     }
 
     @Test
     public void testGetRabbitmqLogCollectClient() {
-        Assertions.assertEquals(LoggingRabbitmqPluginDataHandler.getRabbitmqLogCollectClient().getClass(), RabbitmqLogCollectClient.class);
+        Assert.assertEquals(LoggingRabbitmqPluginDataHandler.getRabbitmqLogCollectClient().getClass(), RabbitmqLogCollectClient.class);
     }
 
     @Test
     public void testGetSelectApiConfigMap() {
-        Assertions.assertEquals(LoggingRabbitmqPluginDataHandler.getSelectApiConfigMap().getClass(), ConcurrentHashMap.class);
-    }
-
-    @Test
-    public void testHandlerPluginUpdateSameConfig() {
-        RabbitmqLogCollectConfig.RabbitmqLogConfig existingConfig = createValidConfig();
-        Singleton.INST.single(existingConfig.getClass(), existingConfig);
-
-        PluginData pluginData = createPluginData();
-        pluginData.setConfig(GsonUtils.getGson().toJson(existingConfig));
-
-        loggingRabbitmqPluginDataHandler.handlerPlugin(pluginData);
-
-        verify(loggingRabbitmqPluginDataHandler, never()).doRefreshConfig(any());
-    }
-
-    @Test
-    public void testHandlerPluginUpdateDifferentConfig() {
-        RabbitmqLogCollectConfig.RabbitmqLogConfig existingConfig = createValidConfig();
-        Singleton.INST.single(existingConfig.getClass(), existingConfig);
-
-        RabbitmqLogCollectConfig.RabbitmqLogConfig updatedConfig = createValidConfig();
-        updatedConfig.setUsername("updatedUsername");
-        updatedConfig.setPassword("updatedPassword");
-        PluginData pluginData = createPluginData();
-        pluginData.setConfig(GsonUtils.getGson().toJson(updatedConfig));
-
-        doNothing().when(loggingRabbitmqPluginDataHandler).doRefreshConfig(any());
-
-        loggingRabbitmqPluginDataHandler.handlerPlugin(pluginData);
-
-        verify(loggingRabbitmqPluginDataHandler, times(1)).doRefreshConfig(any());
-    }
-
-    private PluginData createPluginData() {
-        PluginData pluginData = new PluginData();
-        pluginData.setEnabled(true);
-        pluginData.setId(UUID.randomUUID().toString().replace("-", ""));
-        return pluginData;
-    }
-
-    private RabbitmqLogCollectConfig.RabbitmqLogConfig createValidConfig() {
-        RabbitmqLogCollectConfig.RabbitmqLogConfig config = new RabbitmqLogCollectConfig.RabbitmqLogConfig();
-        config.setHost("127.0.0.1");
-        config.setPort(5672);
-        config.setPassword("admin");
-        config.setUsername("admin");
-        config.setExchangeName("exchange.logging.plugin");
-        config.setQueueName("queue.logging.plugin");
-        config.setRoutingKey("topic.logging");
-        config.setVirtualHost("/");
-        config.setExchangeType("direct");
-        config.setDurable(true);
-        config.setExclusive(false);
-        config.setAutoDelete(false);
-        config.setSampleRate("1");
-        return config;
+        Assert.assertEquals(LoggingRabbitmqPluginDataHandler.getSelectApiConfigMap().getClass(), ConcurrentHashMap.class);
     }
 
 }
