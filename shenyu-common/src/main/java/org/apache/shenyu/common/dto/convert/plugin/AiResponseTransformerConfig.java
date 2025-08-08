001ae4a5560995a4178099f9b68f1842e31962db
HY-love-sleep <583699747@qq.com>
2025-08-08 18:19:03 +0800

    fix: java-format

--- shenyu-common/src/main/java/org/apache/shenyu/common/dto/convert/plugin/AiResponseTransformerConfig.java
+++ shenyu-common/src/main/java/org/apache/shenyu/common/dto/convert/plugin/AiResponseTransformerConfig.java
@@ -171,4 +171,4 @@ public class AiResponseTransformerConfig {
                 + '}';
     }
 
-} 
+}
--- shenyu-plugin/shenyu-plugin-ai/shenyu-plugin-ai-response-transformer/src/main/java/org/apache/shenyu/plugin/ai/transformer/response/cache/ChatClientCache.java
+++ shenyu-plugin/shenyu-plugin-ai/shenyu-plugin-ai-response-transformer/src/main/java/org/apache/shenyu/plugin/ai/transformer/response/cache/ChatClientCache.java
@@ -84,4 +84,4 @@ public class ChatClientCache {
         private ApplicationConfigCacheInstance() {
         }
     }
-} 
+}
