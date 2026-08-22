8dc3305eb029213403240cf5aaba20fd6ff26a7b
Jerry聊AI <1394367234@qq.com>
2026-08-22 17:08:02 +0800

    refactor plugin-divide : convert upstream props as string map.

--- shenyu-plugin/shenyu-plugin-proxy/shenyu-plugin-divide/src/main/java/org/apache/shenyu/plugin/divide/handler/DivideUpstreamDataHandler.java
+++ shenyu-plugin/shenyu-plugin-proxy/shenyu-plugin-divide/src/main/java/org/apache/shenyu/plugin/divide/handler/DivideUpstreamDataHandler.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.stream.Collectors;
 
 /**
@@ -70,26 +69,23 @@ public class DivideUpstreamDataHandler implements DiscoveryUpstreamDataHandler {
             return Collections.emptyList();
         }
         return upstreamList.stream().map(u -> {
-            Properties properties = Optional.ofNullable(u.getProps()).map(ps -> GsonUtils.getInstance().fromJson(ps, Properties.class)).orElse(new Properties());
+            Map<String, String> metadata = Optional.ofNullable(u.getProps())
+                    .map(ps -> GsonUtils.getInstance().toObjectMap(ps, String.class))
+                    .map(HashMap::new)
+                    .orElseGet(HashMap::new);
             Upstream upstream = Upstream.builder()
                     .protocol(u.getProtocol())
                     .url(u.getUrl())
                     .weight(u.getWeight())
-                    .warmup(Integer.parseInt(properties.getProperty("warmup", "10")))
-                    .gray(Boolean.parseBoolean(properties.getProperty("gray", "false")))
-                    .healthCheckEnabled(Boolean.parseBoolean(properties.getProperty("healthCheckEnabled", "true")))
+                    .warmup(Integer.parseInt(metadata.getOrDefault("warmup", "10")))
+                    .gray(Boolean.parseBoolean(metadata.getOrDefault("gray", "false")))
+                    .healthCheckEnabled(Boolean.parseBoolean(metadata.getOrDefault("healthCheckEnabled", "true")))
                     .status(0 == u.getStatus())
                     .timestamp(Optional.ofNullable(u.getDateCreated()).map(Timestamp::getTime).orElse(System.currentTimeMillis()))
                     .build();
-            upstream.setMetadata(convertPropertiesToMetadata(properties));
+            upstream.setMetadata(metadata);
             return upstream;
         }).collect(Collectors.toList());
-    }
-
-    private Map<String, String> convertPropertiesToMetadata(final Properties properties) {
-        Map<String, String> metadata = new HashMap<>(properties.size());
-        properties.forEach((key, value) -> metadata.put(String.valueOf(key), String.valueOf(value)));
-        return metadata;
     }
 
 }
