d8efebf4b5d87c3ba06dbe5a998d4be5c899f47e
dengliming <liming.d.pro@gmail.com>
2026-09-22 11:41:56 +0800

    perf(loadbalancer): cache consistent hash rings

--- shenyu-loadbalancer/src/main/java/org/apache/shenyu/loadbalancer/spi/HashLoadBalancer.java
+++ shenyu-loadbalancer/src/main/java/org/apache/shenyu/loadbalancer/spi/HashLoadBalancer.java
@@ -17,6 +17,8 @@
 
 package org.apache.shenyu.loadbalancer.spi;
 
+import org.apache.shenyu.common.cache.WindowTinyLFUMap;
+import org.apache.shenyu.common.constant.Constants;
 import org.apache.shenyu.loadbalancer.entity.LoadBalanceData;
 import org.apache.shenyu.loadbalancer.entity.Upstream;
 import org.apache.shenyu.spi.Join;
@@ -24,9 +26,11 @@ import org.apache.shenyu.spi.Join;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.SortedMap;
-import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.TreeMap;
 import java.util.stream.IntStream;
 
 /**
@@ -39,33 +43,38 @@ public class HashLoadBalancer extends AbstractLoadBalancer {
      * virtual node used to solve unbalanced load.
      */
     private static final int VIRTUAL_NODE_NUM = 5;
+
+    private static final ThreadLocal<MessageDigest> MD5 = ThreadLocal.withInitial(HashLoadBalancer::newMessageDigest);
+
+    private final Map<List<String>, SortedMap<Long, Integer>> ringCache = new WindowTinyLFUMap<>(Constants.CACHE_MAX_COUNT);
     
     @Override
     public Upstream doSelect(final List<Upstream> upstreamList, final LoadBalanceData data) {
-        final ConcurrentSkipListMap<Long, Upstream> treeMap = new ConcurrentSkipListMap<>();
-        upstreamList.forEach(upstream -> IntStream.range(0, VIRTUAL_NODE_NUM).forEach(i -> {
-            long addressHash = hash("SHENYU-" + upstream.getUrl() + "-HASH-" + i);
-            treeMap.put(addressHash, upstream);
-        }));
+        final List<String> ringKey = new ArrayList<>(upstreamList.size());
+        upstreamList.forEach(upstream -> ringKey.add(upstream.getUrl()));
+        final SortedMap<Long, Integer> treeMap = ringCache.computeIfAbsent(List.copyOf(ringKey), this::buildRing);
         long hash = hash(data.getIp());
-        SortedMap<Long, Upstream> lastRing = treeMap.tailMap(hash);
+        SortedMap<Long, Integer> lastRing = treeMap.tailMap(hash);
         if (!lastRing.isEmpty()) {
-            return lastRing.get(lastRing.firstKey());
+            return upstreamList.get(lastRing.get(lastRing.firstKey()));
         }
-        return treeMap.firstEntry().getValue();
+        return upstreamList.get(treeMap.get(treeMap.firstKey()));
+    }
+
+    private SortedMap<Long, Integer> buildRing(final List<String> upstreamUrls) {
+        final SortedMap<Long, Integer> treeMap = new TreeMap<>();
+        IntStream.range(0, upstreamUrls.size()).forEach(index ->
+                IntStream.range(0, VIRTUAL_NODE_NUM).forEach(virtualNode -> {
+                    long addressHash = hash("SHENYU-" + upstreamUrls.get(index) + "-HASH-" + virtualNode);
+                    treeMap.put(addressHash, index);
+                }));
+        return treeMap;
     }
 
     private static long hash(final String key) {
-        // md5 byte
-        MessageDigest md5;
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("MD5 not supported", e);
-        }
+        MessageDigest md5 = MD5.get();
         md5.reset();
-        byte[] keyBytes;
-        keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
         md5.update(keyBytes);
         byte[] digest = md5.digest();
         // hash code, Truncate to 32-bits
@@ -74,5 +83,13 @@ public class HashLoadBalancer extends AbstractLoadBalancer {
                 | ((long) (digest[1] & 0xFF) << 8)
                 | (digest[0] & 0xFF);
         return hashCode & 0xffffffffL;
+    }
+
+    private static MessageDigest newMessageDigest() {
+        try {
+            return MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 not supported", e);
+        }
     }
 }
